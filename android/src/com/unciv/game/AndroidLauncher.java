14a1bc56264cdfc254267d176cc5ca9f885d1971
Yair Morgenstern <yairm210@hotmail.com>
2018-05-28 22:03:56 +0300

    Fixed crash caused by trebuchet listing "cannon" as upgrade, but the cannon doesn't exist yet 0_0

--- android/src/com/unciv/game/AndroidLauncher.java
+++ android/src/com/unciv/game/AndroidLauncher.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.unciv.UnCivGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new com.unciv.UnCivGame(), config);
+		initialize(new UnCivGame(), config);
 	}
 }
