a3eda1fd52150e1770ac479de4d29a8747cad72c
Yair Morgenstern <yairm210@hotmail.com>
2018-07-20 16:04:38 +0300

    Fixed bug that caused barbarians to not do anything

--- core/src/com/unciv/logic/GameInfo.kt
+++ core/src/com/unciv/logic/GameInfo.kt
@@ -38,7 +38,7 @@ class GameInfo {
         // We need to update the stats after ALL the cities are done updating because
         // maybe one of them has a wonder that affects the stats of all the rest of the cities
 
-        for (civInfo in civilizations.filterNot { it==player || it.isDefeated() }){
+        for (civInfo in civilizations.filterNot { it==player || (it.isDefeated() && !it.isBarbarianCivilization()) }){
             civInfo.startTurn()
             Automation().automateCivMoves(civInfo)
         }
