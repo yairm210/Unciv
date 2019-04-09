06b44140b82faa9b62d0da7eae70f691df235cd3
Yair Morgenstern <yairm210@hotmail.com>
2019-04-09 16:49:24 +0300

    Resolved #603 - Move unit icons back above city label

--- core/src/com/unciv/ui/worldscreen/TileGroupMap.kt
+++ core/src/com/unciv/ui/worldscreen/TileGroupMap.kt
@@ -30,6 +30,7 @@ class TileGroupMap<T: TileGroup>(tileGroups:Collection<T>, padding:Float): Group
         val baseLayers = ArrayList<Group>()
         val featureLayers = ArrayList<Group>()
         val miscLayers = ArrayList<Group>()
+        val unitLayers = ArrayList<Group>()
         val circleCrosshairFogLayers = ArrayList<Group>()
 
         for(group in tileGroups.sortedByDescending { it.tileInfo.position.x + it.tileInfo.position.y }){
@@ -37,13 +38,15 @@ class TileGroupMap<T: TileGroup>(tileGroups:Collection<T>, padding:Float): Group
             baseLayers.add(group.baseLayerGroup.apply { setPosition(group.x,group.y) })
             featureLayers.add(group.featureLayerGroup.apply { setPosition(group.x,group.y) })
             miscLayers.add(group.miscLayerGroup.apply { setPosition(group.x,group.y) })
+            unitLayers.add(group.unitLayerGroup.apply { setPosition(group.x,group.y) })
             circleCrosshairFogLayers.add(group.circleCrosshairFogLayerGroup.apply { setPosition(group.x,group.y) })
         }
         for(group in baseLayers) addActor(group)
         for(group in featureLayers) addActor(group)
         for(group in miscLayers) addActor(group)
         for(group in circleCrosshairFogLayers) addActor(group)
-        for(group in tileGroups) addActor(group) // The above layers are for the visual layers, this is for the cli
+        for(group in tileGroups) addActor(group) // The above layers are for the visual layers, this is for the clickability
+        for(group in unitLayers) addActor(group) // Aaand units above everything else.
 
 
         // there are tiles "below the zero",
