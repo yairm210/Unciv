d8d38bde10f32222243b13c82ec2efde5c4ff562
lishaoxia1985 <lishaoxia1985160@sina.com>
2019-05-02 23:27:03 +0800

    Update WorldScreenTopBar.kt

--- core/src/com/unciv/ui/worldscreen/WorldScreenTopBar.kt
+++ core/src/com/unciv/ui/worldscreen/WorldScreenTopBar.kt
@@ -162,7 +162,7 @@ class WorldScreenTopBar(val screen: WorldScreen) : Table() {
     private fun getHappinessText(civInfo: CivilizationInfo): String {
         var happinessText = civInfo.happiness.toString()
         if (civInfo.goldenAges.isGoldenAge())
-            happinessText += " GOLDEN AGE (${civInfo.goldenAges.turnsLeftForCurrentGoldenAge})"
+            happinessText += "    "+"GOLDEN AGE".tr()+"(${civInfo.goldenAges.turnsLeftForCurrentGoldenAge})"
         else
             happinessText += (" (" + civInfo.goldenAges.storedHappiness + "/"
                     + civInfo.goldenAges.happinessRequiredForNextGoldenAge() + ")")
