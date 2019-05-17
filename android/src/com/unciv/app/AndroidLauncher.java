437fce951abf21cf9cd866fc33413de3519f8a9d
lishaoxia1985 <lishaoxia1985160@sina.com>
2019-05-17 16:56:26 +0800

    update

--- android/src/com/unciv/app/AndroidLauncher.java
+++ android/src/com/unciv/app/AndroidLauncher.java
@@ -5,17 +5,13 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.unciv.UnCivGame;
-import core.java.nativefont.Handler;
 
-public class AndroidLauncher extends AndroidApplication implements Handler {
+public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		String version = BuildConfig.VERSION_NAME;
 		initialize(new UnCivGame(version), config);
-	}
-	@Override
-	public void login() {
 	}
 }
