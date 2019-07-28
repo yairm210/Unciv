2d24e62b0b2c02259cb614ba80ce0895b91713f9
Yair Morgenstern <yairm210@hotmail.com>
2019-07-28 22:50:22 +0300

    Resolved #974 - city stats not summmed correctly

--- core/src/com/unciv/models/stats/Stats.kt
+++ core/src/com/unciv/models/stats/Stats.kt
@@ -100,7 +100,7 @@ open class Stats() {
 class StatMap:LinkedHashMap<String,Stats>(){
     fun add(source:String,stats:Stats){
         if(!containsKey(source)) put(source,stats)
-        else get(source)!!.plus(stats)
+        else put(source, get(source)!!+stats)
         // This CAN'T be get(source)!!.add() because the initial stats we get are sometimes from other places -
         // for instance the Cities is from the currentCityStats and if we add to that we change the value in the cities themselves!
     }
