yairm210/Unciv

commit 750ca0c1e9e82ec77fd888208fb856cc393ad65e

Resolved #1252 - Tiles created inside your borders take control of the city center tile, so they don't become "Invisible cities"

core/src/com/unciv/logic/city/CityExpansionManager.kt
@@ -72,12 +72,17 @@ class CityExpansionManager {
 
     //region state-changing functions
     fun reset() {
-        for(tile in cityInfo.tiles.map { cityInfo.tileMap[it] })
+        for(tile in cityInfo.getTiles())
             relinquishOwnership(tile)
 
         cityInfo.getCenterTile().getTilesInDistance(1)
-                .filter { it.getCity()==null } // can't take ownership of owned tiles
+                .filter { it.getCity()==null } // can't take ownership of owned tiles (by other cities)
                 .forEach { takeOwnership(it) }
+
+        // The only way to create a city inside an owned tile is if it's in your territory
+        // In this case, if you don't assign control of the central tile to the city,
+        // It becomes an invisible city and weird shit starts happening
+        takeOwnership(cityInfo.getCenterTile())
     }
 
     private fun addNewTileWithCulture() {

commit 18ffe9b67f5e53b1780aff8b8459f09236436d5e

Removed unused Fonts functions Fixed bug on city creation caused by 750ca0c

core/src/com/unciv/logic/city/CityExpansionManager.kt
@@ -75,14 +75,15 @@ class CityExpansionManager {
         for(tile in cityInfo.getTiles())
             relinquishOwnership(tile)
 
-        cityInfo.getCenterTile().getTilesInDistance(1)
-                .filter { it.getCity()==null } // can't take ownership of owned tiles (by other cities)
-                .forEach { takeOwnership(it) }
-
         // The only way to create a city inside an owned tile is if it's in your territory
         // In this case, if you don't assign control of the central tile to the city,
         // It becomes an invisible city and weird shit starts happening
         takeOwnership(cityInfo.getCenterTile())
+
+        cityInfo.getCenterTile().getTilesInDistance(1)
+                .filter { it.getCity()==null } // can't take ownership of owned tiles (by other cities)
+                .forEach { takeOwnership(it) }
+
     }
 
     private fun addNewTileWithCulture() {

core/src/com/unciv/ui/utils/Fonts.kt
@@ -6,47 +6,12 @@ import com.unciv.UnCivGame
 import com.unciv.models.gamebasics.GameBasics
 import core.java.nativefont.NativeFont
 import core.java.nativefont.NativeFontPaint
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.net.URL
-import java.security.MessageDigest
 
 class Fonts {
+    // caches for memory and time saving
     companion object {
         val characterSetCache = HashMap<String, String>()
         val fontCache = HashMap<String, BitmapFont>()
-    }
-    fun download(link: String,fontForLanguage: String) {
-        if (!Gdx.files.local("fonts").exists())
-            Gdx.files.local("fonts").mkdirs()
-            val input = URL(link).openStream()
-            val output = FileOutputStream(Gdx.files.local("fonts/$fontForLanguage.ttf").file())
-            input.use {
-                output.use {
-                    input.copyTo(output)
-                }
-            }
-    }
-    fun getMD5(fontForLanguage: String):String {
-        val sb = StringBuffer("")
-        val md = MessageDigest.getInstance("MD5")
-        if (Gdx.files.local("fonts/$fontForLanguage.ttf").exists()) {
-            md.update(FileInputStream(Gdx.files.local("fonts/$fontForLanguage.ttf").file()).readBytes())
-            val b = md.digest()
-            for (i in b) {
-                var d = i.toInt()
-                if (d < 0) d = i + 256
-                if (d < 16) sb.append("0")
-                sb.append(Integer.toHexString(d))
-            }
-            return sb.toString()
-        }
-        return ""
-    }
-    fun containsFont(): Boolean {
-        if (Gdx.files.local("fonts/WenQuanYiMicroHei.ttf").exists())
-            return true
-        return false
     }
 
     fun getCharactersForFont(language:String=""): String {