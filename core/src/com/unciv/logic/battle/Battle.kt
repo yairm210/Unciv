8989237eb925083cf437d4edaf7e9b8728df3502
BenBooth <bennybooth@gmail.com>
2019-11-03 20:37:09 -0500

    fixed capture logic for barbarian

--- core/src/com/unciv/logic/battle/Battle.kt
+++ core/src/com/unciv/logic/battle/Battle.kt
@@ -254,11 +254,11 @@ class Battle(val gameInfo:GameInfo) {
     }
 
     private fun captureCivilianUnit(attacker: ICombatant, defender: ICombatant){
-        // barbarians, City-states, OCC don't capture civilians don't capture settlers
-        var cannotCaptureSettler = (attacker.getCivInfo().isBarbarian()
-                || attacker.getCivInfo().isCityState()
-                || attacker.getCivInfo().isPlayerOneCityChallenger())
-        if(cannotCaptureSettler && defender.getName()==Constants.settler){
+        // barbarians don't capture civilians
+        // City-states & OCC don't capture settlers
+        if(attacker.getCivInfo().isBarbarian()
+                || ((attacker.getCivInfo().isCityState() || attacker.getCivInfo().isPlayerOneCityChallenger())
+                        && defender.getName()==Constants.settler)){
             defender.takeDamage(100)
             return
         }
