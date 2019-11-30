5c2c4bea06d169f70783eb93ea3e4f983d16c7ab
u-ndefine <41176671+u-ndefine@users.noreply.github.com>
2019-11-30 11:31:21 +0800

    Minor typo fix at VictoryScreen.kt

--- core/src/com/unciv/ui/VictoryScreen.kt
+++ core/src/com/unciv/ui/VictoryScreen.kt
@@ -76,7 +76,7 @@ class VictoryScreen : PickerScreen() {
 
         val endGameMessage = when(description){
             "You have won a cultural victory!" -> "You have achieved victory through the awesome power of your Culture. Your civilization's greatness - the magnificence of its monuments and the power of its artists - have astounded the world! Poets will honor you as long as beauty brings gladness to a weary heart."
-            "You have won a domination victory!" -> "The world has been convulsed by war. May great and powerful civilizations have fallen, but you have survived - and emerged victorious! The world will long remember your glorious triumph!"
+            "You have won a domination victory!" -> "The world has been convulsed by war. Many great and powerful civilizations have fallen, but you have survived - and emerged victorious! The world will long remember your glorious triumph!"
             "You have won a scientific victory!" -> "You have achieved victory through mastery of Science! You have conquered the mysteries of nature and led your people on a voyage to a brave new world! Your triumph will be remembered as long as the stars burn in the night sky!"
             else -> "You have been defeated. Your civilization has been overwhelmed by its many foes. But your people do not despair, for they know that one day you shall return - and lead them forward to victory!"
         }
