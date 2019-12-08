7cf695b5b9e4baaa66aa37b6bb82c8f063d276d6
YueR <yuer@promote.cache-dns.local>
2019-12-08 19:29:24 +0800

    update

---
+++ android/src/com/unciv/app/AndroidLauncher.java
@@ -0,0 +1,22 @@
+package com.unciv.app;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.unciv.UncivGame;
+
+// If we convert this to Kotlin, the the Gradle build won't work. =(
+// Stuck with Java for now
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		String version = BuildConfig.VERSION_NAME;
+
+		config.useImmersiveMode=true;
+
+		initialize(new UncivGame(version), config);
+	}
+}
