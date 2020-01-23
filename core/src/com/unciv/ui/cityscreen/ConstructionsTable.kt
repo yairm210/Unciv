f9cd74ae7ad19fbd3f8d14a57a0ae2fb6eba21a4
Marerjh <40261003+Marerjh@users.noreply.github.com>
2020-01-23 15:50:45 +0200

    Update ConstructionsTable.kt

--- core/src/com/unciv/ui/cityscreen/ConstructionsTable.kt
+++ core/src/com/unciv/ui/cityscreen/ConstructionsTable.kt
@@ -31,7 +31,7 @@ class ConstructionsTable(val cityScreen: CityScreen) : Table(CameraStageBaseScre
     private val pad = 10f
 
     init {
-        showCityInfoTableButton = TextButton("Show stats drilldown", skin)
+        showCityInfoTableButton = TextButton("Show stats drilldown".tr(), skin)
         showCityInfoTableButton.onClick {
             cityScreen.showConstructionsTable = false
             cityScreen.update()
