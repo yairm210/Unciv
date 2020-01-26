f1fb8aea18eb11227ba36cccfc2e49a36945aa0f
Yair Morgenstern <yairm210@hotmail.com>
2020-01-27 00:11:01 +0200

    Added Marathon speed per user request

--- core/src/com/unciv/models/metadata/GameSpeed.kt
+++ core/src/com/unciv/models/metadata/GameSpeed.kt
@@ -3,13 +3,15 @@ package com.unciv.models.metadata
 enum class GameSpeed{
     Quick,
     Standard,
-    Epic;
+    Epic,
+    Marathon;
 
     fun getModifier(): Float {
         when(this) {
             Quick -> return 0.67f
             Standard -> return 1f
             Epic -> return 1.5f
+            Marathon -> return 1.5f
         }
     }
 }
