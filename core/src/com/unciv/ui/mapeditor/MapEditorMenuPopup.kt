yairm210/Unciv

commit b95844d2f4601f726eedac93df3c12d7394f5793

Refactor: Streamline popups (#1735)
First, `PopupTable` and all extending classes had the `Table` at the end removed. Second, the popup base classes `Popup(Table)` and `YesNoPopup(Table)` were moved from the worldscreen to the util package.

Third: The popups were inconsistent. In CameraStageBaseScreen, there was a check with 3 parts, one if any tutorial was showing, second if any child is a TradePopup, and third if the boolean field "hasPopupOpen" is true.

However, all of these checks were, in the end, `Popup(Table)`s on the screen. So, this check has simply been changed to check if any child is a `Popup(Table)`. All the other checks and their relating code could simply be removed.

core/src/com/unciv/ui/mapeditor/MapEditorMenuPopup.kt
@@ -12,16 +12,16 @@ import com.unciv.logic.map.RoadStatus
 import com.unciv.models.translations.tr
 import com.unciv.ui.saves.Gzip
 import com.unciv.ui.utils.onClick
-import com.unciv.ui.worldscreen.optionstable.DropBox
-import com.unciv.ui.worldscreen.optionstable.PopupTable
+import com.unciv.ui.worldscreen.mainmenu.DropBox
+import com.unciv.ui.utils.Popup
 import kotlin.concurrent.thread
 
-class MapEditorMenuPopup(mapEditorScreen: MapEditorScreen): PopupTable(mapEditorScreen){
+class MapEditorMenuPopup(mapEditorScreen: MapEditorScreen): Popup(mapEditorScreen){
     init{
         val mapNameEditor = TextField(mapEditorScreen.mapName, skin)
         mapNameEditor.addListener{ mapEditorScreen.mapName=mapNameEditor.text; true }
         add(mapNameEditor).row()
-        
+
         val clearCurrentMapButton = TextButton("Clear current map".tr(),skin)
         clearCurrentMapButton.onClick {
             for(tileGroup in mapEditorScreen.mapHolder.tileGroups.values)
@@ -72,13 +72,13 @@ class MapEditorMenuPopup(mapEditorScreen: MapEditorScreen): PopupTable(mapEditor
                     DropBox().uploadFile("/Maps/" + mapEditorScreen.mapName, gzippedMap)
 
                     remove()
-                    val uploadedSuccessfully = PopupTable(screen)
+                    val uploadedSuccessfully = Popup(screen)
                     uploadedSuccessfully.addGoodSizedLabel("Map uploaded successfully!").row()
                     uploadedSuccessfully.addCloseButton()
                     uploadedSuccessfully.open()
                 } catch (ex: Exception) {
                     remove()
-                    val couldNotUpload = PopupTable(screen)
+                    val couldNotUpload = Popup(screen)
                     couldNotUpload.addGoodSizedLabel("Could not upload map!").row()
                     couldNotUpload.addCloseButton()
                     couldNotUpload.open()

core/src/com/unciv/ui/utils/CameraStageBaseScreen.kt
@@ -32,14 +32,12 @@ import com.unciv.models.translations.tr
 import com.unciv.ui.tutorials.TutorialController
 import com.unciv.ui.tutorials.TutorialMiner
 import com.unciv.ui.tutorials.TutorialRender
-import com.unciv.ui.worldscreen.TradePopup
 import kotlin.concurrent.thread
 
 open class CameraStageBaseScreen : Screen {
 
     var game: UncivGame = UncivGame.Current
     var stage: Stage
-    var hasPopupOpen = false
 
     val tutorialController by lazy {
         TutorialController(TutorialMiner(JsonParser()), TutorialRender(this))
@@ -85,8 +83,8 @@ open class CameraStageBaseScreen : Screen {
         tutorialController.showTutorial(tutorial)
     }
 
-    fun hasVisibleDialogs(): Boolean =
-            tutorialController.isTutorialShowing() || stage.actors.any { it is TradePopup } || hasPopupOpen
+    fun hasOpenPopups(): Boolean =
+            stage.actors.any { it is Popup }
 
     companion object {
         var skin = Skin(Gdx.files.internal("skin/flat-earth-ui.json"))

core/src/com/unciv/ui/utils/Popup.kt
@@ -1,4 +1,4 @@
-package com.unciv.ui.worldscreen.optionstable
+package com.unciv.ui.utils
 
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.scenes.scene2d.Touchable
@@ -8,9 +8,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.utils.Align
 import com.unciv.models.translations.tr
-import com.unciv.ui.utils.*
 
-open class PopupTable(val screen: CameraStageBaseScreen): Table(CameraStageBaseScreen.skin) {
+/**
+ * Base class for all Popups, i.e. Tables that get rendered in the middle of a screen and on top of everything else
+ */
+open class Popup(val screen: CameraStageBaseScreen): Table(CameraStageBaseScreen.skin) {
     init {
         background = ImageGetter.getBackground(ImageGetter.getBlue().lerp(Color.BLACK, 0.5f))
 
@@ -18,7 +20,11 @@ open class PopupTable(val screen: CameraStageBaseScreen): Table(CameraStageBaseS
         this.defaults().pad(5f)
     }
 
+    /**
+     * Displays the Popup on the screen. Will not open the popup if another one is already open.
+     */
     fun open() {
+        if (screen.hasOpenPopups()) return;
         pack()
         center(screen.stage)
         screen.stage.addActor(this)

core/src/com/unciv/ui/worldscreen/TradePopup.kt
@@ -11,11 +11,11 @@ import com.unciv.models.translations.tr
 import com.unciv.ui.trade.DiplomacyScreen
 import com.unciv.ui.utils.addSeparator
 import com.unciv.ui.utils.toLabel
-import com.unciv.ui.worldscreen.optionstable.PopupTable
+import com.unciv.ui.utils.Popup
 import kotlin.math.max
 import kotlin.math.min
 
-class TradePopup(worldScreen: WorldScreen): PopupTable(worldScreen){
+class TradePopup(worldScreen: WorldScreen): Popup(worldScreen){
     init{
         val viewingCiv = worldScreen.viewingCiv
         val tradeRequest = viewingCiv.tradeRequests.first()
@@ -52,7 +52,7 @@ class TradePopup(worldScreen: WorldScreen): PopupTable(worldScreen){
             tradeLogic.acceptTrade()
             viewingCiv.tradeRequests.remove(tradeRequest)
             close()
-            PopupTable(worldScreen).apply {
+            Popup(worldScreen).apply {
                 add(otherCivLeaderName.toLabel()).colspan(2)
                 addSeparator()
                 addGoodSizedLabel("Excellent!").row()

core/src/com/unciv/ui/worldscreen/WorldScreenTopBar.kt
@@ -10,11 +10,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.unciv.UncivGame
 import com.unciv.logic.civilization.CivilizationInfo
 import com.unciv.models.ruleset.tile.ResourceType
-import com.unciv.models.translations.tr
 import com.unciv.models.stats.Stats
+import com.unciv.models.translations.tr
 import com.unciv.ui.EmpireOverviewScreen
 import com.unciv.ui.utils.*
-import com.unciv.ui.worldscreen.optionstable.WorldScreenMenuTable
+import com.unciv.ui.worldscreen.mainmenu.WorldScreenMenuPopup
 import kotlin.math.abs
 import kotlin.math.ceil
 import kotlin.math.roundToInt
@@ -95,8 +95,8 @@ class WorldScreenTopBar(val worldScreen: WorldScreen) : Table() {
                 .apply { setSize(50f, 50f) }
         menuButton.color = Color.WHITE
         menuButton.onClick {
-            if(worldScreen.stage.actors.none { it is WorldScreenMenuTable })
-                WorldScreenMenuTable(worldScreen)
+            if(worldScreen.stage.actors.none { it is WorldScreenMenuPopup })
+                WorldScreenMenuPopup(worldScreen)
         }
         menuButton.centerY(this)
         menuButton.x = menuButton.y

core/src/com/unciv/ui/worldscreen/mainmenu/WorldScreenMenuPopup.kt
@@ -1,4 +1,4 @@
-package com.unciv.ui.worldscreen.optionstable
+package com.unciv.ui.worldscreen.mainmenu
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
@@ -11,6 +11,7 @@ import com.unciv.ui.mapeditor.MapEditorScreen
 import com.unciv.ui.newgamescreen.NewGameScreen
 import com.unciv.ui.saves.LoadGameScreen
 import com.unciv.ui.saves.SaveGameScreen
+import com.unciv.ui.utils.Popup
 import com.unciv.ui.utils.addSeparator
 import com.unciv.ui.utils.disable
 import com.unciv.ui.utils.toLabel
@@ -19,7 +20,7 @@ import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.concurrent.thread
 
-class WorldScreenMenuTable(val worldScreen: WorldScreen) : PopupTable(worldScreen) {
+class WorldScreenMenuPopup(val worldScreen: WorldScreen) : Popup(worldScreen) {
 
     init {
         val width = 200f
@@ -69,13 +70,13 @@ class WorldScreenMenuTable(val worldScreen: WorldScreen) : PopupTable(worldScree
         addSeparator()
 
         addSquareButton("Options".tr()){
-            UncivGame.Current.worldScreen.stage.addActor(WorldScreenOptionsTable(worldScreen))
+            UncivGame.Current.worldScreen.stage.addActor(WorldScreenOptionsPopup(worldScreen))
             remove()
         }.size(width,height)
         addSeparator()
 
         addSquareButton("Community"){
-            WorldScreenCommunityTable(worldScreen)
+            WorldScreenCommunityPopup(worldScreen)
             remove()
         }.size(width,height)
         addSeparator()
@@ -92,7 +93,7 @@ class WorldScreenMenuTable(val worldScreen: WorldScreen) : PopupTable(worldScree
     fun openMultiplayerPopup(){
 
         close()
-        val multiplayerPopup = PopupTable(screen)
+        val multiplayerPopup = Popup(screen)
 
         multiplayerPopup.addGoodSizedLabel("To create a multiplayer game, check the 'multiplayer' toggle in the New Game screen, and for each human player insert that player's user ID.").row()
         multiplayerPopup.addGoodSizedLabel("You can assign your own user ID there easily, and other players can copy their user IDs here and send them to you for you to include them in the game.").row()
@@ -138,7 +139,7 @@ class WorldScreenMenuTable(val worldScreen: WorldScreen) : PopupTable(worldScree
     }
 }
 
-class WorldScreenCommunityTable(val worldScreen: WorldScreen) : PopupTable(worldScreen) {
+class WorldScreenCommunityPopup(val worldScreen: WorldScreen) : Popup(worldScreen) {
     init{
         addButton("Discord"){
             Gdx.net.openURI("https://discord.gg/bjrB4Xw")

commit c4ad9f478eb30254ffda1cb8ff98925245f23e5b

Fix community popup not opening
Fixes a regression in b95844d2f4601f726eedac93df3c12d7394f5793. This commit refactored popups and it was thought that the "screen has popup -> don't show popup" was correct for all popups. That assumption was incorrect, the community popup was not opening anymore as well as the game menu popups (editor and normal game) could not be opened over other popups anymore.

This commit fixes that by introducing a queue for popups. When you try to open a popup and one is already open, the popup you tried to open only gets shown when the popup that was already open is closed. This can be manually overridden with a calling the `open` method with a `(force = true)` argument.

Also, all popups are now and should be opened and closed only with their `open()` and `close()` methods to ensure this behavior works.

core/src/com/unciv/ui/mapeditor/MapEditorMenuPopup.kt
@@ -11,9 +11,9 @@ import com.unciv.logic.map.MapType
 import com.unciv.logic.map.RoadStatus
 import com.unciv.models.translations.tr
 import com.unciv.ui.saves.Gzip
+import com.unciv.ui.utils.Popup
 import com.unciv.ui.utils.onClick
 import com.unciv.ui.worldscreen.mainmenu.DropBox
-import com.unciv.ui.utils.Popup
 import kotlin.concurrent.thread
 
 class MapEditorMenuPopup(mapEditorScreen: MapEditorScreen): Popup(mapEditorScreen){
@@ -96,6 +96,6 @@ class MapEditorMenuPopup(mapEditorScreen: MapEditorScreen): Popup(mapEditorScree
         closeOptionsButton.onClick { close() }
         add(closeOptionsButton).row()
 
-        open()
+        open(force = true)
     }
 }

core/src/com/unciv/ui/mapeditor/MapEditorScreen.kt
@@ -13,6 +13,7 @@ import com.unciv.models.ruleset.RulesetCache
 import com.unciv.models.translations.tr
 import com.unciv.ui.utils.CameraStageBaseScreen
 import com.unciv.ui.utils.onClick
+import com.unciv.ui.utils.popups
 import com.unciv.ui.utils.setFontSize
 
 class MapEditorScreen(): CameraStageBaseScreen() {
@@ -76,7 +77,7 @@ class MapEditorScreen(): CameraStageBaseScreen() {
 
         val optionsMenuButton = TextButton("Menu".tr(), skin)
         optionsMenuButton.onClick {
-            if(stage.actors.any { it is MapEditorMenuPopup })
+            if(popups.any { it is MapEditorMenuPopup })
                 return@onClick // already open
             MapEditorMenuPopup(this)
         }

core/src/com/unciv/ui/utils/CameraStageBaseScreen.kt
@@ -7,21 +7,8 @@ import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.scenes.scene2d.Actor
-import com.badlogic.gdx.scenes.scene2d.InputEvent
-import com.badlogic.gdx.scenes.scene2d.InputListener
-import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.Touchable
-import com.badlogic.gdx.scenes.scene2d.ui.Button
-import com.badlogic.gdx.scenes.scene2d.ui.Cell
-import com.badlogic.gdx.scenes.scene2d.ui.CheckBox
-import com.badlogic.gdx.scenes.scene2d.ui.Image
-import com.badlogic.gdx.scenes.scene2d.ui.Label
-import com.badlogic.gdx.scenes.scene2d.ui.SelectBox
-import com.badlogic.gdx.scenes.scene2d.ui.Skin
-import com.badlogic.gdx.scenes.scene2d.ui.Table
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton
-import com.badlogic.gdx.scenes.scene2d.ui.TextField
+import com.badlogic.gdx.scenes.scene2d.*
+import com.badlogic.gdx.scenes.scene2d.ui.*
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.badlogic.gdx.utils.viewport.ExtendViewport
 import com.unciv.JsonParser
@@ -82,9 +69,6 @@ open class CameraStageBaseScreen : Screen {
     fun displayTutorial(tutorial: Tutorial) {
         tutorialController.showTutorial(tutorial)
     }
-
-    fun hasOpenPopups(): Boolean =
-            stage.actors.any { it is Popup }
 
     companion object {
         var skin = Skin(Gdx.files.internal("skin/flat-earth-ui.json"))

core/src/com/unciv/ui/utils/Popup.kt
@@ -18,20 +18,30 @@ open class Popup(val screen: CameraStageBaseScreen): Table(CameraStageBaseScreen
 
         this.pad(20f)
         this.defaults().pad(5f)
+
+        this.isVisible = false
     }
 
     /**
-     * Displays the Popup on the screen. Will not open the popup if another one is already open.
+     * Displays the Popup on the screen. If another popup is already open, this one will display after the other has
+     * closed. Use [force] = true if you want to open this popup above the other one anyway.
      */
-    fun open() {
-        if (screen.hasOpenPopups()) return;
+    fun open(force: Boolean = false) {
+        if (force || !screen.hasOpenPopups()) {
+            this.isVisible = true
+        }
+
+        screen.stage.addActor(this)
         pack()
         center(screen.stage)
-        screen.stage.addActor(this)
+
     }
 
     open fun close() {
         remove()
+        if (screen.popups.isNotEmpty()) {
+            screen.popups[0].isVisible = true;
+        }
     }
 
     fun addGoodSizedLabel(text: String, size:Int=18): Cell<Label> {
@@ -57,4 +67,9 @@ open class Popup(val screen: CameraStageBaseScreen): Table(CameraStageBaseScreen
 
     fun addCloseButton() = addButton("Close") { close() }
 }
+
+fun CameraStageBaseScreen.hasOpenPopups(): Boolean = stage.actors.any { it is Popup && it.isVisible }
+
+val CameraStageBaseScreen.popups: List<Popup>
+    get() = stage.actors.filter{ it is Popup }.map{ it as Popup }
 

core/src/com/unciv/ui/worldscreen/TradePopup.kt
@@ -9,17 +9,17 @@ import com.unciv.logic.trade.TradeLogic
 import com.unciv.logic.trade.TradeType
 import com.unciv.models.translations.tr
 import com.unciv.ui.trade.DiplomacyScreen
+import com.unciv.ui.utils.Popup
 import com.unciv.ui.utils.addSeparator
 import com.unciv.ui.utils.toLabel
-import com.unciv.ui.utils.Popup
 import kotlin.math.max
 import kotlin.math.min
 
 class TradePopup(worldScreen: WorldScreen): Popup(worldScreen){
-    init{
-        val viewingCiv = worldScreen.viewingCiv
-        val tradeRequest = viewingCiv.tradeRequests.first()
+    val viewingCiv = worldScreen.viewingCiv
+    val tradeRequest = viewingCiv.tradeRequests.first()
 
+    init{
         val requestingCiv = worldScreen.gameInfo.getCivilization(tradeRequest.requestingCiv)
         val translatedNation = requestingCiv.getTranslatedNation()
         val otherCivLeaderName = "[${translatedNation.leaderName}] of [${translatedNation.getNameTranslation()}]".tr()
@@ -50,7 +50,6 @@ class TradePopup(worldScreen: WorldScreen): Popup(worldScreen){
             val tradeLogic = TradeLogic(viewingCiv, requestingCiv)
             tradeLogic.currentTrade.set(trade)
             tradeLogic.acceptTrade()
-            viewingCiv.tradeRequests.remove(tradeRequest)
             close()
             Popup(worldScreen).apply {
                 add(otherCivLeaderName.toLabel()).colspan(2)
@@ -67,8 +66,6 @@ class TradePopup(worldScreen: WorldScreen): Popup(worldScreen){
             requestingCiv.addNotification("[${viewingCiv.civName}] has accepted your trade request", Color.GOLD)
         }
         addButton("Not this time.".tr()){
-            viewingCiv.tradeRequests.remove(tradeRequest)
-
             val diplomacyManager = requestingCiv.getDiplomacyManager(viewingCiv)
             if(trade.ourOffers.all { it.type==TradeType.Luxury_Resource } && trade.theirOffers.all { it.type==TradeType.Luxury_Resource })
                 diplomacyManager.setFlag(DiplomacyFlags.DeclinedLuxExchange,20) // offer again in 20 turns
@@ -82,7 +79,6 @@ class TradePopup(worldScreen: WorldScreen): Popup(worldScreen){
             worldScreen.shouldUpdate=true
         }
         addButton("How about something else...".tr()){
-            viewingCiv.tradeRequests.remove(tradeRequest)
             close()
 
             val diplomacyScreen= DiplomacyScreen(viewingCiv)
@@ -93,5 +89,10 @@ class TradePopup(worldScreen: WorldScreen): Popup(worldScreen){
             worldScreen.shouldUpdate=true
         }
         open()
+    }
+
+    override fun close() {
+        viewingCiv.tradeRequests.remove(tradeRequest)
+        super.close()
     }
 }

core/src/com/unciv/ui/worldscreen/WorldScreenTopBar.kt
@@ -95,8 +95,8 @@ class WorldScreenTopBar(val worldScreen: WorldScreen) : Table() {
                 .apply { setSize(50f, 50f) }
         menuButton.color = Color.WHITE
         menuButton.onClick {
-            if(worldScreen.stage.actors.none { it is WorldScreenMenuPopup })
                 WorldScreenMenuPopup(worldScreen)
+            if(worldScreen.popups.none { it is WorldScreenMenuPopup })
         }
         menuButton.centerY(this)
         menuButton.x = menuButton.y

core/src/com/unciv/ui/worldscreen/mainmenu/WorldScreenMenuPopup.kt
@@ -23,25 +23,25 @@ class WorldScreenMenuPopup(val worldScreen: WorldScreen) : Popup(worldScreen) {
         val height = 30f
         addSquareButton("Map editor".tr()){
             openMapEditorPopup()
-            remove()
+            close()
         }.size(width,height)
         addSeparator()
 
         addSquareButton("Civilopedia".tr()){
             UncivGame.Current.setScreen(CivilopediaScreen(worldScreen.gameInfo.ruleSet))
-            remove()
+            close()
         }.size(width,height)
         addSeparator()
 
         addSquareButton("Load game".tr()){
             UncivGame.Current.setScreen(LoadGameScreen())
-            remove()
+            close()
         }.size(width,height)
         addSeparator()
 
         addSquareButton("Save game".tr()) {
             UncivGame.Current.setScreen(SaveGameScreen())
-            remove()
+            close()
         }.size(width,height)
         addSeparator()
 
@@ -57,13 +57,13 @@ class WorldScreenMenuPopup(val worldScreen: WorldScreen) : Popup(worldScreen) {
 
         addSquareButton("Options".tr()){
             UncivGame.Current.worldScreen.stage.addActor(WorldScreenOptionsPopup(worldScreen))
-            remove()
+            close()
         }.size(width,height)
         addSeparator()
 
         addSquareButton("Community"){
             WorldScreenCommunityPopup(worldScreen)
-            remove()
+            close()
         }.size(width,height)
         addSeparator()
 
@@ -71,7 +71,7 @@ class WorldScreenMenuPopup(val worldScreen: WorldScreen) : Popup(worldScreen) {
             close()
         }.size(width,height)
 
-        open()
+        open(force = true)
     }
 
 
@@ -159,12 +159,12 @@ class WorldScreenCommunityPopup(val worldScreen: WorldScreen) : Popup(worldScree
     init{
         addButton("Discord"){
             Gdx.net.openURI("https://discord.gg/bjrB4Xw")
-            remove()
+            close()
         }
 
         addButton("Github"){
             Gdx.net.openURI("https://github.com/yairm210/UnCiv")
-            remove()
+            close()
         }
 
         addCloseButton()

core/src/com/unciv/ui/worldscreen/mainmenu/WorldScreenOptionsPopup.kt
@@ -35,44 +35,43 @@ class WorldScreenOptionsPopup(val worldScreen:WorldScreen) : Popup(worldScreen){
         settings.save()
         clear()
 
-        val innerTable = Popup(screen) // cheating, to get the old code to fit inside a Scroll =)
-        innerTable.background = null
+        val innerTable = Table(CameraStageBaseScreen.skin)
 
         innerTable.add("Display options".toLabel(fontSize = 24)).colspan(2).row()
 
         innerTable.add("Show worked tiles".toLabel())
-        innerTable.addButton(if (settings.showWorkedTiles) "Yes".tr() else "No".tr()) {
+        addButton(innerTable, if (settings.showWorkedTiles) "Yes".tr() else "No".tr()) {
             settings.showWorkedTiles= !settings.showWorkedTiles
             update()
         }
 
         innerTable.add("Show resources and improvements".toLabel())
-        innerTable.addButton(if (settings.showResourcesAndImprovements) "Yes".tr() else "No".tr()) {
+        addButton(innerTable, if (settings.showResourcesAndImprovements) "Yes".tr() else "No".tr()) {
             settings.showResourcesAndImprovements = !settings.showResourcesAndImprovements
             update()
         }
 
 
         innerTable.add("Show tutorials".toLabel())
-        innerTable.addButton(if (settings.showTutorials) "Yes".tr() else "No".tr()) {
+        addButton(innerTable, if (settings.showTutorials) "Yes".tr() else "No".tr()) {
             settings.showTutorials = !settings.showTutorials
             update()
         }
 
         innerTable.add("Show minimap".toLabel())
-        innerTable.addButton(if (settings.showMinimap) "Yes".tr() else "No".tr()) {
+        addButton(innerTable, if (settings.showMinimap) "Yes".tr() else "No".tr()) {
             settings.showMinimap = !settings.showMinimap
             update()
         }
 
         innerTable.add("Show pixel units".toLabel())
-        innerTable.addButton(if (settings.showPixelUnits) "Yes".tr() else "No".tr()) {
+        addButton(innerTable, if (settings.showPixelUnits) "Yes".tr() else "No".tr()) {
             settings.showPixelUnits = !settings.showPixelUnits
             update()
         }
 
         innerTable.add("Show pixel improvements".toLabel())
-        innerTable.addButton(if (settings.showPixelImprovements) "Yes".tr() else "No".tr()) {
+        addButton(innerTable, if (settings.showPixelImprovements) "Yes".tr() else "No".tr()) {
             settings.showPixelImprovements = !settings.showPixelImprovements
             update()
         }
@@ -85,7 +84,7 @@ class WorldScreenOptionsPopup(val worldScreen:WorldScreen) : Popup(worldScreen){
 
         // Do not add to template.properties yet please.
         innerTable.add("Continuous rendering\n(HIGHLY EXPERIMENTAL)".toLabel())
-        innerTable.addButton(if (settings.continuousRendering) "Yes".tr() else "No".tr()) {
+        addButton(innerTable, if (settings.continuousRendering) "Yes".tr() else "No".tr()) {
             settings.continuousRendering = !settings.continuousRendering
             Gdx.graphics.isContinuousRendering = settings.continuousRendering
             update()
@@ -95,32 +94,32 @@ class WorldScreenOptionsPopup(val worldScreen:WorldScreen) : Popup(worldScreen){
 
 
         innerTable.add("Check for idle units".toLabel())
-        innerTable.addButton(if (settings.checkForDueUnits) "Yes".tr() else "No".tr()) {
+        addButton(innerTable, if (settings.checkForDueUnits) "Yes".tr() else "No".tr()) {
             settings.checkForDueUnits = !settings.checkForDueUnits
             update()
         }
 
         innerTable.add("Move units with a single tap".toLabel())
-        innerTable.addButton(if (settings.singleTapMove) "Yes".tr() else "No".tr()) {
+        addButton(innerTable, if (settings.singleTapMove) "Yes".tr() else "No".tr()) {
             settings.singleTapMove = !settings.singleTapMove
             update()
         }
 
         innerTable.add("Auto-assign city production".toLabel())
-        innerTable.addButton(if (settings.autoAssignCityProduction) "Yes".tr() else "No".tr()) {
+        addButton(innerTable, if (settings.autoAssignCityProduction) "Yes".tr() else "No".tr()) {
             settings.autoAssignCityProduction = !settings.autoAssignCityProduction
             update()
         }
 
         innerTable.add("Auto-build roads".toLabel())
-        innerTable.addButton(if (settings.autoBuildingRoads) "Yes".tr() else "No".tr()) {
+        addButton(innerTable, if (settings.autoBuildingRoads) "Yes".tr() else "No".tr()) {
             settings.autoBuildingRoads = !settings.autoBuildingRoads
             update()
         }
 
 
         innerTable.add("Enable nuclear weapons".toLabel())
-        innerTable.addButton(if (settings.nuclearWeaponEnabled) "Yes".tr() else "No".tr()) {
+        addButton(innerTable, if (settings.nuclearWeaponEnabled) "Yes".tr() else "No".tr()) {
             settings.nuclearWeaponEnabled = !settings.nuclearWeaponEnabled
             update()
         }
@@ -150,8 +149,14 @@ class WorldScreenOptionsPopup(val worldScreen:WorldScreen) : Popup(worldScreen){
         UncivGame.Current.worldScreen.shouldUpdate = true
     }
 
+    private fun addButton(table: Table, text: String, action: () -> Unit): Cell<TextButton> {
+        val button = TextButton(text.tr(), skin).apply { color = ImageGetter.getBlue() }
+        button.onClick(action)
+        return table.add(button).apply { row() }
+    }
 
-    private fun addSoundEffectsVolumeSlider(innerTable: Popup) {
+
+    private fun addSoundEffectsVolumeSlider(innerTable: Table) {
         innerTable.add("Sound effects volume".tr())
 
         val soundEffectsVolumeSlider = Slider(0f, 1.0f, 0.1f, false, skin)
@@ -166,7 +171,7 @@ class WorldScreenOptionsPopup(val worldScreen:WorldScreen) : Popup(worldScreen){
         innerTable.add(soundEffectsVolumeSlider).row()
     }
 
-    private fun addMusicVolumeSlider(innerTable: Popup) {
+    private fun addMusicVolumeSlider(innerTable: Table) {
         val musicLocation =Gdx.files.local(UncivGame.Current.musicLocation)
         if(musicLocation.exists()) {
             innerTable.add("Music volume".tr())
@@ -208,7 +213,7 @@ class WorldScreenOptionsPopup(val worldScreen:WorldScreen) : Popup(worldScreen){
         }
     }
 
-    private fun addResolutionSelectBox(innerTable: Popup) {
+    private fun addResolutionSelectBox(innerTable: Table) {
         innerTable.add("Resolution".toLabel())
 
         val resolutionSelectBox = SelectBox<String>(skin)
@@ -229,7 +234,7 @@ class WorldScreenOptionsPopup(val worldScreen:WorldScreen) : Popup(worldScreen){
         })
     }
 
-    private fun addTileSetSelectBox(innerTable: Popup) {
+    private fun addTileSetSelectBox(innerTable: Table) {
         innerTable.add("Tileset".toLabel())
 
         val tileSetSelectBox = SelectBox<String>(skin)
@@ -252,7 +257,7 @@ class WorldScreenOptionsPopup(val worldScreen:WorldScreen) : Popup(worldScreen){
         })
     }
 
-    private fun addAutosaveTurnsSelectBox(innerTable: Popup) {
+    private fun addAutosaveTurnsSelectBox(innerTable: Table) {
         innerTable.add("Turns between autosaves".toLabel())
 
         val autosaveTurnsSelectBox = SelectBox<Int>(skin)
@@ -272,7 +277,7 @@ class WorldScreenOptionsPopup(val worldScreen:WorldScreen) : Popup(worldScreen){
         })
     }
 
-    private fun addLanguageSelectBox(innerTable: Popup) {
+    private fun addLanguageSelectBox(innerTable: Table) {
         val languageSelectBox = SelectBox<Language>(skin)
         val languageArray = Array<Language>()
         UncivGame.Current.translations.percentCompleteOfLanguages