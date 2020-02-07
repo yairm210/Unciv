198a97359dbff58398839fe6515001832947aa47
lyrjie <lyrjie@gmail.com>
2020-02-07 15:38:44 +0300

    Turn limit will be derived from the GameSpeed.modifier and base game duration

--- core/src/com/unciv/models/metadata/GameSpeed.kt
+++ core/src/com/unciv/models/metadata/GameSpeed.kt
@@ -1,13 +1,14 @@
 package com.unciv.models.metadata
 
+const val BASE_GAME_DURATION_TURNS = 500f
+
 /** Game speed
  *
  *  @param modifier cost modifier
- *  @param turnLimit turn at which the score victory should be triggered
  * */
-enum class GameSpeed(val modifier: Float, val turnLimit: Int) {
-    Quick(0.67f, 330),
-    Standard(1f, 500),
-    Epic(1.5f, 750),
-    Marathon(3f, 1500);
+enum class GameSpeed(val modifier: Float) {
+    Quick(0.67f),
+    Standard(1f),
+    Epic(1.5f),
+    Marathon(3f);
 }
