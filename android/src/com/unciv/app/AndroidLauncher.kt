0cb1f0ff8d4cb2063906f8869ac96299219d740c
beewall <beewall+git@protonmail.com>
2020-02-07 19:59:45 -0600

    Move mod folder copying into its own function

--- android/src/com/unciv/app/AndroidLauncher.kt
+++ android/src/com/unciv/app/AndroidLauncher.kt
@@ -10,19 +10,7 @@ class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Mod directory in the internal app data (where Gdx.files.local looks)
-        val internalModsDir = File("${filesDir.path}/mods")
-
-        // Mod directory in the shared app data (where the user can see and modify)
-        val externalModsDir = File("${getExternalFilesDir(null)?.path}/mods")
-
-        // Empty out the mods directory so it can be replaced by the external one
-        // Done to ensure it only contains mods in the external dir (so users can delete some)
-        if (internalModsDir.exists()) internalModsDir.deleteRecursively()
-
-        // Copy external mod directory (with data user put in it) to internal (where it can be read)
-        if (!externalModsDir.exists()) externalModsDir.mkdirs()
-        externalModsDir.copyRecursively(internalModsDir)
+		copyMods()
 
         val config = AndroidApplicationConfiguration().apply { useImmersiveMode = true }
         val game = UncivGame(BuildConfig.VERSION_NAME,
@@ -30,4 +18,25 @@ class AndroidLauncher : AndroidApplication() {
                             {this.finish()}
         initialize(game, config)
     }
+
+	/**
+	 * Copies mods from external data directory (where users can access) to the private one (where
+	 * libGDX reads from). Note: deletes all files currently in the private mod directory and
+	 * replaces them with the ones in the external folder!)
+	 */
+	private fun copyMods() {
+		// Mod directory in the internal app data (where Gdx.files.local looks)
+		val internalModsDir = File("${filesDir.path}/mods")
+
+		// Mod directory in the shared app data (where the user can see and modify)
+		val externalModsDir = File("${getExternalFilesDir(null)?.path}/mods")
+
+		// Empty out the mods directory so it can be replaced by the external one
+		// Done to ensure it only contains mods in the external dir (so users can delete some)
+		if (internalModsDir.exists()) internalModsDir.deleteRecursively()
+
+		// Copy external mod directory (with data user put in it) to internal (where it can be read)
+		if (!externalModsDir.exists()) externalModsDir.mkdirs()
+		externalModsDir.copyRecursively(internalModsDir)
+	}
 }
