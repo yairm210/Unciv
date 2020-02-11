9ee0c94bf5c8f635655ad09586d605416cd2ee98
rayray61 <54810477+rayray61@users.noreply.github.com>
2020-02-11 16:31:25 +0300

    Update Turkish.properties (#1907)
    * Cleaned up Unit Actions
    
    * Resolved #1902 - buildings requiring worked resources can be built in cities that are built on that resource
    
    * Legalism grants enqueued buildings (#1906)
    
    * Update Turkish.properties
    
    Co-authored-by: Yair Morgenstern <yairm210@hotmail.com>
    Co-authored-by: Federico Luongo <fluo392@gmail.com>

--- core/src/com/unciv/models/ruleset/Building.kt
+++ core/src/com/unciv/models/ruleset/Building.kt
@@ -311,8 +311,8 @@ class Building : NamedStats(), IConstruction{
                     .any {
                         it.resource != null
                                 && requiredNearbyImprovedResources!!.contains(it.resource!!)
-                                && (it.getTileResource().improvement == it.improvement || it.improvement in Constants.greatImprovements)
                                 && it.getOwner() == civInfo
+                                && (it.getTileResource().improvement == it.improvement || it.improvement in Constants.greatImprovements || it.isCityCenter())
                     }
             if (!containsResourceWithImprovement) return "Nearby $requiredNearbyImprovedResources required"
         }
