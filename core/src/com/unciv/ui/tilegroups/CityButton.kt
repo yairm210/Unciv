ff115d688d8dc44d318f06c7256314d1d1227f60
JackRainy <JackRainy@users.noreply.github.com>
2020-02-22 23:01:14 +0200

    Better naming of variable

--- core/src/com/unciv/ui/tilegroups/CityButton.kt
+++ core/src/com/unciv/ui/tilegroups/CityButton.kt
@@ -70,14 +70,14 @@ class CityButton(val city: CityInfo, internal val tileGroup: WorldTileGroup, ski
             // clicking swings the button a little down to allow selection of units there.
             // this also allows to target selected units to move to the city tile from elsewhere.
             if (isButtonMoved) {
-                val currentCiv = UncivGame.Current.worldScreen.viewingCiv
+                val viewingCiv = UncivGame.Current.worldScreen.viewingCiv
                 // second tap on the button will go to the city screen
                 // if this city belongs to you
                 if (UncivGame.Current.viewEntireMapForDebug || belongsToViewingCiv()) {
                     UncivGame.Current.setScreen(CityScreen(city))
-                } else if (currentCiv.knows(city.civInfo)) {
+                } else if (viewingCiv.knows(city.civInfo)) {
                     // If city doesn't belong to you, go directly to its owner's diplomacy screen.
-                    val screen = DiplomacyScreen(currentCiv)
+                    val screen = DiplomacyScreen(viewingCiv)
                     screen.updateRightSide(city.civInfo)
                     UncivGame.Current.setScreen(screen)
                 }
