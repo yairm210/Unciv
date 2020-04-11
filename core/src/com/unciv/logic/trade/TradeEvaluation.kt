cc1ae034d39d4c57f9ea772c318e851b0aeaffb2
Yair Morgenstern <yairm210@hotmail.com>
2020-04-11 22:26:12 +0300

    Fixed "Trade is no longer valid" problem

--- core/src/com/unciv/logic/trade/TradeEvaluation.kt
+++ core/src/com/unciv/logic/trade/TradeEvaluation.kt
@@ -25,7 +25,7 @@ class TradeEvaluation{
 
         fun hasResource(tradeOffer: TradeOffer): Boolean {
             val resourcesByName = offerer.getCivResourcesByName()
-            return resourcesByName.containsKey(tradeOffer.name) && resourcesByName[tradeOffer.name]!! >= tradeOffer.amount
+            return resourcesByName.containsKey(tradeOffer.name) && resourcesByName[tradeOffer.name]!! >= 0
         }
 
         when(tradeOffer.type){
