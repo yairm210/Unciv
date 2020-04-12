736f2a6f766b7abbd2dbdab3b61e64164c71a7e1
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2020-04-12 16:29:17 +0200

    Keyboard support in Pickers and Popups: YesNoPopup

--- core/src/com/unciv/ui/utils/YesNoPopup.kt
+++ core/src/com/unciv/ui/utils/YesNoPopup.kt
@@ -1,14 +1,12 @@
 package com.unciv.ui.utils
 
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.unciv.UncivGame
-import com.unciv.models.translations.tr
 
 class YesNoPopup(question:String, action:()->Unit,
-                 screen: CameraStageBaseScreen = UncivGame.Current.worldScreen, restoredefault:()->Unit = {}) : Popup(screen){
+                 screen: CameraStageBaseScreen = UncivGame.Current.worldScreen, restoreDefault:()->Unit = {}) : Popup(screen){
     init{
         add(question.toLabel()).colspan(2).row()
-        add(TextButton("No".tr(), skin).onClick { close(); restoredefault() })
-        add(TextButton("Yes".tr(), skin).onClick { close(); action() })
+        addInlineButton("No") { close(); restoreDefault() }
+        addInlineButton("Yes") { close(); action() }
     }
 }
