096eb7935abc6d90fc4d06b262d491008ae24f1b
Yair Morgenstern <yairm210@hotmail.com>
2020-04-14 15:17:40 +0300

    Welp forgot I was on debug mode I should probably add in a pre-commit check

--- core/src/com/unciv/logic/automation/UnitAutomation.kt
+++ core/src/com/unciv/logic/automation/UnitAutomation.kt
@@ -291,7 +291,7 @@ object UnitAutomation {
                     // move into position far away enough that the bombard doesn't hurt
                     if (tileToMoveTo != null)
                         unit.movement.headTowards(tileToMoveTo)
-                } else {
+                } else { // unit range <= 2
                     // calculate total damage of units in surrounding 4-spaces from enemy city (so we can attack a city from 2 directions at once)
                     val militaryUnitsAroundEnemyCity =
                             closestReachableEnemyCity.getTilesInDistance(3)
