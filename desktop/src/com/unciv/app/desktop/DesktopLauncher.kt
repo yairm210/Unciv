ec6b5fe65416977b4a1c2d5f44bec7c757c702c0
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2020-04-17 21:26:10 +0200

    Optimization: Reduce need for Texture swapping

--- desktop/src/com/unciv/app/desktop/DesktopLauncher.kt
+++ desktop/src/com/unciv/app/desktop/DesktopLauncher.kt
@@ -11,6 +11,7 @@ import com.badlogic.gdx.tools.texturepacker.TexturePacker
 import com.unciv.UncivGame
 import com.unciv.models.translations.tr
 import java.io.File
+import java.nio.charset.Charset
 import java.util.*
 import kotlin.concurrent.timer
 import kotlin.system.exitProcess
@@ -43,49 +44,51 @@ internal object DesktopLauncher {
     private fun packImages() {
         val startTime = System.currentTimeMillis()
 
+        // see ../Images/pack.json for the most important settings
         val settings = TexturePacker.Settings()
-        // Apparently some chipsets, like NVIDIA Tegra 3 graphics chipset (used in Asus TF700T tablet),
-        // don't support non-power-of-two texture sizes - kudos @yuroller!
-        // https://github.com/yairm210/UnCiv/issues/1340
-        settings.maxWidth = 2048
-        settings.maxHeight = 2048
-        settings.combineSubdirectories = true
-        settings.pot = true
-        settings.fast = true
 
-        // This is so they don't look all pixelated
-        settings.filterMag = Texture.TextureFilter.MipMapLinearLinear
-        settings.filterMin = Texture.TextureFilter.MipMapLinearLinear
-
-        if (File("../Images").exists()) // So we don't run this from within a fat JAR
-            packImagesIfOutdated(settings, "../Images", ".", "game")
+        val imageDir = "..${File.separator}Images"
+        if (File(imageDir).exists())   // So we don't run this from within a fat JAR
+            if (packImagesIfOutdated(settings, imageDir, ".", "game"))
+                if (File("$imageDir${File.separator}H").exists()) {
+                    val atlasFile = "game.atlas"
+                    val atlasOut = mutableListOf<String>()
+                    File(atlasFile).forEachLine {
+                        atlasOut += if (it.length > 2 && it[1] in "/${File.separator}")
+                            it.drop(2) else it
+                    }
+                    File(atlasFile).writeText(atlasOut.joinToString("\n"), Charset.forName ("UTF-8"))
+                }
 
         // pack for mods as well
         val modDirectory = File("mods")
         if(modDirectory.exists()) {
+            settings.maxWidth = 1024
             for (mod in modDirectory.listFiles()!!){
-                if (!mod.isHidden && File(mod.path + "/Images").exists())
-                    packImagesIfOutdated(settings, mod.path + "/Images", mod.path, "game")
+                val modImageDir = "${mod.path}${File.separator}Images"
+                if (!mod.isHidden && File(modImageDir).exists())
+                    packImagesIfOutdated(settings, modImageDir, mod.path, "game")
             }
         }
 
         val texturePackingTime = System.currentTimeMillis() - startTime
-        println("Packing textures - "+texturePackingTime+"ms")
+        println("Packing textures - ${texturePackingTime}ms")
     }
 
-    private fun packImagesIfOutdated (settings: TexturePacker.Settings, input: String, output: String, packFileName: String) {
+    private fun packImagesIfOutdated (settings: TexturePacker.Settings, input: String, output: String, packFileName: String): Boolean {
         fun File.listTree(): Sequence<File> = when {
-                this.isFile -> sequenceOf(this)
-                this.isDirectory -> this.listFiles().asSequence().flatMap { it.listTree() }
-                else -> sequenceOf()
-            }
+            this.isFile -> sequenceOf(this)
+            this.isDirectory -> this.listFiles().asSequence().flatMap { it.listTree() }
+            else -> sequenceOf()
+        }
 
         val atlasFile = File("$output${File.separator}$packFileName.atlas")
         if (atlasFile.exists() && File("$output${File.separator}$packFileName.png").exists()) {
             val atlasModTime = atlasFile.lastModified()
-            if (!File(input).listTree().any { it.extension in listOf("png","jpg","jpeg") && it.lastModified() > atlasModTime }) return
+            if (!File(input).listTree().any { it.extension in listOf("png","jpg","jpeg","json") && it.lastModified() > atlasModTime }) return false
         }
         TexturePacker.process(settings, input, output, packFileName )
+        return true
     }
 
     private fun tryActivateDiscord(game: UncivGame) {
