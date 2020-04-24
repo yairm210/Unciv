fea7b77c74517604a99164869e06c86781c5df07
git-is-crap <63000004+SomeTroglodyte@users.noreply.github.com>
2020-04-24 12:14:24 +0200

    SAF-based Import/Export: Start

--- core/src/com/unciv/MainMenuScreen.kt
+++ core/src/com/unciv/MainMenuScreen.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.scenes.scene2d.Touchable
 import com.badlogic.gdx.scenes.scene2d.actions.Actions
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.utils.Align
 import com.unciv.logic.GameSaver
