12f4d4d9b8de1da6a3e6c37c7ffa19532f4a3f23
Yair Morgenstern <yairm210@hotmail.com>
2020-06-16 20:24:11 +0300

    Resolved #2735 - Diplomatic "Friends with friend/enemy" modifiers are recalculated every turn

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,8 +3,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.3.71"
     const val appName = "Unciv"
-    const val appCodeNumber = 438
-    const val appVersion = "3.9.3"
+    const val appCodeNumber = 439
+    const val appVersion = "3.9.3-patch1"
 
     const val gdxVersion = "1.9.10"
     const val roboVMVersion = "2.3.1"
