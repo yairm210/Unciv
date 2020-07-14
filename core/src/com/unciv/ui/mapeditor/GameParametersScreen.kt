a0daa6be4ca29a061187d2c1b424e601345e8198
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2020-07-14 17:17:37 +0200

    Add mod compatibility for extended map editor (#2840)
    * Add mod compatibility for extended map editor
    
    * fix crashes when loading scenario with mods enabled
    
    * update imagegetter ruleset when loading scenario
    
    * reload ruleset on new scenrio
    
    * Fixed crash when units/improvements from the missing mods present in the map editor.
    
    * removeMissingModReferences - better name

--- core/src/com/unciv/ui/mapeditor/GameParametersScreen.kt
+++ core/src/com/unciv/ui/mapeditor/GameParametersScreen.kt
@@ -3,6 +3,7 @@ package com.unciv.ui.mapeditor
 import com.unciv.UncivGame
 import com.unciv.logic.map.Scenario
 import com.unciv.models.ruleset.Ruleset
+import com.unciv.models.ruleset.RulesetCache
 import com.unciv.ui.newgamescreen.GameOptionsTable
 import com.unciv.ui.newgamescreen.GameSetupInfo
 import com.unciv.ui.newgamescreen.PlayerPickerTable
@@ -17,10 +18,10 @@ import com.unciv.ui.utils.*
  * @param [mapEditorScreen] previous screen from map editor.
  */
 class GameParametersScreen(var mapEditorScreen: MapEditorScreen): IPreviousScreen, PickerScreen() {
-    override var gameSetupInfo: GameSetupInfo = mapEditorScreen.gameSetupInfo
-    override var ruleset: Ruleset = mapEditorScreen.ruleset
-    var playerPickerTable = PlayerPickerTable(this, this.gameSetupInfo.gameParameters)
-    var gameOptionsTable = GameOptionsTable(mapEditorScreen) { desiredCiv: String -> playerPickerTable.update(desiredCiv) }
+    override var gameSetupInfo = mapEditorScreen.gameSetupInfo.clone()
+    override var ruleset = RulesetCache.getComplexRuleset(gameSetupInfo.gameParameters)
+    var playerPickerTable = PlayerPickerTable(this, gameSetupInfo.gameParameters)
+    var gameOptionsTable = GameOptionsTable(this) { desiredCiv: String -> playerPickerTable.update(desiredCiv) }
 
 
     init {
@@ -34,7 +35,8 @@ class GameParametersScreen(var mapEditorScreen: MapEditorScreen): IPreviousScree
         rightSideButton.onClick {
             mapEditorScreen.gameSetupInfo = gameSetupInfo
             mapEditorScreen.scenario = Scenario(mapEditorScreen.tileMap, mapEditorScreen.gameSetupInfo.gameParameters)
-            mapEditorScreen.ruleset = ruleset //TODO: figure out whether it is necessary
+            mapEditorScreen.ruleset.clear()
+            mapEditorScreen.ruleset.add(ruleset)
             mapEditorScreen.tileEditorOptions.update()
             mapEditorScreen.mapHolder.updateTileGroups()
             UncivGame.Current.setScreen(mapEditorScreen)
