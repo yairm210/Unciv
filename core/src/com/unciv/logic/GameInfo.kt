7d360e5fa1e7f61fbb5ba10c94078d9096ad1226
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2020-07-22 09:43:03 +0200

    Skip spectator turn in multiplayer games (#2873)

--- core/src/com/unciv/logic/GameInfo.kt
+++ core/src/com/unciv/logic/GameInfo.kt
@@ -93,9 +93,9 @@ class GameInfo {
         while (thisPlayer.playerType == PlayerType.AI
             || turns < UncivGame.Current.simulateUntilTurnForDebug
                 || (turns < simulateMaxTurns && simulateUntilWin)
-                // For multiplayer, if there are 3+ players and one is defeated,
+                // For multiplayer, if there are 3+ players and one is defeated or spectator,
                 // we'll want to skip over their turn
-                || (thisPlayer.isDefeated() && gameParameters.isOnlineMultiplayer)
+                || ((thisPlayer.isDefeated() || thisPlayer.isSpectator()) && gameParameters.isOnlineMultiplayer)
         ) {
             if (!thisPlayer.isDefeated() || thisPlayer.isBarbarian()) {
                 NextTurnAutomation.automateCivMoves(thisPlayer)
