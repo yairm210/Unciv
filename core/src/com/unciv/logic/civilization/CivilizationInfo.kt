236c7a1c15af24a395374f383f0e14b185222076
Yair Morgenstern <yairm210@hotmail.com>
2020-10-07 23:05:46 +0300

    Revert "When checking for nation uniques, it also checks the tech uniques (#3240)"
    This reverts commit 31226c97e86d9a2bc956461a5fdb29d90c9bd47d.

--- core/src/com/unciv/logic/civilization/CivilizationInfo.kt
+++ core/src/com/unciv/logic/civilization/CivilizationInfo.kt
@@ -192,8 +192,7 @@ class CivilizationInfo {
     fun getMatchingUniques(uniqueTemplate: String): Sequence<Unique> {
         return nation.uniqueObjects.asSequence().filter { it.placeholderText == uniqueTemplate } +
                 cities.asSequence().flatMap { it.cityConstructions.builtBuildingUniqueMap.getUniques(uniqueTemplate).asSequence() } +
-                policies.policyUniques.getUniques(uniqueTemplate) +
-                tech.getTechUniques().asSequence().map { Unique(it) }.filter { it.placeholderText == uniqueTemplate }
+                policies.policyUniques.getUniques(uniqueTemplate)
     }
 
     //region Units
