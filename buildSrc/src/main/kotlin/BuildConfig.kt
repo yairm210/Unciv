8292b848b7e02f463f8c12569922c926d67f6f41
Yair Morgenstern <yairm210@hotmail.com>
2020-10-11 22:21:36 +0300

    3.11.3-patch1

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,7 +3,7 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.3.71"
     const val appName = "Unciv"
-    const val appCodeNumber = 484
+    const val appCodeNumber = 485
     const val appVersion = "3.11.3-patch1"
 
     const val gdxVersion = "1.9.10"
