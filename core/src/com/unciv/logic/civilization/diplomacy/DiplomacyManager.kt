114effe1aed701eacb7148fc3d85190e75fb9dbd
Yair Morgenstern <yairm210@hotmail.com>
2020-11-23 20:01:58 +0200

    Resolved #3360 - notify peace treaty to all commmon known civs

--- core/src/com/unciv/logic/civilization/diplomacy/DiplomacyManager.kt
+++ core/src/com/unciv/logic/civilization/diplomacy/DiplomacyManager.kt
@@ -526,14 +526,12 @@ class DiplomacyManager() {
         diplomaticStatus = DiplomaticStatus.Peace
         otherCivDiplomacy().diplomaticStatus = DiplomaticStatus.Peace
 
-        if (otherCiv().isAtWarWith(civInfo)) {
-            for (civ in getCommonKnownCivs()) {
-                civ.addNotification(
-                        "[${civInfo.civName}] and [${otherCiv().civName}] have signed the Peace Treaty!",
-                        null,
-                        Color.WHITE
-                )
-            }
+        for (civ in getCommonKnownCivs()) {
+            civ.addNotification(
+                    "[${civInfo.civName}] and [${otherCiv().civName}] have signed the Peace Treaty!",
+                    null,
+                    Color.WHITE
+            )
         }
 
         val otherCiv = otherCiv()
