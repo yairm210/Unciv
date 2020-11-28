87fa2bfb88212b0b30e14fdcd3e0deb81bd2e1f8
9kgsofrice <edwhitham@gmail.com>
2020-11-28 02:30:36 +0000

    removed duplicate code

--- core/src/com/unciv/models/ruleset/unit/BaseUnit.kt
+++ core/src/com/unciv/models/ruleset/unit/BaseUnit.kt
@@ -131,10 +131,6 @@ class BaseUnit : INamed, IConstruction {
                         && !construction.cityInfo.civInfo.hasResource(it.params[0])
                         && !construction.cityInfo.civInfo.gameInfo.gameParameters.godMode)))})
             return "Should not be displayed"
-        /*if (uniqueObjects.any { it.placeholderText == "Not displayed as an available construction without []"
-                        && (!construction.cityInfo.civInfo.hasResource(it.params[0])
-                        && !construction.cityInfo.civInfo.gameInfo.gameParameters.godMode)})
-            return "Should not be displayed"*/
         val civRejectionReason = getRejectionReason(construction.cityInfo.civInfo)
         if (civRejectionReason != "") return civRejectionReason
         return ""
