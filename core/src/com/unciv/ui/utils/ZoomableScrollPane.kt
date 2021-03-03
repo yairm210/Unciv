9a7e63789160cf7c365fb8c45eb42cf98ebd8ea4
GGGuenni <leonard@eynck.de>
2021-03-03 11:54:21 +0100

    World Wrap Part 1 (#3604)
    * Adding fake infinite scroll functionality
    
    * improve scroll jump
    
    * really smooth jump now
    
    * World wrap has functionality now
    
    This is not compileable on its own!
    Needs branches WorldWrap-v1 and WorldWrap-v2 to be mergeable
    
    * Mirror tiles rendering
    
    * changed to run v2-alternative code
    
    * Use generic type
    
    * default worldWrap to false
    
    * forward continousScrollingX as parameter
    
    * added unit movement button for mirror tiles
    
    * Add clickable tile layer
    
    * Smooth wrap jump
    
    * code a bit better to read
    
    * Removed unnecessary overrides and catches
    
    * mirrorTiles are stored in one HashMap now
    
    instead of two seperate ones

--- core/src/com/unciv/ui/utils/ZoomableScrollPane.kt
+++ core/src/com/unciv/ui/utils/ZoomableScrollPane.kt
@@ -1,13 +1,17 @@
 package com.unciv.ui.utils
 
+import com.badlogic.gdx.scenes.scene2d.Event
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
 import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener
+import kotlin.math.abs
 import kotlin.math.sqrt
 
 
 open class ZoomableScrollPane: ScrollPane(null) {
+    var continousScrollingX = false
+
     init{
         // Remove the existing inputListener
         // which defines that mouse scroll = vertical movement
@@ -44,5 +48,31 @@ open class ZoomableScrollPane: ScrollPane(null) {
                 zoom(scale)
             }
         })
+    }
+
+    override fun getFlickScrollListener(): ActorGestureListener {
+        //This is mostly just Java code from the ScrollPane class reimplemented as Kotlin code
+        //Had to change a few things to bypass private access modifiers
+        return object : ActorGestureListener() {
+            override fun pan(event: InputEvent, x: Float, y: Float, deltaX: Float, deltaY: Float) {
+                setScrollbarsVisible(true)
+                scrollX -= deltaX
+                scrollY += deltaY
+
+                //this is the new feature to fake an infinite scroll
+                when {
+                    continousScrollingX && scrollPercentX >= 1 && deltaX < 0 -> {
+                        scrollPercentX = 0f
+                    }
+                    continousScrollingX && scrollPercentX <= 0 && deltaX > 0-> {
+                        scrollPercentX = 1f
+                    }
+                }
+
+                //clamp() call is missing here but it doesn't seem to make any big difference in this case
+
+                if ((isScrollX && deltaX != 0f || isScrollY && deltaY != 0f)) cancelTouchFocus()
+            }
+        }
     }
 }
