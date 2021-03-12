870bf9fc320c513b93578d4d267a7eaa92cba2c7
Yair Morgenstern <yairm210@hotmail.com>
2021-03-12 12:26:00 +0200

    3.13.7-patch1

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,8 +3,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.4.30"
     const val appName = "Unciv"
-    const val appCodeNumber = 543
-    const val appVersion = "3.13.7"
+    const val appCodeNumber = 544
+    const val appVersion = "3.13.7-patch1"
 
     const val gdxVersion = "1.9.14"
     const val roboVMVersion = "2.3.1"
