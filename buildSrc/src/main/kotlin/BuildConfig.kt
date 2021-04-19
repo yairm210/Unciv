0f23bed790dde17b7b2f7e2f724d227b541bed16
Yair Morgenstern <yairm210@hotmail.com>
2021-04-19 21:51:38 +0300

    Version increment

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -6,7 +6,7 @@ object BuildConfig {
     const val appCodeNumber = 557
     const val appVersion = "3.14.2"
 
-    const val gdxVersion = "1.9.14"
+    const val gdxVersion = "1.10.0"
     const val roboVMVersion = "2.3.1"
     const val ashleyVersion = "1.7.0"
     const val aiVersion = "1.8.0"
