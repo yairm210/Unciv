4545cdbd3a9e7447ef0ab9852ea67745cfc28696
GGGuenni <leonard@eynck.de>
2021-04-23 11:17:40 +0200

    Added fogOfWarColor and unexploredTileColor

--- core/src/com/unciv/models/tilesets/TileSetConfig.kt
+++ core/src/com/unciv/models/tilesets/TileSetConfig.kt
@@ -1,7 +1,11 @@
 package com.unciv.models.tilesets
 
+import com.badlogic.gdx.graphics.Color
+
 class TileSetConfig {
     var useColorAsBaseTerrain = true
+    var unexploredTileColor: Color = Color.DARK_GRAY
+    var fogOfWarColor: Color = Color.BLACK
     var ruleVariants: HashMap<String, Array<String>> = HashMap()
 
     fun updateConfig(other: TileSetConfig){
