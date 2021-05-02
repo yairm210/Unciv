edea3530438b3145f01925ecf13ba96deb5d62b2
Yair Morgenstern <yairm210@hotmail.com>
2021-05-02 22:14:56 +0300

    resolved #3869 but no one really cares

--- core/src/com/unciv/ui/worldscreen/mainmenu/OptionsPopup.kt
+++ core/src/com/unciv/ui/worldscreen/mainmenu/OptionsPopup.kt
@@ -128,7 +128,8 @@ class OptionsPopup(val previousScreen:CameraStageBaseScreen) : Popup(previousScr
 
         addHeader("Other options")
 
-        addYesNoRow("Show experimental world wrap for maps\nHIGHLY EXPERIMENTAL - YOU HAVE BEEN WARNED!",
+        addYesNoRow("Show experimental world wrap for maps\n"
+                +"HIGHLY EXPERIMENTAL - YOU HAVE BEEN WARNED!".tr(),
                 settings.showExperimentalWorldWrap)
         { settings.showExperimentalWorldWrap = it }
 
