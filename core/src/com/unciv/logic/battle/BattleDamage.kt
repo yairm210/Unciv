28475a6d287922ec12a3026f482ffd9212a8ac91
xlenstra <xanderlenstra@gmail.com>
2021-06-12 02:11:44 +0200

    Updated Honor policies to G&K

--- core/src/com/unciv/logic/battle/BattleDamage.kt
+++ core/src/com/unciv/logic/battle/BattleDamage.kt
@@ -75,12 +75,17 @@ object BattleDamage {
                 }
             }
 
-            if (civInfo.hasUnique("+15% combat strength for melee units which have another military unit in an adjacent tile")
-                && combatant.isMelee()
-                && combatant.getTile().neighbors.flatMap { it.getUnits() }
-                    .any { it.civInfo == civInfo && !it.type.isCivilian() && !it.type.isAirUnit() }
-            )
-                modifiers["Discipline"] = 15
+            var adjacentUnitBonus = 0;
+            for (unique in civInfo.getMatchingUniques("+[]% combat strength for [] units which have another [] unit in an adjacent tile")) {
+                if (combatant.matchesCategory(unique.params[1])
+                    && combatant.getTile().neighbors.flatMap { it.getUnits() }
+                    .any { it.civInfo == civInfo && it.matchesFilter(unique.params[2]) } 
+                ) {
+                    adjacentUnitBonus += unique.params[0].toInt()
+                }
+            }
+            if (adjacentUnitBonus != 0)
+                modifiers["Adjacent unit"] = adjacentUnitBonus
 
             val civResources = civInfo.getCivResourcesByName()
             for (resource in combatant.unit.baseUnit.getResourceRequirements().keys)
@@ -109,8 +114,10 @@ object BattleDamage {
         if (enemy.getCivInfo().isBarbarian()) {
             modifiers["Difficulty"] =
                 (civInfo.gameInfo.getDifficulty().barbarianBonus * 100).toInt()
-            if (civInfo.hasUnique("+25% bonus vs Barbarians"))
-                modifiers["vs Barbarians"] = 25
+            val barbarianBonus = civInfo.getMatchingUniques("+[]% bonus vs Barbarians")
+                .map { it.params[0].toInt() }.sum()
+            if (barbarianBonus > 0)
+                modifiers["vs Barbarians"] = barbarianBonus 
         }
 
         return modifiers
--- core/src/com/unciv/logic/map/MapUnit.kt
+++ core/src/com/unciv/logic/map/MapUnit.kt
@@ -319,11 +319,12 @@ class MapUnit {
 
     fun getCostOfUpgrade(): Int {
         val unitToUpgradeTo = getUnitToUpgradeTo()
-        var goldCostOfUpgrade = (unitToUpgradeTo.cost - baseUnit().cost) * 2 + 10
-        for (unique in civInfo.getMatchingUniques("Gold cost of upgrading military units reduced by 33%"))
-            goldCostOfUpgrade = (goldCostOfUpgrade * 0.66f).toInt()
+        var goldCostOfUpgrade = (unitToUpgradeTo.cost - baseUnit().cost) * 2f + 10f
+        for (unique in civInfo.getMatchingUniques("Gold cost of upgrading military units reduced by []%")) {
+            goldCostOfUpgrade *= (1 - unique.params[0].toFloat() / 100f)
+        }
         if (goldCostOfUpgrade < 0) return 0 // For instance, Landsknecht costs less than Spearman, so upgrading would cost negative gold
-        return goldCostOfUpgrade
+        return goldCostOfUpgrade.toInt()
     }
 
 
