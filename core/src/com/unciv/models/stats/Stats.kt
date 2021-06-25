yairm210/Unciv

commit 823e6ffc2a4d14c04a661e2e0f8c2253c0d0844a

Faster 'false' results for isStats, as proposed by @SomeTroglodyte  in #4259

core/src/com/unciv/models/stats/Stats.kt
@@ -118,7 +118,10 @@ open class Stats() {
         private val statRegexPattern = "([+-])(\\d+) ($allStatNames)"
         private val statRegex = Regex(statRegexPattern)
         private val entireStringRegexPattern = Regex("$statRegexPattern(, $statRegexPattern)*")
-        fun isStats(string:String): Boolean = entireStringRegexPattern.matches(string)
+        fun isStats(string:String): Boolean {
+            if (string[0] != '+' && string[0] != '-') return false // very quick negative check before the heavy Regex
+            return entireStringRegexPattern.matches(string)
+        }
         fun parse(string:String):Stats{
             val toReturn = Stats()
             val statsWithBonuses = string.split(", ")

commit 70d07ecaa8910480efb6494f10581cb7ac32c245

Fixed main menu crash
Fixed a critical bug where the game would crash when any option was
picked in the main menu.

The bug was introduced by 823e6ffc2a4d14c04a661e2e0f8c2253c0d0844a.

core/src/com/unciv/models/stats/Stats.kt
@@ -119,7 +119,7 @@ open class Stats() {
         private val statRegex = Regex(statRegexPattern)
         private val entireStringRegexPattern = Regex("$statRegexPattern(, $statRegexPattern)*")
         fun isStats(string:String): Boolean {
-            if (string[0] != '+' && string[0] != '-') return false // very quick negative check before the heavy Regex
+            if (string.isEmpty() || string[0] !in "+-") return false // very quick negative check before the heavy Regex
             return entireStringRegexPattern.matches(string)
         }
         fun parse(string:String):Stats{