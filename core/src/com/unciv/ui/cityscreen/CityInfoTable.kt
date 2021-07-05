9c31843fb884c5d5c54f0b4d9d0f4a67f05dd492
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2021-07-05 23:23:38 +0200

    Spruced up Civilopedia - phase 5 - buildings

--- core/src/com/unciv/ui/cityscreen/CityInfoTable.kt
+++ core/src/com/unciv/ui/cityscreen/CityInfoTable.kt
@@ -91,7 +91,7 @@ class CityInfoTable(private val cityScreen: CityScreen) : Table(CameraStageBaseS
             if (wonderDetailsTable.hasChildren())
                 wonderDetailsTable.clear()
             else {
-                val detailsString = building.getDescription(true,
+                val detailsString = building.getDescription(
                         cityScreen.city, cityScreen.city.civInfo.gameInfo.ruleSet)
                 wonderDetailsTable.add(detailsString.toLabel().apply { wrap = true })
                         .width(cityScreen.stage.width / 4 - 2 * pad).row() // when you set wrap, then you need to manually set the size of the label
