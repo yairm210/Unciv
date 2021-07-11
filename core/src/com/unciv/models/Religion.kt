3828ca518893175ac0f832e12cfa6789a9facd0c
xlenstra <xanderlenstra@gmail.com>
2021-07-11 22:06:09 +0200

    Fixed crashes on loading save games with religion

--- core/src/com/unciv/models/Religion.kt
+++ core/src/com/unciv/models/Religion.kt
@@ -1,26 +1,42 @@
 package com.unciv.models
 
+import com.unciv.logic.GameInfo
 import com.unciv.logic.civilization.CivilizationInfo
 import com.unciv.models.ruleset.Unique
 import com.unciv.models.stats.INamed
 
 /** Data object for Religions */
-class Religion(override var name: String, val foundingCiv: CivilizationInfo) : INamed {
+class Religion() : INamed {
     
     var pantheonBeliefs: HashSet<String> = hashSetOf()
     var founderBeliefs: HashSet<String> = hashSetOf()
     var followerBeliefs: HashSet<String> = hashSetOf()
-
+    override lateinit var name: String
+    lateinit var foundingCivName: String
+    
+    @Transient
+    lateinit var gameInfo: GameInfo
+    
+    constructor(name: String, gameInfo: GameInfo, foundingCivName: String) : this() {
+        this.name = name
+        this.foundingCivName = foundingCivName
+        this.gameInfo = gameInfo
+    }
+    
     fun clone(): Religion {
-        val toReturn = Religion(name, foundingCiv)
+        val toReturn = Religion(name, gameInfo, foundingCivName)
         toReturn.pantheonBeliefs.addAll(pantheonBeliefs)
         toReturn.founderBeliefs.addAll(founderBeliefs)
         toReturn.followerBeliefs.addAll(followerBeliefs)
         return toReturn
     }
     
+    fun setTransients(gameInfo: GameInfo) {
+        this.gameInfo = gameInfo
+    }
+    
     private fun getUniquesOfBeliefs(beliefs: HashSet<String>): Sequence<Unique> {
-        val rulesetBeliefs = foundingCiv.gameInfo.ruleSet.beliefs
+        val rulesetBeliefs = gameInfo.ruleSet.beliefs
         return beliefs.mapNotNull {
             if (it !in rulesetBeliefs) null
             else rulesetBeliefs[it]!!.uniqueObjects
