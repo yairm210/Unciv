482a1260ca98aa8b4d4d9257639a88243d15d75c
Xander Lenstra <71121390+xlenstra@users.noreply.github.com>
2021-07-15 09:36:59 +0200

    Fixed bug which leads to crashes for almost all users (#4519)

--- core/src/com/unciv/ui/trade/DiplomacyScreen.kt
+++ core/src/com/unciv/ui/trade/DiplomacyScreen.kt
@@ -56,7 +56,7 @@ class DiplomacyScreen(val viewingCiv:CivilizationInfo):CameraStageBaseScreen() {
     private fun updateLeftSideTable() {
         leftSideTable.clear()
         for (civ in viewingCiv.gameInfo.civilizations
-                .filterNot { it.isDefeated() || it == viewingCiv || it.isBarbarian() || it.isSpectator() || viewingCiv.knows(it) }) {
+                .filterNot { it.isDefeated() || it == viewingCiv || it.isBarbarian() || it.isSpectator() || !viewingCiv.knows(it) }) {
 
             val civIndicator = ImageGetter.getNationIndicator(civ.nation, 100f)
 
