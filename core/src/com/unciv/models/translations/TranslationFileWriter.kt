fdf165918f43d419fd9fbf1edbe6be225efef518
xlenstra <xanderlenstra@gmail.com>
2021-07-20 11:42:02 +0200

    Update translationFileWriter to include strings from new json files

--- core/src/com/unciv/models/translations/TranslationFileWriter.kt
+++ core/src/com/unciv/models/translations/TranslationFileWriter.kt
@@ -280,6 +280,10 @@ object TranslationFileWriter {
             }
 
             fun serializeElement(element: Any) {
+                if (element is String) {
+                    submitString(element)
+                    return
+                }
                 val allFields = (element.javaClass.declaredFields + element.javaClass.fields
                         + element.javaClass.superclass.declaredFields) // This is so the main PolicyBranch, which inherits from Policy, will recognize its Uniques and have them translated
                         .filter {
@@ -333,11 +337,14 @@ object TranslationFileWriter {
 
     private fun getJavaClassByName(name: String): Class<Any> {
         return when (name) {
+            "Beliefs" -> emptyArray<Belief>().javaClass
             "Buildings" -> emptyArray<Building>().javaClass
             "Difficulties" -> emptyArray<Difficulty>().javaClass
+            "Eras" -> emptyArray<Era>().javaClass
             "Nations" -> emptyArray<Nation>().javaClass
             "Policies" -> emptyArray<PolicyBranch>().javaClass
             "Quests" -> emptyArray<Quest>().javaClass
+            "Religions" -> emptyArray<String>().javaClass
             "Specialists" -> emptyArray<Specialist>().javaClass
             "Techs" -> emptyArray<TechColumn>().javaClass
             "Terrains" -> emptyArray<Terrain>().javaClass
