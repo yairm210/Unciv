e7b15ab6001e47fbcb4b86ee2239f2a19a5599d3
Interdice <67503847+Interdice@users.noreply.github.com>
2021-08-04 15:11:23 +1000

    You can now gift improvements to the ai

--- core/src/com/unciv/logic/city/CityInfo.kt
+++ core/src/com/unciv/logic/city/CityInfo.kt
@@ -647,7 +647,7 @@ class CityInfo {
             .map { it.getOwner()?.civName }.filterNotNull().toSet() 
             .distinct().toList()
     }
-    fun getImproveableTiles(): List<TileInfo> = getTiles().map {it}
+    fun getImprovableTiles(): List<TileInfo> = getTiles().map {it}
             .filter {it.hasViewableResource(civInfo) && it.improvement == null}.toList()
 
 
