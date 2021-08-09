4409657f4f644ba96700eae18b12d6e1b8e8306c
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2021-08-09 16:49:11 +0200

    City screen purchasable constructions prettier

--- core/src/com/unciv/logic/city/CityConstructions.kt
+++ core/src/com/unciv/logic/city/CityConstructions.kt
@@ -153,16 +153,20 @@ class CityConstructions {
     }
 
 
-    /** @constructionName needs to be a non-perpetual construction, else a cost of -1 is inferred */
+    /** @constructionName needs to be a non-perpetual construction, else an empty string is returned */
     internal fun getTurnsToConstructionString(constructionName: String, useStoredProduction:Boolean = true): String {
         val construction = getConstruction(constructionName)
-        val cost = 
-            if (construction is INonPerpetualConstruction) construction.getProductionCost(cityInfo.civInfo)
-            else -1 // This could _should_ never be reached
+        if (construction !is INonPerpetualConstruction) return ""   // shouldn't happen
+        val cost = construction.getProductionCost(cityInfo.civInfo)
         val turnsToConstruction = turnsToConstruction(constructionName, useStoredProduction)
         val currentProgress = if (useStoredProduction) getWorkDone(constructionName) else 0
-        if (currentProgress == 0) return "\n$cost${Fonts.production} $turnsToConstruction${Fonts.turn}"
-        else return "\n$currentProgress/$cost${Fonts.production}\n$turnsToConstruction${Fonts.turn}"
+        if (construction.getMatchingUniques("Unbuildable").none())
+            return "\n" + (if (currentProgress == 0) "" else "$currentProgress/") +
+                    "$cost${Fonts.production} $turnsToConstruction${Fonts.turn}"
+        if (!construction.canBePurchasedWithAnyStat(cityInfo)) return ""  // expensive and only suppresses the \n
+        return Stat.values().filter {
+            construction.canBePurchasedWithStat(cityInfo, it, true)
+        }.joinToString(prefix = "\n") { "${construction.getStatBuyCost(cityInfo, it)}${it.character}" }
     }
 
     // This function appears unused, can it be removed?
--- core/src/com/unciv/models/stats/Stat.kt
+++ core/src/com/unciv/models/stats/Stat.kt
@@ -2,13 +2,18 @@ package com.unciv.models.stats
 
 import com.unciv.logic.civilization.NotificationIcon
 import com.unciv.models.UncivSound
+import com.unciv.ui.utils.Fonts
 
-enum class Stat(val notificationIcon: String, val purchaseSound: UncivSound) {
-    Production(NotificationIcon.Production, UncivSound.Click),
-    Food(NotificationIcon.Food, UncivSound.Click),
-    Gold(NotificationIcon.Gold, UncivSound.Coin),
-    Science(NotificationIcon.Science, UncivSound.Chimes),
-    Culture(NotificationIcon.Culture, UncivSound.Paper),
-    Happiness(NotificationIcon.Happiness, UncivSound.Click),
-    Faith(NotificationIcon.Faith, UncivSound.Choir);
+enum class Stat(
+    val notificationIcon: String,
+    val purchaseSound: UncivSound,
+    val character: Char
+) {
+    Production(NotificationIcon.Production, UncivSound.Click, Fonts.production),
+    Food(NotificationIcon.Food, UncivSound.Click, Fonts.food),
+    Gold(NotificationIcon.Gold, UncivSound.Coin, Fonts.gold),
+    Science(NotificationIcon.Science, UncivSound.Chimes, Fonts.science),
+    Culture(NotificationIcon.Culture, UncivSound.Paper, Fonts.culture),
+    Happiness(NotificationIcon.Happiness, UncivSound.Click, Fonts.happiness),
+    Faith(NotificationIcon.Faith, UncivSound.Choir, Fonts.faith);
 }
