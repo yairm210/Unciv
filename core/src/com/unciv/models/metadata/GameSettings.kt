5c5c45c45d373a26e3c2bc96f74b9b471dc5c818
xlenstra <xanderlenstra@gmail.com>
2021-09-08 23:22:14 +0200

    Religion goes from alpha into beta: available from stat screen immediately

--- core/src/com/unciv/models/metadata/GameSettings.kt
+++ core/src/com/unciv/models/metadata/GameSettings.kt
@@ -47,7 +47,6 @@ class GameSettings {
     var visualMods = HashSet<String>()
 
     var showExperimentalWorldWrap = false // We're keeping this as a config due to ANR problems on Android phones for people who don't know what they're doing :/
-    var showExperimentalReligion = false
 
     var lastOverviewPage: String = "Cities"
 
