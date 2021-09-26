8b5f1985774c78272e85520b732812c27207786a
yairm210 <yairm210@hotmail.com>
2021-09-26 14:58:11 +0300

    Hopefully fixed Google Play release

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,7 +3,7 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.5.30"
     const val appName = "Unciv"
-    const val appCodeNumber = 630
+    const val appCodeNumber = 631
     const val appVersion = "3.17.2"
 
     const val gdxVersion = "1.10.0"
