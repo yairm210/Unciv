861de2901efa1b8ab1e8bbcb4f1efa44c40999f7
xlenstra <xanderlenstra@gmail.com>
2021-10-04 00:58:17 +0200

    Added two more conditionals and updated movement, sight & combat to use these

--- core/src/com/unciv/models/ruleset/unique/StateForConditionals.kt
+++ core/src/com/unciv/models/ruleset/unique/StateForConditionals.kt
@@ -4,12 +4,15 @@ import com.unciv.logic.battle.CombatAction
 import com.unciv.logic.battle.ICombatant
 import com.unciv.logic.city.CityInfo
 import com.unciv.logic.civilization.CivilizationInfo
+import com.unciv.logic.map.MapUnit
 import com.unciv.logic.map.TileInfo
 
 data class StateForConditionals(
     val civInfo: CivilizationInfo? = null,
     val cityInfo: CityInfo? = null,
-//    val attacker: ICombatant? = null,
+    val unit: MapUnit? = null,
+    
+    val attacker: ICombatant? = null,
     val defender: ICombatant? = null,
 //    val attackedTile: TileInfo? = null,
     val combatAction: CombatAction? = null,
