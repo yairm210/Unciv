13859e78c1c98275ea91b37af48183863a7320b9
Xander Lenstra <71121390+xlenstra@users.noreply.github.com>
2021-10-20 15:57:55 +0200

    Unified and enumified production percentage bonus uniques (#5521)

--- core/src/com/unciv/models/ruleset/unique/UniqueParameterType.kt
+++ core/src/com/unciv/models/ruleset/unique/UniqueParameterType.kt
@@ -1,6 +1,7 @@
 package com.unciv.models.ruleset.unique
 
 import com.unciv.models.ruleset.BeliefType
+import com.unciv.models.ruleset.Building
 import com.unciv.models.ruleset.Ruleset
 import com.unciv.models.ruleset.tile.ResourceType
 import com.unciv.models.ruleset.tile.TerrainType
@@ -91,6 +92,29 @@ enum class UniqueParameterType(val parameterName:String) {
             return UniqueType.UniqueComplianceErrorSeverity.RulesetSpecific
         }
     },
+    BuildingFilter("buildingFilter") {
+        override fun getErrorSeverity(
+            parameterText: String,
+            ruleset: Ruleset
+        ): UniqueType.UniqueComplianceErrorSeverity? {
+            if (parameterText == "All") return null
+            if (Stat.values().any { it.name == parameterText }) return null
+            if (BuildingName.getErrorSeverity(parameterText, ruleset) == null) return null
+            return UniqueType.UniqueComplianceErrorSeverity.WarningOnly
+        } 
+    },
+    // Only used in values deprecated in 3.17.10
+        ConstructionFilter("constructionFilter") {
+            override fun getErrorSeverity(
+                parameterText: String,
+                ruleset: Ruleset
+            ): UniqueType.UniqueComplianceErrorSeverity? {
+                if (BuildingFilter.getErrorSeverity(parameterText, ruleset) == null) return null
+                if (BaseUnitFilter.getErrorSeverity(parameterText, ruleset) == null) return null
+                return UniqueType.UniqueComplianceErrorSeverity.WarningOnly
+            }
+        },
+    //
     TerrainFilter("terrainFilter") {
         private val knownValues = setOf("All",
             "Coastal", "River", "Open terrain", "Rough terrain", "Water resource",
