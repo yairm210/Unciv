5cf5e13ffb0bd340a2284badb5043bd8fd1927ca
Xander Lenstra <71121390+xlenstra@users.noreply.github.com>
2021-11-09 05:20:27 +0100

    Fixed a bug where maps created before the split PR could no longer be opened after (#5651)

--- core/src/com/unciv/logic/map/MapParameters.kt
+++ core/src/com/unciv/logic/map/MapParameters.kt
@@ -142,7 +142,7 @@ class MapParameters {
 
     /** This is used mainly for the map editor, so you can continue editing a map under the same ruleset you started with */
     var mods = LinkedHashSet<String>()
-    var baseRuleset = BaseRuleset.Civ_V_Vanilla.name // Hardcoded as the Rulesetcache is not yet initialized when starting up 
+    var baseRuleset = BaseRuleset.Civ_V_GnK.fullName // Hardcoded as the Rulesetcache is not yet initialized when starting up 
 
     /** Unciv Version of creation for support cases */
     var createdWithVersion = ""
