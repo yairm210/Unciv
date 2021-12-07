e3d86db66d4436dffe9ff777a25ff5dcfd2348f4
yairm210 <yairm210@hotmail.com>
2021-12-07 21:39:48 +0200

    3.18.7-patch1

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,7 +4,7 @@ object BuildConfig {
     const val kotlinVersion = "1.5.30"
     const val appName = "Unciv"
     const val appCodeNumber = 652
-    const val appVersion = "3.18.7"
+    const val appVersion = "3.18.7-patch1"
 
     const val gdxVersion = "1.10.0"
     const val roboVMVersion = "2.3.1"
