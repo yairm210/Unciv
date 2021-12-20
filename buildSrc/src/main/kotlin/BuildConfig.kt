c70ac334bd01951c6635dc8b6cbdb5de53691161
yairm210 <yairm210@hotmail.com>
2021-12-20 21:25:17 +0200

    3.18.11

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,8 +3,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.5.30"
     const val appName = "Unciv"
-    const val appCodeNumber = 658
-    const val appVersion = "3.18.10-patch1"
+    const val appCodeNumber = 659
+    const val appVersion = "3.18.11"
 
     const val gdxVersion = "1.10.0"
     const val roboVMVersion = "2.3.1"
