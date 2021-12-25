ea51c7155b32e0421cd8de6c23b3d0793a98d2ed
Xander Lenstra <71121390+xlenstra@users.noreply.github.com>
2021-12-25 16:53:10 +0100

    Fixed a bug where an empty improvement picker screen could open (#5815)
    * Fixed a bug where an empty improvement picker screen could open
    
    * Moved the check for constructing instead of building to the unit

--- core/src/com/unciv/ui/worldscreen/unit/UnitActions.kt
+++ core/src/com/unciv/ui/worldscreen/unit/UnitActions.kt
@@ -377,8 +377,12 @@ object UnitActions {
         if (unit.isEmbarked()) return
 
         val canConstruct = unit.currentMovement > 0
-                && !tile.isCityCenter()
-                && unit.civInfo.gameInfo.ruleSet.tileImprovements.values.any { tile.canBuildImprovement(it, unit.civInfo) && unit.canBuildImprovement(it) }
+            && !tile.isCityCenter()
+            && unit.civInfo.gameInfo.ruleSet.tileImprovements.values.any { 
+                tile.canBuildImprovement(it, unit.civInfo) 
+                && unit.canBuildImprovement(it)
+            }
+        
 
         actionList += UnitAction(UnitActionType.ConstructImprovement,
             isCurrentAction = unit.currentTile.hasImprovementInProgress(),
