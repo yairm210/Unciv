44f5c4ecdcbd05e5b8242d19ca3bab7f3a2b6c06
xlenstra <xanderlenstra@gmail.com>
2022-01-27 13:19:51 +0100

    Possibly fixed a crash accessing gameInfo before it is initialized

--- core/src/com/unciv/logic/trade/TradeOffer.kt
+++ core/src/com/unciv/logic/trade/TradeOffer.kt
@@ -6,28 +6,38 @@ import com.unciv.models.metadata.GameSpeed
 import com.unciv.models.translations.tr
 import com.unciv.ui.utils.Fonts
 import com.unciv.logic.trade.TradeType.TradeTypeNumberType
-import com.unciv.models.ruleset.tile.ResourceSupply
 
-data class TradeOffer(val name:String, val type:TradeType, var amount:Int = 1, var duration: Int = -1) {
+data class TradeOffer(val name:String, val type:TradeType, var amount:Int = 1, private var _duration: Int? = null) {
 
-    init {
-        // Duration needs to be part of the variables defined in the primary constructor, 
-        // so that it will be copied over with the automatically generated copy()
-        
-        duration =
-            if (type.isImmediate) -1 // -1 for offers that are immediate (e.g. gold transfer)
-            else {
+    /**
+     * It could be that UncivGame.Current is not initialized when unzipping saves, leading to crashes
+     * The easiest way around this, is to only access the UncivGame.Current whenever the data is needed
+     * instead of when the object is created. The obvious way to do that would be to use a lazy.
+     * Those are, sadly, immutable, and as duration also contains the amount of turns left for a trade,
+     * it needs to be mutable. So the next logical solution would be to use a function for accessing
+     * the value, which is basically what is done here. The duration variable is used in the API,
+     * while internally values are saved in the _duration variable.
+    */
+    var duration: Int
+        get() {
+            if (_duration == null) {
                 // Do *not* access UncivGame.Current.gameInfo in the default constructor!
                 val gameSpeed = UncivGame.Current.gameInfo.gameParameters.gameSpeed
-                when {
+                val correctDuration = when {
+                    type.isImmediate -> -1 // -1 for offers that are immediate (e.g. gold transfer)
                     name == Constants.peaceTreaty -> 10
                     gameSpeed == GameSpeed.Quick -> 25
                     else -> (30 * gameSpeed.modifier).toInt()
                 }
+                _duration = correctDuration
             }
-    }
-
-    constructor() : this("", TradeType.Gold) // so that the json deserializer can work
+            return _duration!!
+        }
+        set (newValue: Int) {
+            _duration = newValue
+        }
+    
+    constructor() : this("", TradeType.Gold, _duration = null) // so that the json deserializer can work
 
     @Suppress("CovariantEquals")    // This is an overload, not an override of the built-in equals(Any?)
     fun equals(offer: TradeOffer): Boolean {
