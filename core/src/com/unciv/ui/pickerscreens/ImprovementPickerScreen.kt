da01d40cc76b2d4d902ac23f889aa572b7cf5bea
itanasi <spellman23@gmail.com>
2022-02-12 20:14:55 -0800

    Add missing .tr() translation calls

--- core/src/com/unciv/ui/pickerscreens/ImprovementPickerScreen.kt
+++ core/src/com/unciv/ui/pickerscreens/ImprovementPickerScreen.kt
@@ -106,11 +106,11 @@ class ImprovementPickerScreen(val tileInfo: TileInfo, unit: MapUnit, val onAccep
             if (tileInfo.improvement != null && removeImprovement) labelText += "\n" + "Replaces [${tileInfo.improvement}]".tr()
 
             val pickNow = if (suggestRemoval)
-                (Constants.remove + tileInfo.getLastTerrain().name + " first").toLabel()
+                (Constants.remove + tileInfo.getLastTerrain().name + " first").tr().toLabel()
             else if (tileInfo.improvementInProgress != improvement.name)
-                "Pick now!".toLabel().onClick { accept(improvement) }
+                "Pick now!".tr().toLabel().onClick { accept(improvement) }
             else
-                "Current construction".toLabel()
+                "Current construction".tr().toLabel()
 
             val statIcons = getStatIconsTable(provideResource, removeImprovement)
 
