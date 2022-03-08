a62e8006bd0b8105482de7a56552ed081b90a768
Yair Morgenstern <yairm210@hotmail.com>
2022-03-08 22:00:41 +0200

    Node.js script creates new changelog text and adds it to the changelog

--- .github/workflows/createChangelogEntry.js
+++ .github/workflows/createChangelogEntry.js
@@ -1,14 +1,19 @@
 
 const { Octokit } = require("@octokit/rest");
+const { version } = require("os");
+const internal = require("stream");
+const fs = require("fs")
 
-//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 
+
+// To be run from the main Unciv repo directory
+// Summarizes and adds the summary to the changelog.md file
+// Meant to be run from a Github action as part of the preparation for version rollout
 
 async function main(){
 
-    const myArgs = process.argv.slice(2) // see https://nodejs.org/en/knowledge/command-line/how-to-parse-command-line-arguments/
-    const octokit = new Octokit({
-        auth: myArgs[0],
-    });
+    // no need to add auth: token since we're only reading from the commit list, which is public anyway
+    const octokit = new Octokit({});
+    
 
     var result = await octokit.repos.listCommits({
         owner: "yairm210",
@@ -16,24 +21,27 @@ async function main(){
         per_page: 50 })
     
 
-//   var result = await getCommits()
-
     var commitSummary = "";
     var ownerToCommits = {}
     var reachedPreviousVersion = false
+    var nextVersionString = ""
     result.data.forEach(commit => {
         if (reachedPreviousVersion) return
         var author = commit.author.login
         if (author=="uncivbot[bot]") return
         var commitMessage = commit.commit.message.split("\n")[0];
 
-        if (commitMessage.match(/^\d+\.\d+\.\d+$/)){ // match EXACT version, like 3.4.55  ^ is for start-of-line, $ for end-of-line
-        reachedPreviousVersion=true
-        console.log("Previous version: "+commitMessage)
-        return
+        var versionMatches = commitMessage.match(/^\d+\.\d+\.(\d+)$/)
+        if (versionMatches){ // match EXACT version, like 3.4.55  ^ is for start-of-line, $ for end-of-line
+            reachedPreviousVersion=true
+            var minorVersion = Number(versionMatches[1])
+            console.log("Previous version: "+commitMessage)
+            nextVersionString = commitMessage.replace(RegExp(minorVersion+"$"), minorVersion+1 )
+            console.log("Next version: " + nextVersionString)
+            return
         }
         if (commitMessage.startsWith("Merge ") || commitMessage.startsWith("Update ")) return
-        commitMessage = commitMessage.replace(/\(\#\d+\)/,"") // match PR auto-text, like (#2345)
+        commitMessage = commitMessage.replace(/\(\#\d+\)/,"").replace(/\#\d+/,"") // match PR auto-text, like (#2345) or just #2345
         if (author != "yairm210"){
         if (ownerToCommits[author] == undefined) ownerToCommits[author]=[]
         ownerToCommits[author].push(commitMessage)
@@ -50,6 +58,12 @@ async function main(){
     })
     console.log(commitSummary)
 
+    var textToAddToChangelog = "## "+ nextVersionString + commitSummary + "\n\n"
+
+    var changelogPath = 'changelog.md'
+    var currentChangelog = fs.readFileSync(changelogPath).toString()
+    var newChangelog = textToAddToChangelog + currentChangelog
+    fs.writeFileSync(changelogPath, newChangelog)
 }
 
 main()
