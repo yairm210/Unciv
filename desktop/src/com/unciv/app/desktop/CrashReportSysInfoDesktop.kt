673df3d5501fc348aa33f8f89a94b34ce8d27843
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2022-03-23 12:24:58 +0100

    Now gets Windows info the hard way

--- desktop/src/com/unciv/app/desktop/CrashReportSysInfoDesktop.kt
+++ desktop/src/com/unciv/app/desktop/CrashReportSysInfoDesktop.kt
@@ -2,6 +2,7 @@ package com.unciv.app.desktop
 
 import com.badlogic.gdx.files.FileHandle
 import com.unciv.ui.utils.CrashReportSysInfo
+import java.nio.charset.Charset
 
 class CrashReportSysInfoDesktop : CrashReportSysInfo {
 
@@ -9,26 +10,19 @@ class CrashReportSysInfoDesktop : CrashReportSysInfo {
         val builder = StringBuilder()
 
         // Operating system
-        builder.append("OS: " + (System.getProperty("os.name") ?: "Unknown"))
-        val osInfo = listOfNotNull(System.getProperty("os.arch"), System.getProperty("os.version")).joinToString()
-        if (osInfo.isNotEmpty()) builder.append(" ($osInfo)")
+        val osName = System.getProperty("os.name") ?: "Unknown"
+        val isWindows = osName.startsWith("Windows", ignoreCase = true)
+        builder.append("OS: $osName")
+        if (!isWindows) {
+            val osInfo = listOfNotNull(System.getProperty("os.arch"), System.getProperty("os.version")).joinToString()
+            if (osInfo.isNotEmpty()) builder.append(" ($osInfo)")
+        }
         builder.appendLine()
 
-        // Linux distro
-        val osRelease: Map<String,String> = try {
-            FileHandle("/etc/os-release")
-                .readString()
-                .split('\n')
-                .map { it.split('=') }
-                .filter { it.size == 2 }
-                .associate { it[0] to it[1].removeSuffix("\"").removePrefix("\"") }
-        } catch (ex: Throwable) { mapOf() }
-        if ("NAME" in osRelease) {
-            builder.append('\t')
-            builder.appendLine(
-                osRelease["PRETTY_NAME"] ?: "${osRelease["NAME"]} ${osRelease["VERSION"]}"
-            )
-        }
+        // Specific release info
+        val osRelease = if (isWindows) getWinVer() else getLinuxDistro()
+        if (osRelease.isNotEmpty())
+            builder.appendLine("\t$osRelease")
 
         // Java runtime version
         val javaVendor: String? = System.getProperty("java.vendor")
@@ -47,5 +41,60 @@ class CrashReportSysInfoDesktop : CrashReportSysInfo {
         }
 
         return builder.toString()
+    }
+
+    companion object {
+        @Suppress("SpellCheckingInspection")
+        private val winVerCommand = """
+        cmd /c
+        reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName &&
+        reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ReleaseId &&
+        reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentBuild &&
+        reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v DisplayVersion
+        """.trimIndent().replace('\n', ' ')
+
+        /** Kludge to get the important Windows version info (no easier way than the registry AFAIK)
+         *  using a subprocess running reg query. Other methods would involve nasty reflection
+         *  to break java.util.prefs.Preferences out of its Sandbox, or JNA requiring new bindings.
+         */
+        fun getWinVer(): String {
+            val entries: Map<String,String> = try {
+                val process = Runtime.getRuntime().exec(winVerCommand)
+                process.waitFor()
+                val output = process.inputStream.readAllBytes().toString(Charset.defaultCharset())
+
+                val goodLines = output.split('\n').mapNotNull {
+                    it.removeSuffix("\r").run {
+                        if (startsWith("    ") || startsWith("\t")) trim() else null
+                    }
+                }
+
+                goodLines.map { it.split("REG_SZ") }
+                    .filter { it.size == 2 }
+                    .associate { it[0].trim() to it[1].trim() }
+            } catch (ex: Throwable) { mapOf() }
+
+            if ("ProductName" !in entries) return ""
+
+            return entries["ProductName"]!! +
+                    ((entries["DisplayVersion"] ?: entries["ReleaseId"])?.run { " Version $this" } ?: "") +
+                    (entries["CurrentBuild"]?.run { " (Build $this)" } ?: "")
+        }
+
+        /** Get linux Distribution out of the /etc/os-release file (ini-style)
+         *  Should be safely silent on systems not supporting that file.
+         */
+        fun getLinuxDistro(): String {
+            val osRelease: Map<String,String> = try {
+                FileHandle("/etc/os-release")
+                    .readString()
+                    .split('\n')
+                    .map { it.split('=') }
+                    .filter { it.size == 2 }
+                    .associate { it[0] to it[1].removeSuffix("\"").removePrefix("\"") }
+            } catch (ex: Throwable) { mapOf() }
+            if ("NAME" !in osRelease) return ""
+            return osRelease["PRETTY_NAME"] ?: "${osRelease["NAME"]} ${osRelease["VERSION"]}"
+        }
     }
 }
