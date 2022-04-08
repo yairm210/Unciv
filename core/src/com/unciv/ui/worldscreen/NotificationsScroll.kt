b1d7e4f7b68562a3d33986bd714d992e9cf772db
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2022-04-08 23:10:54 +0200

    Nicer NotificationScroll

--- core/src/com/unciv/ui/worldscreen/NotificationsScroll.kt
+++ core/src/com/unciv/ui/worldscreen/NotificationsScroll.kt
@@ -3,43 +3,69 @@ package com.unciv.ui.worldscreen
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Touchable
+import com.badlogic.gdx.scenes.scene2d.ui.Cell
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.utils.Align
 import com.unciv.logic.civilization.Notification
+import com.unciv.ui.utils.WrappableLabel
 import com.unciv.ui.images.ImageGetter
 import com.unciv.ui.utils.onClick
-import com.unciv.ui.utils.toLabel
 import kotlin.math.min
 import com.unciv.ui.utils.AutoScrollPane as ScrollPane
 
-class NotificationsScroll(internal val worldScreen: WorldScreen) : ScrollPane(null) {
+class NotificationsScroll(
+    private val worldScreen: WorldScreen,
+    private val maxNotificationsHeight: Float
+) : ScrollPane(null) {
+    private companion object {
+        /** Scale the entire ScrollPane by this factor */
+        const val scaleFactor = 0.5f
+        /** Complement of [scaleFactor] because multiplication is cheaper */
+        const val inverseScaleFactor = 1f / scaleFactor
+        /** Limit width by wrapping labels to this percentage of the stage */
+        const val maxWidthOfStage = 0.333f
+        /** Logical size of the notification icons - note fontsize is coded separately */
+        const val iconSize = 30f
+    }
 
-    var notificationsHash: Int = 0
+    private var notificationsHash: Int = 0
 
     private var notificationsTable = Table()
+    private var endOfTableSpacerCell: Cell<*>? = null
+
+    private val maxEntryWidth = worldScreen.stage.width * maxWidthOfStage * inverseScaleFactor
 
     init {
         actor = notificationsTable.right()
         touchable = Touchable.childrenOnly
-        setScale(.5f)
+        setScale(scaleFactor)
     }
 
-    internal fun update(notifications: MutableList<Notification>) {
+    internal fun update(notifications: MutableList<Notification>, tileInfoTableHeight: Float) {
 
         // no news? - keep our list as it is, especially don't reset scroll position
-        if (notificationsHash == notifications.hashCode())
+        if (notificationsHash == notifications.hashCode()) {
+            sizeScrollingSpacer(tileInfoTableHeight)
+            layout()
             return
+        }
         notificationsHash = notifications.hashCode()
 
         notificationsTable.clearChildren()
-        for (notification in notifications.toList().reversed()) { // toList to avoid concurrency problems
-            val label = notification.text.toLabel(Color.BLACK, 30)
+        for (notification in notifications.asReversed().toList()) { // toList to avoid concurrency problems
             val listItem = Table()
-
             listItem.background = ImageGetter.getRoundedEdgeRectangle()
-            listItem.add(label)
 
+            val labelWidth = maxEntryWidth - iconSize * notification.icons.size - 10f
+            val label = WrappableLabel(notification.text, labelWidth, Color.BLACK, 30)
+            label.setAlignment(Align.center)
+            if (label.prefWidth > labelWidth * scaleFactor) {  // can't explain why the comparison needs scaleFactor
+                label.wrap = true
+                listItem.add(label).maxWidth(label.optimizePrefWidth()).padRight(10f)
+            } else {
+                listItem.add(label).padRight(10f)
+            }
 
-            val iconSize = 30f
             if (notification.icons.isNotEmpty()) {
                 val ruleset = worldScreen.gameInfo.ruleSet
                 for (icon in notification.icons.reversed()) {
@@ -63,9 +89,26 @@ class NotificationsScroll(internal val worldScreen: WorldScreen) : ScrollPane(nu
 
             notificationsTable.add(clickArea).right().row()
         }
-        notificationsTable.pack()
+
+        notificationsTable.pack()  // needed to get height - prefHeight is set and close but not quite the same value
+        val filledHeight = notificationsTable.height
+
+        sizeScrollingSpacer(tileInfoTableHeight)
+
         pack()
-        height = min(notificationsTable.height, worldScreen.stage.height * 2 / 3 - 15f)
+        height = min(filledHeight, maxNotificationsHeight * inverseScaleFactor)  // after this, maxY is still incorrect until layout()
     }
 
+    /** Add some empty space that can be scrolled under the TileInfoTable which is covering our lower part */
+    private fun sizeScrollingSpacer(tileInfoTableHeight: Float) {
+        if (endOfTableSpacerCell == null) {
+            endOfTableSpacerCell = notificationsTable.add().pad(5f)
+            notificationsTable.row()
+        }
+        val scaledHeight = tileInfoTableHeight * inverseScaleFactor
+        endOfTableSpacerCell!!.height(scaledHeight)
+        notificationsTable.invalidate() // looks redundant but isn't
+        // (the flags it sets are already on when inspected in debugger, but when omitting it the
+        // ScrollPane will not properly scroll down to the new maxY when TileInfoTable changes to a smaller height)
+    }
 }
