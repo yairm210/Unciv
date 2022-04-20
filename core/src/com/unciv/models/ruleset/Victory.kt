4f38a2a4d63bf4fd205ce4c89c0d5cdb8dbe992f
xlenstra <xanderlenstra@gmail.com>
2022-04-20 18:26:20 +0200

    Added milestone objects

--- core/src/com/unciv/models/ruleset/Victory.kt
+++ core/src/com/unciv/models/ruleset/Victory.kt
@@ -1,6 +1,45 @@
 package com.unciv.models.ruleset
 
 import com.unciv.models.stats.INamed
+import com.unciv.logic.civilization.CivilizationInfo
+import com.unciv.models.translations.getPlaceholderParameters
+import com.unciv.models.translations.tr
+
+
+enum class MilestoneType(text: String) {
+    BuiltBuilding("Built [building]"),
+    AddedUnitInCapital("Add [amount] [unit] units in capital"),
+    DestroyAllPlayers("Destroy all players"),
+    CaptureAllCapitals("Capture all capitals"),
+    CompletePolicyBranches("Complete [amount] policy branches")
+}
+
+
+class Milestone(private val uniqueDescription: String) {
+
+    val type: MilestoneType = MilestoneType.valueOf(uniqueDescription)
+    val params by lazy { uniqueDescription.getPlaceholderParameters() }
+
+    fun getDisplayString() = uniqueDescription.tr()
+    fun hasBeenCompletedBy(civInfo: CivilizationInfo): Boolean {
+        return when (type) {
+            MilestoneType.BuiltBuilding ->
+                civInfo.cities.any { it.cityConstructions.builtBuildings.contains(params[0])}
+            MilestoneType.AddedUnitInCapital ->
+                civInfo.victoryManager.currentsSpaceshipParts[params[1]]!! >= params[0].toInt()
+            MilestoneType.DestroyAllPlayers ->
+                civInfo.gameInfo.getAliveMajorCivs() == listOf(civInfo)
+            MilestoneType.CaptureAllCapitals ->
+                civInfo.cities.count {
+                    it.isOriginalCapital
+                    && civInfo.gameInfo.getCivilization(it.foundingCiv).isMajorCiv()
+                } == civInfo.gameInfo.civilizations.count { it.isMajorCiv() }
+            MilestoneType.CompletePolicyBranches ->
+                civInfo.policies.completedBranches.size >= params[0].toInt()
+        }
+    }
+}
+
 
 class Victory : INamed {
     override var name = ""
@@ -8,7 +47,11 @@ class Victory : INamed {
     val hiddenInVictoryScreen = false
     // Things to do to win
     val milestones = ArrayList<String>()
+    val milestoneObjects by lazy { milestones.map { Milestone(it) }}
 }
+
+
+
 
 enum class VictoryType {
     Neutral,
