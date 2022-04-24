3428150bb6755c25edc505ae177d578213627d93
JackRainy <JackRainy@users.noreply.github.com>
2022-04-25 00:54:41 +0300

    Comments are added

--- core/src/com/unciv/logic/map/UnitMovementAlgorithms.kt
+++ core/src/com/unciv/logic/map/UnitMovementAlgorithms.kt
@@ -536,12 +536,18 @@ class UnitMovementAlgorithms(val unit: MapUnit) {
         val theirOldPosition = otherUnit.getTile()
 
         // Swap the units
+        // Step 1: Release the destination tile
         otherUnit.removeFromTile()
+        // Step 2: Perform the movement
         unit.movement.moveToTile(destination)
+        // Step 3: Release the newly taken tile
         unit.removeFromTile()
+        // Step 4: Perform the another movement
         otherUnit.putInTile(theirOldPosition)
         otherUnit.movement.moveToTile(ourOldPosition)
+        // Step 5: Restore the position in the new tile
         unit.putInTile(theirOldPosition)
+        // Step 6: Update states
         otherUnit.mostRecentMoveType = UnitMovementMemoryType.UnitMoved
         unit.mostRecentMoveType = UnitMovementMemoryType.UnitMoved
     }
