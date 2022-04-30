8d5baa32d88463c630f570e1120c9b6d9e536655
JackRainy <JackRainy@users.noreply.github.com>
2022-05-01 00:02:31 +0300

    Basic unit tests

---
+++ tests/src/com/unciv/logic/civilization/CapitalConnectionsFinderTests.kt
@@ -0,0 +1,159 @@
+package com.unciv.logic.civilization
+
+import com.badlogic.gdx.math.Vector2
+import com.unciv.logic.GameInfo
+import com.unciv.logic.city.CityInfo
+import com.unciv.logic.map.RoadStatus
+import com.unciv.logic.map.TileInfo
+import com.unciv.logic.map.TileMap
+import com.unciv.models.ruleset.Nation
+import com.unciv.models.ruleset.Ruleset
+import com.unciv.models.ruleset.RulesetCache
+import com.unciv.models.ruleset.tile.TerrainType
+import com.unciv.models.ruleset.unique.UniqueType
+import com.unciv.testing.GdxTestRunner
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(GdxTestRunner::class)
+class CapitalConnectionsFinderTests {
+
+    private val mockGameInfo = mockk<GameInfo>()
+    private val slot = slot<String>()
+
+    private val testCivilizationNames = arrayListOf("America", "Germany", "Greece")
+    private val civilizations = testCivilizationNames.associateWith { CivilizationInfo(it) }
+    private val ourCiv = civilizations.values.first()
+    private val tilesMap = TileMap().apply { tileMatrix = ArrayList() }
+    private var rules = Ruleset()
+
+    @Before
+    fun setup() {
+        RulesetCache.loadRulesets()
+        rules = RulesetCache.getVanillaRuleset()
+        // Setup the GameInfo mock
+        every { mockGameInfo.getCivilization(capture(slot)) } answers { civilizations.getValue(slot.captured) }
+        every { mockGameInfo.civilizations } answers { civilizations.values.toMutableList() }
+        every { mockGameInfo.tileMap } returns tilesMap
+        every { mockGameInfo.ruleSet } returns rules
+        every { mockGameInfo.getCities() } answers { civilizations.values.asSequence().flatMap { it.cities } }
+        // Needs for founding cities
+        every { mockGameInfo.turns } returns 1
+
+        // Initialize test civilizations so they pass certain criteria
+        civilizations.values.forEach {
+            it.gameInfo = mockGameInfo
+            it.nation = Nation()
+            it.nation.name = it.civName // for isBarbarian()
+            it.tech.techsResearched.add(rules.tileImprovements[RoadStatus.Road.name]!!.techRequired!!)
+            it.tech.techsResearched.add(rules.tileImprovements[RoadStatus.Railroad.name]!!.techRequired!!)
+        }
+    }
+
+    @After
+    fun tearDown() {
+        (tilesMap.values as ArrayList<TileInfo>).clear()
+        for (civ in civilizations.values)
+            civ.cities = emptyList()
+    }
+
+    private fun createLand(from: Int, to: Int) {
+        // create map
+        val tiles = tilesMap.tileMatrix
+        tilesMap.bottomY = from
+        tiles.add(ArrayList())
+        for (y in from..to)
+            tiles[0].add(TileInfo().apply { tileMap = tilesMap
+                position = Vector2(0f, y.toFloat())
+                baseTerrain = rules.terrains.values.first { it.type == TerrainType.Land }.name })
+    }
+
+    private fun createWater(from: Int, to: Int) {
+        // create map
+        val tiles = tilesMap.tileMatrix
+        tilesMap.bottomY = from
+        // here we assume the row with a land is already created
+        tiles.add(ArrayList())
+        for (y in from..to)
+            tiles[1].add(TileInfo().apply { tileMap = tilesMap
+                position = Vector2(1f, y.toFloat())
+                isWater = true
+                baseTerrain = rules.terrains.values.first { it.type == TerrainType.Water }.name })
+    }
+
+    private fun createMedium(from:Int, to: Int, type: RoadStatus) {
+        val tiles = tilesMap.tileMatrix
+        for (tile in tiles[0])
+            if (tile != null && tile.position.y > from && tile.position.y < to)
+                tile.roadStatus = type
+    }
+
+    private fun createCity(civInfo: CivilizationInfo, position: Vector2, name: String, capital: Boolean = false, hasHarbor: Boolean = false): CityInfo {
+        return CityInfo().apply {
+            this.civInfo = civInfo
+            location = position
+            if (capital)
+                cityConstructions.builtBuildings.add(rules.buildings.values.first { it.hasUnique(UniqueType.IndicatesCapital) }.name)
+            if (hasHarbor)
+                cityConstructions.builtBuildings.add(rules.buildings.values.first { it.name == "Harbor" }.name)
+            this.name = name
+            setTransients()
+            tilesMap[location].setOwningCity(this)
+        }
+    }
+
+    @Test
+    fun `Own cities are connected by road`() {
+
+        createLand(-2,2)
+        ourCiv.cities = listOf( createCity(ourCiv, Vector2(0f, 0f), "Capital", true),
+                                createCity(ourCiv, Vector2(0f, -2f), "Connected"),
+                                createCity(ourCiv, Vector2(0f, 2f), "Not connected"))
+        createMedium(-2, 0, RoadStatus.Road)
+
+        val connectionsFinder = CapitalConnectionsFinder(ourCiv)
+        val res = connectionsFinder.find()
+
+        Assert.assertTrue(res.keys.any { it.name == "Connected" } && !res.keys.any { it.name == "Not connected" }  )
+    }
+
+    @Test
+    fun `Own cities are connected by railroad`() {
+
+        createLand(-2,2)
+        ourCiv.cities = listOf( createCity(ourCiv, Vector2(0f, 0f), "Capital", true),
+            createCity(ourCiv, Vector2(0f, 2f), "Connected"),
+            createCity(ourCiv, Vector2(0f, -2f), "Not connected"))
+        createMedium(0, 2, RoadStatus.Railroad)
+
+        val connectionsFinder = CapitalConnectionsFinder(ourCiv)
+        val res = connectionsFinder.find()
+
+        Assert.assertTrue(res.keys.any { it.name == "Connected" } && !res.keys.any { it.name == "Not connected" }  )
+    }
+
+    @Test
+    fun `Own cities are connected by road and harbor`() {
+
+        createLand(-2,4)
+        createWater(-2,4)
+        ourCiv.cities = listOf( createCity(ourCiv, Vector2(0f, 0f), "Capital", true),
+            createCity(ourCiv, Vector2(0f, -2f), "Not connected"),
+            createCity(ourCiv, Vector2(0f, 2f), "Connected1", capital = false, hasHarbor = true),
+            createCity(ourCiv, Vector2(0f, 4f), "Connected2", capital = false, hasHarbor = true))
+
+        createMedium(0,2, RoadStatus.Road)
+
+        val connectionsFinder = CapitalConnectionsFinder(ourCiv)
+        val res = connectionsFinder.find()
+
+        Assert.assertTrue(res.keys.count { it.name.startsWith("Connected") } == 2 && !res.keys.any { it.name == "Not connected" }  )
+    }
+
+}
