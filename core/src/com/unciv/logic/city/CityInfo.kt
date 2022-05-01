660fb81bf24640f351967b55456b9840bf49840e
xlenstra <xanderlenstra@gmail.com>
2022-05-01 21:15:33 +0200

    Fixed `provides [amount] [resource]` not accepting conditionals This was due to it still using `getLocalMatchingUniques` without providing a `StateForConditionals`, and then manually filtering them if they didn't provide conditionals. This of course doesn't work anymore, as `getLocalMatchingUniques` then assumes that all conditionals aren't satisfied. Also refactored a loop over `uniqueObjects` with `isOfType()` to just use `getMatchingUniques()`.

--- core/src/com/unciv/logic/city/CityInfo.kt
+++ core/src/com/unciv/logic/city/CityInfo.kt
@@ -351,24 +351,21 @@ class CityInfo {
         for (tileInfo in getTiles()) {
             if (tileInfo.improvement == null) continue
             val tileImprovement = tileInfo.getTileImprovement()
-            for (unique in tileImprovement!!.uniqueObjects) {
-                if (unique.isOfType(UniqueType.ProvidesResources)) {
-                    if (!unique.conditionalsApply(civInfo, this)) continue
-                    val resource = getRuleset().tileResources[unique.params[1]] ?: continue
-                    cityResources.add(
-                        resource,
-                        unique.params[0].toInt() * civInfo.getResourceModifier(resource),
-                        "Improvements"
-                    )
-                }
-                if (unique.isOfType(UniqueType.ConsumesResources)) {
-                    val resource = getRuleset().tileResources[unique.params[1]] ?: continue
-                    cityResources.add(
-                        resource,
-                        -1 * unique.params[0].toInt(),
-                        "Improvements"
-                    )
-                }
+            for (unique in tileImprovement!!.getMatchingUniques(UniqueType.ProvidesResources, StateForConditionals(civInfo, this))) {
+                val resource = getRuleset().tileResources[unique.params[1]] ?: continue
+                cityResources.add(
+                    resource,
+                    unique.params[0].toInt() * civInfo.getResourceModifier(resource),
+                    "Improvements"
+                )
+            }
+            for (unique in tileImprovement.getMatchingUniques(UniqueType.ConsumesResources, StateForConditionals(civInfo, this))) {
+                val resource = getRuleset().tileResources[unique.params[1]] ?: continue
+                cityResources.add(
+                    resource,
+                    -1 * unique.params[0].toInt(),
+                    "Improvements"
+                )
             }
         }
 
@@ -383,8 +380,7 @@ class CityInfo {
             }
         }
 
-        for (unique in getLocalMatchingUniques(UniqueType.ProvidesResources)) { // E.G "Provides [1] [Iron]"
-            if (!unique.conditionalsApply(civInfo, this)) continue
+        for (unique in getLocalMatchingUniques(UniqueType.ProvidesResources, StateForConditionals(civInfo, this))) { // E.G "Provides [1] [Iron]"
             val resource = getRuleset().tileResources[unique.params[1]]
             if (resource != null) {
                 cityResources.add(
