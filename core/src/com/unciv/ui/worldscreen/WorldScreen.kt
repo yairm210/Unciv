d5b81b65481ff621f30b2b96a45cd01979855190
Azzurite <azzurite@gmail.com>
2022-05-03 22:46:54 +0200

    Always notify spectators of the next players' turn

--- core/src/com/unciv/ui/worldscreen/WorldScreen.kt
+++ core/src/com/unciv/ui/worldscreen/WorldScreen.kt
@@ -363,7 +363,7 @@ class WorldScreen(val gameInfo: GameInfo, val viewingCiv:CivilizationInfo) : Bas
                 // stuff has changed and the "waiting for X" will now show the correct civ
                 stopMultiPlayerRefresher()
                 latestGame.isUpToDate = true
-                if (viewingCiv.civName == latestGame.currentPlayer) {
+                if (viewingCiv.civName == latestGame.currentPlayer || viewingCiv.civName == Constants.spectator) {
                     game.multiplayerTurnNotifier?.turnStarted()
                 }
                 postCrashHandlingRunnable { createNewWorldScreen(latestGame) }
