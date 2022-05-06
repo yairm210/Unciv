023226aabd36c9da5bccb0158f23435084383d38
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2022-05-06 23:45:34 +0600

    check multiplayerServer url if the user is playing on it

--- desktop/src/com/unciv/app/desktop/PlatformSpecificHelpersDesktop.kt
+++ desktop/src/com/unciv/app/desktop/PlatformSpecificHelpersDesktop.kt
@@ -2,6 +2,7 @@ package com.unciv.app.desktop
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.unciv.ui.utils.GeneralPlatformSpecificHelpers
+import com.unciv.UncivGame
 import java.net.URL
 
 class PlatformSpecificHelpersDesktop(config: Lwjgl3ApplicationConfiguration) : GeneralPlatformSpecificHelpers {
@@ -12,7 +13,8 @@ class PlatformSpecificHelpersDesktop(config: Lwjgl3ApplicationConfiguration) : G
 
     override fun isInternetConnected(): Boolean {
         return try {
-            val u = URL("http://www.dropbox.com")
+            val multiplayerServer = UncivGame.Current.settings.multiplayerServer ?: "Dropbox"
+            val u = URL(if (multiplayerServer != "Dropbox") multiplayerServer else "http://www.dropbox.com")
             val conn = u.openConnection()
             conn.connect()
             true
