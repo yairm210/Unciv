52bc6278a3ed67e4e818ad1199da2004b98f309f
xlenstra <xanderlenstra@gmail.com>
2022-05-08 21:51:51 +0200

    I suppose I might as well update this now that we're a version later

--- core/src/com/unciv/models/ruleset/unique/UniqueType.kt
+++ core/src/com/unciv/models/ruleset/unique/UniqueType.kt
@@ -749,7 +749,7 @@ enum class UniqueType(val text: String, vararg targets: UniqueTarget, val flags:
 
     // region DEPRECATED AND REMOVED
 
-    @Deprecated("as of 4.0.14", ReplaceWith("Irremovable"), DeprecationLevel.ERROR)
+    @Deprecated("as of 4.0.15", ReplaceWith("Irremovable"), DeprecationLevel.ERROR)
     Indestructible("Indestructible", UniqueTarget.Improvement),
     
     @Deprecated("as of 3.19.1", ReplaceWith("[stats] from every [Wonder]"), DeprecationLevel.ERROR)
