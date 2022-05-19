0ced4d96e7200ff3fc5dfe386640c7cb1c9395dc
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2022-05-19 14:30:31 +0200

    SimpleHttp gets a connection timeout (#6812)
    * SimpleHttp gets a connection timeout

--- core/src/com/unciv/logic/multiplayer/storage/SimpleHttp.kt
+++ core/src/com/unciv/logic/multiplayer/storage/SimpleHttp.kt
@@ -8,25 +8,28 @@ import java.io.InputStreamReader
 import java.net.*
 import java.nio.charset.Charset
 
+private typealias SendRequestCallback = (success: Boolean, result: String, code: Int?)->Unit
+
 object SimpleHttp {
-    fun sendGetRequest(url: String, action: (success: Boolean, result: String, code: Int?)->Unit) {
-        sendRequest(Net.HttpMethods.GET, url, "", action)
+    fun sendGetRequest(url: String, timeout: Int = 5000, action: SendRequestCallback) {
+        sendRequest(Net.HttpMethods.GET, url, "", timeout, action)
     }
 
-    fun sendRequest(method: String, url: String, content: String, action: (success: Boolean, result: String, code: Int?)->Unit) {
+    fun sendRequest(method: String, url: String, content: String, timeout: Int = 5000, action: SendRequestCallback) {
         var uri = URI(url)
         if (uri.host == null) uri = URI("http://$url")
 
         val urlObj: URL
         try {
             urlObj = uri.toURL()
-        } catch (t:Throwable){
+        } catch (t: Throwable) {
             action(false, "Bad URL", null)
             return
         }
-        
+
         with(urlObj.openConnection() as HttpURLConnection) {
             requestMethod = method  // default is GET
+            connectTimeout = timeout
             if (UncivGame.isCurrentInitialized())
                 setRequestProperty("User-Agent", "Unciv/${UncivGame.Current.version}-GNU-Terry-Pratchett")
             else
--- core/src/com/unciv/ui/worldscreen/mainmenu/OptionsPopup.kt
+++ core/src/com/unciv/ui/worldscreen/mainmenu/OptionsPopup.kt
@@ -137,8 +137,15 @@ class OptionsPopup(
         (previousScreen.game.screen as BaseScreen).openOptionsPopup(tabs.activePage)
     }
 
-    private fun successfullyConnectedToServer(action: (Boolean, String, Int?) -> Unit){
-        SimpleHttp.sendGetRequest("${settings.multiplayerServer}/isalive", action)
+    private fun successfullyConnectedToServer(action: (Boolean) -> Unit) {
+        launchCrashHandling("TestIsAlive") {
+            SimpleHttp.sendGetRequest("${settings.multiplayerServer}/isalive") {
+                success, _, _ ->
+                postCrashHandlingRunnable {
+                    action(success)
+                }
+            }
+        }
     }
 
     //region Page builders
@@ -307,7 +314,7 @@ class OptionsPopup(
             }
             popup.open(true)
 
-            successfullyConnectedToServer { success, _, _ ->
+            successfullyConnectedToServer { success ->
                 popup.addGoodSizedLabel(if (success) "Success!" else "Failed!").row()
                 popup.addCloseButton()
             }
