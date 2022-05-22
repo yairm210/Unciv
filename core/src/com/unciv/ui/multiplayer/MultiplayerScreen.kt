9b3887c598c8eea0c193d6319aec54e25cf6c01b
Azzurite <azzurite@gmail.com>
2022-05-22 03:15:07 +0200

    Refactor: Extract GameList into standalone file

--- core/src/com/unciv/ui/multiplayer/MultiplayerScreen.kt
+++ core/src/com/unciv/ui/multiplayer/MultiplayerScreen.kt
@@ -14,6 +14,7 @@ import com.unciv.ui.utils.*
 import com.unciv.ui.crashhandling.launchCrashHandling
 import com.unciv.ui.crashhandling.postCrashHandlingRunnable
 import com.unciv.ui.images.ImageGetter
+import com.unciv.ui.multiplayer.GameList
 import com.unciv.ui.popup.Popup
 import com.unciv.ui.popup.ToastPopup
 import java.io.FileNotFoundException
@@ -242,121 +243,4 @@ class MultiplayerScreen(previousScreen: BaseScreen) : PickerScreen() {
         is UncivShowableException -> ex.message!! // some of these seem to be translated already, but not all
         else -> "Unhandled problem, [${ex::class.simpleName}] ${ex.message}"
     }
-}
-
-private class GameList(
-    onSelected: (String) -> Unit
-) : VerticalGroup() {
-
-    private val gameDisplays = mutableMapOf<String, GameDisplay>()
-
-    private val events = EventBus.EventReceiver()
-
-    init {
-        padTop(10f)
-        padBottom(10f)
-
-        events.receive(MultiplayerGameAdded::class) {
-            val multiplayerGame = UncivGame.Current.onlineMultiplayer.getGameByName(it.name)
-            if (multiplayerGame == null) return@receive
-            addGame(it.name, multiplayerGame.preview, multiplayerGame.error, onSelected)
-        }
-        events.receive(MultiplayerGameNameChanged::class) {
-            val gameDisplay = gameDisplays.remove(it.name)
-            if (gameDisplay == null) return@receive
-            gameDisplay.changeName(it.newName)
-            gameDisplays[it.newName] = gameDisplay
-            children.sort()
-        }
-        events.receive(MultiplayerGameDeleted::class) {
-            val gameDisplay = gameDisplays.remove(it.name)
-            if (gameDisplay == null) return@receive
-            gameDisplay.remove()
-        }
-        for (game in UncivGame.Current.onlineMultiplayer.games) {
-            addGame(game.name, game.preview, game.error, onSelected)
-        }
-    }
-
-    private fun addGame(name: String, preview: GameInfoPreview?, error: Exception?, onSelected: (String) -> Unit) {
-        val gameDisplay = GameDisplay(name, preview, error, onSelected)
-        gameDisplays[name] = gameDisplay
-        addActor(gameDisplay)
-        children.sort()
-    }
-}
-
-private class GameDisplay(
-    multiplayerGameName: String,
-    preview: GameInfoPreview?,
-    error: Exception?,
-    private val onSelected: (String) -> Unit
-) : Table(), Comparable<GameDisplay> {
-    var gameName: String = multiplayerGameName
-        private set
-    val gameButton = TextButton(gameName, BaseScreen.skin)
-    val turnIndicator = createIndicator("OtherIcons/ExclamationMark")
-    val errorIndicator = createIndicator("StatIcons/Malcontent")
-    val refreshIndicator = createIndicator("EmojiIcons/Turn")
-    val statusIndicators = HorizontalGroup()
-
-    val events = EventBus.EventReceiver()
-
-    init {
-        padBottom(5f)
-
-        updateTurnIndicator(preview)
-        updateErrorIndicator(error != null)
-        add(statusIndicators)
-        add(gameButton)
-        onClick { onSelected(gameName) }
-
-        val isOurGame: (HasMultiplayerGameName) -> Boolean = { it.name == gameName }
-        events.receive(MultiplayerGameUpdateStarted::class, isOurGame, {
-            statusIndicators.addActor(refreshIndicator)
-        })
-        events.receive(MultiplayerGameUpdateEnded::class, isOurGame) {
-            refreshIndicator.remove()
-        }
-        events.receive(MultiplayerGameUpdated::class, isOurGame) {
-            updateTurnIndicator(it.preview)
-            updateErrorIndicator(false)
-        }
-        events.receive(MultiplayerGameUpdateFailed::class, isOurGame) {
-            updateErrorIndicator(true)
-        }
-    }
-
-    fun changeName(newName: String) {
-        gameName = newName
-        gameButton.setText(newName)
-    }
-
-    private fun updateTurnIndicator(preview: GameInfoPreview?) {
-        if (preview?.isUsersTurn() == true) {
-            statusIndicators.addActor(turnIndicator)
-        } else {
-            turnIndicator.remove()
-        }
-    }
-
-    private fun updateErrorIndicator(hasError: Boolean) {
-        if (hasError) {
-            statusIndicators.addActor(errorIndicator)
-        } else {
-            errorIndicator.remove()
-        }
-    }
-
-    private fun createIndicator(imagePath: String): Actor {
-        val image = ImageGetter.getImage(imagePath)
-        image.setSize(50f)
-        val container = Container(image)
-        container.padRight(5f)
-        return container
-    }
-
-    override fun compareTo(other: GameDisplay): Int = gameName.compareTo(other.gameName)
-    override fun equals(other: Any?): Boolean = (other is GameDisplay) && (gameName == other.gameName)
-    override fun hashCode(): Int = gameName.hashCode()
 }
