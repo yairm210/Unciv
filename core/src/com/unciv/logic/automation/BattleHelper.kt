5efa9582e3166d4b2ca272e364ec5e69328b7956
alexban011 <alexban011@gmail.com>
2022-05-26 13:31:06 +0300

    feature

--- core/src/com/unciv/logic/automation/BattleHelper.kt
+++ core/src/com/unciv/logic/automation/BattleHelper.kt
@@ -102,6 +102,13 @@ object BattleHelper {
         if (tileCombatant.getCivInfo() == combatant.getCivInfo()) return false
         if (!combatant.getCivInfo().isAtWarWith(tileCombatant.getCivInfo())) return false
 
+        if (combatant is MapUnitCombatant && combatant.unit.baseUnit.unitType.contains("Water") &&
+                combatant.unit.baseUnit.isMelee() && !combatant.unit.civInfo.hasUnique(UniqueType.LandUnitEmbarkation)){
+            if (tileCombatant is MapUnitCombatant && tileCombatant.unit.baseUnit.unitType.contains("Water")) {
+                return false
+            }
+        }
+
         if (combatant is MapUnitCombatant &&
             combatant.unit.hasUnique(UniqueType.CanOnlyAttackUnits) &&
             combatant.unit.getMatchingUniques(UniqueType.CanOnlyAttackUnits).none { tileCombatant.matchesCategory(it.params[0]) }
