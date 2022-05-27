067be5a292aed6be1245ebf352beb4c6de613a70
Azzurite <azzurite@gmail.com>
2022-05-27 16:36:14 +0200

    4.1.4-patch1

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,8 +3,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.6.21"
     const val appName = "Unciv"
-    const val appCodeNumber = 717
-    const val appVersion = "4.1.4"
+    const val appCodeNumber = 718
+    const val appVersion = "4.1.4-patch1"
 
     const val gdxVersion = "1.11.0"
     const val roboVMVersion = "2.3.1"
