b6377f5158c83be845d867ab982a5df2cc318347
alexban011 <alexban011@gmail.com>
2022-06-09 02:11:52 +0300

    UI improvement

--- core/src/com/unciv/ui/overviewscreen/NotificationsOverviewTable.kt
+++ core/src/com/unciv/ui/overviewscreen/NotificationsOverviewTable.kt
@@ -1,12 +1,17 @@
 package com.unciv.ui.overviewscreen
 
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Touchable
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.unciv.UncivGame
 import com.unciv.logic.civilization.CivilizationInfo
 import com.unciv.logic.civilization.Notification
+import com.unciv.ui.images.ImageGetter
 import com.unciv.ui.utils.BaseScreen
+import com.unciv.ui.utils.WrappableLabel
 import com.unciv.ui.utils.onClick
+import com.unciv.ui.worldscreen.NotificationsScroll
 import com.unciv.ui.worldscreen.WorldScreen
 
 class NotificationsOverviewTable(
@@ -17,6 +22,16 @@ class NotificationsOverviewTable(
 
     val notificationLog = viewingPlayer.notificationsLog
     private val notificationTable = Table(BaseScreen.skin)
+
+
+    val scaleFactor = 0.3f
+    val inverseScaleFactor = 1f / scaleFactor
+
+    val maxWidthOfStage = 0.333f
+
+    val iconSize = 20f
+
+    private val maxEntryWidth = worldScreen.stage.width * maxWidthOfStage * inverseScaleFactor
 
     init {
         val tablePadding = 30f
@@ -32,8 +47,10 @@ class NotificationsOverviewTable(
 
         for (index in notificationLog.indices) {
             val turnCounter = notificationLog.lastIndex - index
-            notificationTable.add(notificationsArrayTable(turnCounter.toString(), notificationLog[index].notifications))
-            notificationTable.padTop(20f).row()
+            if (notificationLog[turnCounter].notifications.isNotEmpty()) {
+                notificationTable.add(notificationsArrayTable(turnCounter.toString(), notificationLog[turnCounter].notifications))
+                notificationTable.padTop(20f).row()
+            }
         }
     }
 
@@ -46,12 +63,35 @@ class NotificationsOverviewTable(
             turnTable.add("$index turn").row()
 
         for (index2 in notifications.indices) {
-            turnTable.add(notifications[index2].text)
-            turnTable.touchable = Touchable.enabled
-            turnTable.onClick {
+            val notification = Table (BaseScreen.skin)
+
+            val labelWidth = maxEntryWidth * notifications[index2].icons.size - 10f
+            val label = WrappableLabel(notifications[index2].text, labelWidth, Color.BLACK, 20)
+
+            notification.add(label)
+            notification.background = ImageGetter.getRoundedEdgeRectangle()
+            notification.touchable = Touchable.enabled
+            notification.onClick {
                 UncivGame.Current.resetToWorldScreen()
                 notifications[index2].action?.execute(worldScreen)
             }
+
+            if (notifications[index2].icons.isNotEmpty()) {
+                val ruleset = worldScreen.gameInfo.ruleSet
+                for (icon in notifications[index2].icons.reversed()) {
+                    val image: Actor = when {
+                        ruleset.technologies.containsKey(icon) -> ImageGetter.getTechIcon(icon)
+                        ruleset.nations.containsKey(icon) -> ImageGetter.getNationIndicator(ruleset.nations[icon]!!,
+                            iconSize
+                        )
+                        ruleset.units.containsKey(icon) -> ImageGetter.getUnitIcon(icon)
+                        else -> ImageGetter.getImage(icon)
+                    }
+                    notification.add(image).size(iconSize).padRight(5f)
+                }
+            }
+
+            turnTable.add(notification)
             turnTable.padTop(20f).row()
         }
         turnTable.padTop(20f).row()
