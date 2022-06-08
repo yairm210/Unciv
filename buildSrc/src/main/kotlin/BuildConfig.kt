9bad4628b9a317a84f610eecfdf17a3c7ed72070
yairm210 <yairm210@users.noreply.github.com>
2022-06-08 07:22:56 +0000

    Bump version and create initial changelog entry

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,8 +3,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.6.21"
     const val appName = "Unciv"
-    const val appCodeNumber = 721
-    const val appVersion = "4.1.7"
+    const val appCodeNumber = 722
+    const val appVersion = "4.1.8"
 
     const val gdxVersion = "1.11.0"
     const val roboVMVersion = "2.3.1"
