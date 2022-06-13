b9d15b621509adab1cd5b484126fe5b285b7ee6e
Azzurite <azzurite@gmail.com>
2022-06-13 03:37:23 +0200

    Make the loading screen nicer

--- core/src/com/unciv/ui/LoadingScreen.kt
+++ core/src/com/unciv/ui/LoadingScreen.kt
@@ -1,14 +1,30 @@
 package com.unciv.ui
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Pixmap
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.unciv.Constants
+import com.unciv.ui.popup.Popup
 import com.unciv.ui.utils.BaseScreen
-import com.unciv.ui.utils.extensions.center
-import com.unciv.ui.utils.extensions.toLabel
 
+/** A loading screen that creates a screenshot of the current screen and adds a "Loading..." popup on top of that */
 class LoadingScreen : BaseScreen() {
+    val screenshot = Texture(Pixmap.createFromFrameBuffer(0, 0, Gdx.graphics.backBufferWidth, Gdx.graphics.backBufferHeight))
     init {
-        val loadingLabel = Constants.loading.toLabel()
-        stage.addActor(loadingLabel)
-        loadingLabel.center(stage)
+        val popup = Popup(stage)
+        popup.addGoodSizedLabel(Constants.loading)
+        popup.open()
+    }
+
+    override fun render(delta: Float) {
+        val camera = stage.viewport.camera
+        camera.update()
+        val batch = stage.batch as SpriteBatch
+        batch.projectionMatrix = camera.combined
+        batch.begin()
+        batch.draw(screenshot, 0f, 0f, camera.viewportWidth, camera.viewportHeight, 0, 0, screenshot.width, screenshot.height, false, true)
+        stage.root.draw(batch, 1f)
+        batch.end()
     }
 }
