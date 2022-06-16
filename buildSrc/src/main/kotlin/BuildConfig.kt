192bc9eb468ef0bef4e28b55a7fb9daf99c7f336
Yair Morgenstern <yairm210@hotmail.com>
2022-06-16 21:20:06 +0300

    Version rollout (#7185)
    * Bump version and create initial changelog entry
    
    * Update French.properties (#7174)
    
    * Update Catalan.properties (#7173)
    
    * Update Catalan.properties
    
    * Update Catalan.properties
    
    * Update Spanish.properties (#7172)
    
    * Update Filipino.properties (#7166)
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Italian.properties (#7165)
    
    * Update Italian.properties
    
    * Update Italian.properties
    
    * Update Brazilian_Portuguese.properties (#7149)
    
    * Update Brazilian_Portuguese.properties
    
    * Fixed Floodfill_Abbreviation mistake
    
    * Update Brazilian_Portuguese.properties
    
    * Update Brazilian_Portuguese.properties
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: Joan Josep <juanjo.ng.83@gmail.com>
    Co-authored-by: Caballero Arepa <78449553+ArchDuque-Pancake@users.noreply.github.com>
    Co-authored-by: Shann Aurelle Ripalda <shannaurelleg@gmail.com>
    Co-authored-by: Giuseppe D'Addio <41149920+Smashfanful@users.noreply.github.com>
    Co-authored-by: Kalil <kalilcamera67@gmail.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,8 +3,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.7.0"
     const val appName = "Unciv"
-    const val appCodeNumber = 724
-    const val appVersion = "4.1.10"
+    const val appCodeNumber = 725
+    const val appVersion = "4.1.11"
 
     const val gdxVersion = "1.11.0"
     const val roboVMVersion = "2.3.1"
