bd9bf3730b7bcf267b33555a59dd203a55c745a7
Azzurite <azzurite@gmail.com>
2022-06-22 13:52:59 +0200

    Update initial version

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -423,7 +423,7 @@ class UncivGame(parameters: UncivGameParameters) : Game() {
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.1.13-patch1", 729)
+        val VERSION = Version("4.1.13-patch2", 730)
         //endregion
 
         lateinit var Current: UncivGame
