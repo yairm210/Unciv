5c1ca900b79f90f1299ee28d4bb9e5e441377d7a
Yair Morgenstern <yairm210@hotmail.com>
2022-07-11 10:35:56 +0300

    Version rollout (#7422)
    * Bump version and create initial changelog entry
    
    * Update Simplified_Chinese.properties (#7415)
    
    * Update Traditional_Chinese.properties (#7413)
    
    * Update French.properties (#7406)
    
    * Update French.properties
    
    * Update French.properties
    
    Revert bad (idea) changes
    
    * Update German translation (#7405)
    
    * Update Spanish.properties (#7401)
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: heipizhu4 <100454479+heipizhu4@users.noreply.github.com>
    Co-authored-by: Chia-Hung Ni <jameseses@gmail.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: MartinP <77325495+Mape6@users.noreply.github.com>
    Co-authored-by: Caballero Arepa <78449553+ArchDuque-Pancake@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,8 +3,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.7.0"
     const val appName = "Unciv"
-    const val appCodeNumber = 736
-    const val appVersion = "4.1.17-patch1"
+    const val appCodeNumber = 737
+    const val appVersion = "4.1.18"
 
     const val gdxVersion = "1.11.0"
     const val roboVMVersion = "2.3.1"
