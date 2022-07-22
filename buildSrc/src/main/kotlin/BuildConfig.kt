19997665de2adfabdfbb38df88ca34f7b33709cc
Yair Morgenstern <yairm210@hotmail.com>
2022-07-22 16:47:42 +0300

    4.1.21-patch2

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,8 +3,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.7.0"
     const val appName = "Unciv"
-    const val appCodeNumber = 741
-    const val appVersion = "4.1.21-patch1"
+    const val appCodeNumber = 742
+    const val appVersion = "4.1.21-patch2"
 
     const val gdxVersion = "1.11.0"
     const val roboVMVersion = "2.3.1"
