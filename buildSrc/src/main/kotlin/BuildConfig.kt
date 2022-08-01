e510bf4929eb2cff989ac420fd3e80b29d0a3c94
Yair Morgenstern <yairm210@hotmail.com>
2022-08-01 19:42:59 +0300

    Resolved error where missionary cost was determined by Capital, and not by cities it could actually be constructed in

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,7 +4,7 @@ object BuildConfig {
     const val kotlinVersion = "1.7.0"
     const val appName = "Unciv"
     const val appCodeNumber = 743
-    const val appVersion = "4.1.22"
+    const val appVersion = "4.2.0"
 
     const val gdxVersion = "1.11.0"
     const val roboVMVersion = "2.3.1"
