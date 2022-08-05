948b95a9dc5ecacb99ddf61915d76d6257b1fa93
Yair Morgenstern <yairm210@hotmail.com>
2022-08-05 10:49:32 +0300

    Android gradle update - explicitly state build tools version, remove desugaring

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -65,13 +65,12 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
-
-        isCoreLibraryDesugaringEnabled = true
     }
     androidResources {
         // Don't add local save files and fonts to release, obviously
         ignoreAssetsPattern = "!SaveFiles:!fonts:!maps:!music:!mods"
     }
+    buildToolsVersion = "30.0.3"
 }
 
 
@@ -130,5 +129,4 @@ dependencies {
     // Known Android Lint warning: "GradleDependency"
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.work:work-runtime-ktx:2.6.0")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
 }
