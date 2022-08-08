a4424d2ab131fac3fbd656c9bc6b781ee3d51590
OptimizedForDensity <105244635+OptimizedForDensity@users.noreply.github.com>
2022-08-08 10:13:27 -0400

    Several pathfinding optimizations (#7523)
    * Slight pathfinding optimization
    
    * Cache canReach()
    
    * More optimizations
    
    * Use hashset instead of two arraylists

--- core/src/com/unciv/logic/map/MapUnit.kt
+++ core/src/com/unciv/logic/map/MapUnit.kt
@@ -840,6 +840,7 @@ class MapUnit : IsPartOfGameInfoSerialization {
     }
 
     fun endTurn() {
+        movement.clearPathfindingCache()
         if (currentMovement > 0
             && getTile().improvementInProgress != null
             && canBuildImprovement(getTile().getTileImprovementInProgress()!!)
@@ -886,6 +887,7 @@ class MapUnit : IsPartOfGameInfoSerialization {
     }
 
     fun startTurn() {
+        movement.clearPathfindingCache()
         currentMovement = getMaxMovement().toFloat()
         attacksThisTurn = 0
         due = true
