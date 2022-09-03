67489276fc06519aefdfaaaa9db4794a8b11bc06
nick <nacro711072@yahoo.com.tw>
2022-09-03 18:35:25 +0800

    fix: lateinit property cityInfo has not been initialized issue.

--- core/src/com/unciv/logic/GameInfo.kt
+++ core/src/com/unciv/logic/GameInfo.kt
@@ -491,7 +491,12 @@ class GameInfo : IsPartOfGameInfoSerialization, HasGameInfoSerializationVersion 
 
         for (religion in religions.values) religion.setTransients(this)
 
-        for (civInfo in civilizations) civInfo.setTransients()
+        civilizations.forEach { civInfo ->
+            civInfo.setTransients()
+        }
+        civilizations.forEach { civInfo ->
+            civInfo.thingsToFocusOnForVictory = civInfo.getPreferredVictoryTypeObject()?.getThingsToFocus(civInfo) ?: setOf()
+        }
 
         convertFortify()
 
