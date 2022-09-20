0e1b530322409f0423dc938e818bd83d6d10a59f
itanasi <44038014+itanasi@users.noreply.github.com>
2022-09-19 21:16:03 -0700

    Initial yairm suggested changes

--- core/src/com/unciv/logic/automation/unit/WorkerAutomation.kt
+++ core/src/com/unciv/logic/automation/unit/WorkerAutomation.kt
@@ -327,7 +327,7 @@ class WorkerAutomation(
 
 
         if (tile.improvement == null || junkImprovement == true) {
-            if (tile.improvementInProgress != null && (tile.improvementInProgress == UnitActionType.Repair.name || unit.canBuildImprovement(tile.getTileImprovementInProgress()!!, tile))) return true
+            if (tile.improvementInProgress != null && unit.canBuildImprovement(tile.getTileImprovementInProgress()!!, tile)) return true
             val chosenImprovement = chooseImprovement(unit, tile)
             if (chosenImprovement != null && tile.canBuildImprovement(chosenImprovement, civInfo) && unit.canBuildImprovement(chosenImprovement, tile)) return true
         } else if (!tile.containsGreatImprovement() && tile.hasViewableResource(civInfo)
