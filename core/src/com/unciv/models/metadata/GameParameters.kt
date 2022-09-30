b4bc8bf7116c107f6c777334c8410d0a7431c14d
xk730 <40309144+xk730@users.noreply.github.com>
2022-09-30 17:23:36 +0800

    Update GameParameters.kt

--- core/src/com/unciv/models/metadata/GameParameters.kt
+++ core/src/com/unciv/models/metadata/GameParameters.kt
@@ -35,7 +35,7 @@ class GameParameters : IsPartOfGameInfoSerialization { // Default values are the
     var startingEra = "Ancient era"
 
     var isOnlineMultiplayer = false
-    var anyoneCanSpectate = false
+    var anyoneCantSpectate = false
     var baseRuleset: String = BaseRuleset.Civ_V_GnK.fullName
     var mods = LinkedHashSet<String>()
 
