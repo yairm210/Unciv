91bb6fa89aff6c3d0d801a5443b0bd170ed8dba2
itanasi <44038014+itanasi@users.noreply.github.com>
2022-10-05 11:29:56 -0700

    Create Cannot Move Unique (#7855)
    * Create Cannot Move Unique
    
    * Covering other potential avenues of moving
    
    * Remove transient, fix Air Units movement range UI
    
    * Better fix for AirUnits

--- core/src/com/unciv/ui/worldscreen/WorldMapHolder.kt
+++ core/src/com/unciv/ui/worldscreen/WorldMapHolder.kt
@@ -30,6 +30,7 @@ import com.unciv.models.AttackableTile
 import com.unciv.models.UncivSound
 import com.unciv.models.helpers.MapArrowType
 import com.unciv.models.helpers.MiscArrowTypes
+import com.unciv.models.ruleset.unique.UniqueType
 import com.unciv.ui.UncivStage
 import com.unciv.ui.audio.SoundPlayer
 import com.unciv.ui.images.ImageGetter
