01d95652c0ff0559116f0606d225c7d9e819e143
Yair Morgenstern <yairm210@hotmail.com>
2022-11-07 18:15:36 +0200

    Version rollout (#7992)
    * Bump version and create initial changelog entry
    
    * Update Filipino.properties (#7983)
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Catalan.properties (#7979)
    
    * little amend
    
    * Update Catalan.properties
    
    * Translate vietnamese (#7978)
    
    * update vietnam
    
    * update Vietnamese.properties
    
    * Update Bulgarian.properties (#7974)
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Shann Aurelle Ripalda <shannaurelleg@gmail.com>
    Co-authored-by: Joan Josep <juanjo.ng.83@gmail.com>
    Co-authored-by: vvinam <vvinam@gmail.com>
    Co-authored-by: Dob <100198047+dobval@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,8 +3,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.7.0"
     const val appName = "Unciv"
-    const val appCodeNumber = 760
-    const val appVersion = "4.2.16"
+    const val appCodeNumber = 761
+    const val appVersion = "4.2.17"
 
     const val gdxVersion = "1.11.0"
     const val roboVMVersion = "2.3.1"
--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -470,7 +470,7 @@ class UncivGame(parameters: UncivGameParameters) : Game() {
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.2.16", 760)
+        val VERSION = Version("4.2.17", 761)
         //endregion
 
         lateinit var Current: UncivGame
