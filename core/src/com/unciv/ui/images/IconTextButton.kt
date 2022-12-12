08369143b79321e96a8499478ab690ccf90dae2c
tunerzinc@gmail.com <vfylfhby>
2022-12-12 02:19:49 +0100

    NextTurnActions (pick tech/policy/etc) now can have icons

--- core/src/com/unciv/ui/images/IconTextButton.kt
+++ core/src/com/unciv/ui/images/IconTextButton.kt
@@ -18,7 +18,7 @@ import com.unciv.ui.utils.extensions.toLabel
  * @param fontSize Text size for [String.toLabel].
  * @param fontColor Text colour for [String.toLabel].
  */
-class IconTextButton(
+open class IconTextButton(
     text: String,
     val icon: Actor? = null,
     fontSize: Int = Constants.defaultFontSize,
@@ -27,13 +27,15 @@ class IconTextButton(
     /** [Label] instance produced by and with content and formatting as specified to [String.toLabel]. */
     val label = text.toLabel(fontColor, fontSize)
     /** Table cell containing the [icon] if any, or `null`. */
-    val iconCell: Cell<Actor>? =
+    val iconCell: Cell<Actor> =
         if (icon != null) {
             val size = fontSize.toFloat()
             icon.setSize(size,size)
             icon.setOrigin(Align.center)
             add(icon).size(size).padRight(size / 3)
-        } else null
+        } else {
+            add()
+        }
     /** Table cell instance containing the [label]. */
     val labelCell: Cell<Label> = add(label)
 }
