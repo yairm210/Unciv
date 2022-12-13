eebad0238b3917df6b9f6e24fdfac2b245e5b916
alexban011 <alexban011@gmail.com>
2022-12-13 22:50:35 +0200

    version bumped (#8124)

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,7 +1,7 @@
 package com.unciv.build
 
 object BuildConfig {
-    const val kotlinVersion = "1.7.0"
+    const val kotlinVersion = "1.7.21"
     const val appName = "Unciv"
     const val appCodeNumber = 775
     const val appVersion = "4.3.7-patch1"
