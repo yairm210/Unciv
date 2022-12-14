ed83cc995450714c5b9de22a58ca130ef691cc2e
itanasi <44038014+itanasi@users.noreply.github.com>
2022-12-13 21:45:46 -0800

    AI Pillages Neutral Roads only at War (#8087)
    * Only pillage neutral roads when at war.
    Track road ownership based on who built the road or who last owned the tile
    
    * Remove excessively safe checks
    
    * Fix potential null exception
    
    * Rework, use string
    
    * yairm suggestions
    
    * readability

--- core/src/com/unciv/ui/worldscreen/unit/UnitActions.kt
+++ core/src/com/unciv/ui/worldscreen/unit/UnitActions.kt
@@ -887,7 +887,10 @@ object UnitActions {
         if (!tile.canPillageTile()) return false
         val tileOwner = tile.getOwner()
         // Can't pillage friendly tiles, just like you can't attack them - it's an 'act of war' thing
-        return tileOwner == null || unit.civInfo.isAtWarWith(tileOwner)
+        return if (tileOwner != null)
+            unit.civInfo.isAtWarWith(tileOwner)
+        else if (tile.canPillageTile()) true
+        else (tile.canPillageRoad() && tile.roadOwner != "" && unit.civInfo.isAtWarWith(tile.getRoadOwner()!!))
     }
 
     private fun addGiftAction(unit: MapUnit, actionList: ArrayList<UnitAction>, tile: TileInfo) {
