d6f7405d15711f9ad3cfb9c742ab3f5b3b362cce
Philip Keiter <FiretronP75@users.noreply.github.com>
2022-12-21 18:34:26 -0600

    Add new game option: No City Razing.

--- core/src/com/unciv/ui/newgamescreen/GameOptionsTable.kt
+++ core/src/com/unciv/ui/newgamescreen/GameOptionsTable.kt
@@ -65,6 +65,7 @@ class GameOptionsTable(
 
 
         val checkboxTable = Table().apply { defaults().left().pad(2.5f) }
+        checkboxTable.addNoCityRazingCheckbox()
         checkboxTable.addNoBarbariansCheckbox()
         checkboxTable.addRagingBarbariansCheckbox()
         checkboxTable.addOneCityChallengeCheckbox()
@@ -88,6 +89,10 @@ class GameOptionsTable(
         checkbox.isDisabled = lockable && locked
         add(checkbox).colspan(2).row()
     }
+
+    private fun Table.addNoCityRazingCheckbox() =
+            addCheckbox("No City Razing", gameParameters.noCityRazing)
+            { gameParameters.noCityRazing = it }
 
     private fun Table.addNoBarbariansCheckbox() =
             addCheckbox("No Barbarians", gameParameters.noBarbarians)
