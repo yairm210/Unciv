969579957ff0ac9b3d25561ad166f6fc67e97a93
Yair Morgenstern <yairm210@hotmail.com>
2023-01-12 18:59:54 +0200

    Better bonus resource color

--- core/src/com/unciv/models/ruleset/tile/ResourceType.kt
+++ core/src/com/unciv/models/ruleset/tile/ResourceType.kt
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.Color
 enum class ResourceType(val color:String) {
     Luxury("#ffd800"),
     Strategic("#c14d00"),
-    Bonus("#aaaaaa");
+    Bonus("#a8c3c9");
 
     fun getColor() : Color {
         return Color.valueOf(color)
