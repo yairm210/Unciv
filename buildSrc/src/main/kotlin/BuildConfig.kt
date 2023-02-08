1e0a0fb6c136700a5ffc83bf3a89370c2179c5b7
Yair Morgenstern <yairm210@hotmail.com>
2023-02-08 15:09:37 +0200

    4.4.12-patch1

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,18 +3,9 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.8.0"
     const val appName = "Unciv"
-    const val appCodeNumber = 809
-    const val appVersion = "4.4.12"
+    const val appCodeNumber = 810
+    const val appVersion = "4.4.12-patch1"
 
     const val gdxVersion = "1.11.0"
     const val roboVMVersion = "2.3.1"
 }
-
-/**
- * OLD:
- * 10852
- *
- * NEW:
- * 10258 for 5
- * ---
- */
