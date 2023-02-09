bf0dd071e0027227fe86e0ad7e2fdb5d61c6ec34
Yair Morgenstern <yairm210@hotmail.com>
2023-02-09 19:09:58 +0200

    Resolved #8637 - fixed air sweep crash Due to air sweep tiles being "attackable tiles" even when not containing combatants

--- core/src/com/unciv/logic/automation/unit/AttackableTile.kt
+++ core/src/com/unciv/logic/automation/unit/AttackableTile.kt
@@ -5,4 +5,4 @@ import com.unciv.logic.map.tile.Tile
 
 class AttackableTile(val tileToAttackFrom: Tile, val tileToAttack: Tile,
                      val movementLeftAfterMovingToAttackTile:Float,
-                     /** This is only for debug purposes */ val combatant:ICombatant)
+                     /** This is only for debug purposes */ val combatant:ICombatant?)
