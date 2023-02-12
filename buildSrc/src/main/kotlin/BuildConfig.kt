03bb0b3b7989ef8e9bcd43c34b14777516b060d7
Yair Morgenstern <yairm210@hotmail.com>
2023-02-12 20:04:50 +0200

    Version rollout (#8663)
    * Bump version and create initial changelog entry
    
    * Update Polish.properties (#8662)
    
    * Update Filipino.properties (#8659)
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Czech.properties (#8657)
    
    * Update Hungarian.properties (#8642)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Lesiakower <125187776+Lesiakower@users.noreply.github.com>
    Co-authored-by: Shann Aurelle Ripalda <shannaurelleg@gmail.com>
    Co-authored-by: Weverak <74535809+Weverak@users.noreply.github.com>
    Co-authored-by: balazsszalab <balazsszalab@freemail.hu>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,8 +3,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.8.0"
     const val appName = "Unciv"
-    const val appCodeNumber = 811
-    const val appVersion = "4.4.13-gp"
+    const val appCodeNumber = 812
+    const val appVersion = "4.4.14"
 
     const val gdxVersion = "1.11.0"
     const val roboVMVersion = "2.3.1"
