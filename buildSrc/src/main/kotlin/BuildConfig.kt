cff8876ec844d8266fcc0bfb80ba854ff4f20a4b
Yair Morgenstern <yairm210@hotmail.com>
2023-02-26 10:13:25 +0200

    4.5.0-patch3 Fixed 'unitsToRemove' error

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,8 +3,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.8.0"
     const val appName = "Unciv"
-    const val appCodeNumber = 821
-    const val appVersion = "4.5.0-patch2"
+    const val appCodeNumber = 822
+    const val appVersion = "4.5.0-patch3"
 
     const val gdxVersion = "1.11.0"
     const val roboVMVersion = "2.3.1"
