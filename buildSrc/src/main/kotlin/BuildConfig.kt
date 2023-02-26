526741c11afa79d38b674000534e21b678b5ed3d
Yair Morgenstern <yairm210@hotmail.com>
2023-02-26 09:32:51 +0200

    4.5.0-patch2 Lazy loading of system fonts so startup is fast

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,8 +3,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.8.0"
     const val appName = "Unciv"
-    const val appCodeNumber = 820
-    const val appVersion = "4.5.0-patch1"
+    const val appCodeNumber = 821
+    const val appVersion = "4.5.0-patch2"
 
     const val gdxVersion = "1.11.0"
     const val roboVMVersion = "2.3.1"
