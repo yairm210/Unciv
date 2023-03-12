18ae19658b0ad57a48f40cd8c3134a2704eab241
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2023-03-12 19:11:31 +0100

    Make City center minimum tile yields moddable - patch1

--- core/src/com/unciv/logic/map/tile/TileStatFunctions.kt
+++ core/src/com/unciv/logic/map/tile/TileStatFunctions.kt
@@ -77,6 +77,12 @@ class TileStatFunctions(val tile: Tile) {
         return stats
     }
 
+    /** Ensures each stat is >= [other].stat - modifies in place */
+    private fun Stats.coerceAtLeast(other: Stats) {
+        for ((stat, value) in other)
+            if (this[stat] < value) this[stat] = value
+    }
+
     /** Gets basic stats to start off [getTileStats] or [getTileStartYield], independently mutable result */
     private fun getTerrainStats(): Stats {
         var stats: Stats? = null
