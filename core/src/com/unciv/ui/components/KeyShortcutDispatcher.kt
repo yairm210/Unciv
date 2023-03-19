863163084fe8e2e7a374c0798c1b3292e4ca7a48
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2023-03-19 17:55:42 +0100

    Keyboard Bindings Step 5: Late resolving (#8923)

--- core/src/com/unciv/ui/components/KeyShortcutDispatcher.kt
+++ core/src/com/unciv/ui/components/KeyShortcutDispatcher.kt
@@ -1,7 +1,15 @@
 package com.unciv.ui.components
 
 open class KeyShortcutDispatcher {
-    data class KeyShortcut(val key: KeyCharAndCode, val priority: Int = 0)
+    /**
+     * @param key       The hardcoded key that will be bound to an action through the [add] methods -OR-
+     * @param binding   The abstract [KeyboardBinding] that will be bound to an action through the [add] methods
+     * @param priority  Used by the [Resolver] - only the actions bound to the incoming key with the _highest priority_ will run
+     */
+    data class KeyShortcut(val binding: KeyboardBinding, val key: KeyCharAndCode, val priority: Int = 0) {
+        /** Debug helper */
+        override fun toString() = if (binding.hidden) "$key@$priority" else "$binding@$priority"
+    }
 
     private data class ShortcutAction(val shortcut: KeyShortcut, val action: () -> Unit)
     private val shortcuts: MutableList<ShortcutAction> = mutableListOf()
@@ -12,13 +20,13 @@ open class KeyShortcutDispatcher {
         shortcuts.add(ShortcutAction(shortcut, action))
     }
 
-    fun add(binding: KeyboardBinding, action: (() -> Unit)?) {
-        add(KeyboardBindings[binding], action)
+    fun add(binding: KeyboardBinding, priority: Int = 1, action: (() -> Unit)?) {
+        add(KeyShortcut(binding, KeyCharAndCode.UNKNOWN, priority), action)
     }
 
     fun add(key: KeyCharAndCode?, action: (() -> Unit)?) {
         if (key != null)
-            add(KeyShortcut(key), action)
+            add(KeyShortcut(KeyboardBinding.None, key), action)
     }
 
     fun add(char: Char?, action: (() -> Unit)?) {
@@ -36,7 +44,7 @@ open class KeyShortcutDispatcher {
     }
 
     fun remove(binding: KeyboardBinding) {
-        remove(KeyboardBindings[binding])
+        shortcuts.removeIf { it.shortcut.binding == binding }
     }
 
     fun remove(key: KeyCharAndCode?) {
@@ -62,15 +70,17 @@ open class KeyShortcutDispatcher {
             if (!dispatcher.isActive()) return
 
             for ((shortcut, action) in dispatcher.shortcuts) {
-                if (shortcut.key == key) {
-                    if (shortcut.priority == priority)
-                        triggeredActions.add(action)
-                    else if (shortcut.priority > priority) {
-                        priority = shortcut.priority
-                        triggeredActions.clear()
-                        triggeredActions.add(action)
-                    }
+                var (binding, shortcutKey, shortcutPriority) = shortcut
+                if (binding != KeyboardBinding.None) {
+                    shortcutKey = KeyboardBindings[binding]
+                    if (shortcutKey == binding.defaultKey) shortcutPriority--
                 }
+                if (shortcutKey != key || shortcutPriority < priority) continue
+                if (shortcutPriority > priority) {
+                    priority = shortcutPriority
+                    triggeredActions.clear()
+                }
+                triggeredActions.add(action)
             }
         }
     }
