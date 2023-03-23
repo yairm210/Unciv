c1720af91787a086a247d0276ff3eb11204b5301
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2023-03-23 14:54:00 +0100

    Linting and a little hardening of the UI-Skins code

--- core/src/com/unciv/models/skins/SkinCache.kt
+++ core/src/com/unciv/models/skins/SkinCache.kt
@@ -22,38 +22,40 @@ object SkinCache : HashMap<String, SkinConfig>() {
     fun assembleSkinConfigs(ruleSetMods: Set<String>) {
         // Needs to be a list and not a set, so subsequent mods override the previous ones
         // Otherwise you rely on hash randomness to determine override order... not good
-        val mods = mutableListOf("")
+        val mods = mutableListOf("")  // Not an emptyList - placeholder for built-in skin
         if (UncivGame.isCurrentInitialized()) {
             mods.addAll(UncivGame.Current.settings.visualMods)
         }
         mods.addAll(ruleSetMods)
         clear()
         for (mod in mods.distinct()) {
-            for (entry in allConfigs.entries.filter { it.key.mod == mod } ) { // Built-in skins all have empty strings as their `.mod`, so loop through all of them.
-                val skin = entry.key.skin
-                if (skin in this) this[skin]!!.updateConfig(entry.value)
-                else this[skin] = entry.value.clone()
+            for ((key, config) in allConfigs.filter { it.key.mod == mod } ) { // Built-in skins all have empty strings as their `.mod`, so loop through all of them.
+                val skin = key.skin
+                if (skin in this) this[skin]!!.updateConfig(config)
+                else this[skin] = config.clone()
             }
         }
     }
 
-    fun loadSkinConfigs(consoleMode: Boolean = false){
+    fun loadSkinConfigs(consoleMode: Boolean = false) {
         allConfigs.clear()
         var skinName = ""
 
         //load internal Skins
         val fileHandles: Sequence<FileHandle> =
                 if (consoleMode) FileHandle("jsons/Skins").list().asSequence()
-                else ImageGetter.getAvailableSkins().map { Gdx.files.internal("jsons/Skins/$it.json")}.filter { it.exists() }
+                else ImageGetter.getAvailableSkins()
+                    .map { Gdx.files.internal("jsons/Skins/$it.json") }
+                    .filter { it.exists() }
 
-        for (configFile in fileHandles){
+        for (configFile in fileHandles) {
             skinName = configFile.nameWithoutExtension().removeSuffix("Config")
             try {
                 val key = SkinAndMod(skinName, "")
                 assert(key !in allConfigs)
                 allConfigs[key] = json().fromJsonFile(SkinConfig::class.java, configFile)
                 debug("SkinConfig loaded successfully: %s", configFile.name())
-            } catch (ex: Exception){
+            } catch (ex: Exception) {
                 debug("Exception loading SkinConfig '%s':", configFile.path())
                 debug("  %s", ex.localizedMessage)
                 debug("  (Source file %s line %s)", ex.stackTrace[0].fileName, ex.stackTrace[0].lineNumber)
@@ -70,21 +72,21 @@ object SkinCache : HashMap<String, SkinConfig>() {
             if (modName.startsWith('.')) continue
             if (!modFolder.isDirectory) continue
 
-            try {
-                for (configFile in modFolder.child("jsons/Skins").list()){
+            for (configFile in modFolder.child("jsons/Skins").list()) {
+                try {
                     skinName = configFile.nameWithoutExtension().removeSuffix("Config")
                     val key = SkinAndMod(skinName, modName)
                     assert(key !in allConfigs)
                     allConfigs[key] = json().fromJsonFile(SkinConfig::class.java, configFile)
                     debug("Skin loaded successfully: %s", configFile.path())
+                } catch (ex: Exception) {
+                    debug("Exception loading Skin '%s/jsons/Skins/%s':", modFolder.name(), skinName)
+                    debug("  %s", ex.localizedMessage)
+                    debug("  (Source file %s line %s)", ex.stackTrace[0].fileName, ex.stackTrace[0].lineNumber)
                 }
-            } catch (ex: Exception){
-                debug("Exception loading Skin '%s/jsons/Skins/%s':", modFolder.name(), skinName)
-                debug("  %s", ex.localizedMessage)
-                debug("  (Source file %s line %s)", ex.stackTrace[0].fileName, ex.stackTrace[0].lineNumber)
             }
         }
 
-        assembleSkinConfigs(hashSetOf()) // no game is loaded, this is just the initial game setup
+        assembleSkinConfigs(emptySet()) // no game is loaded, this is just the initial game setup
     }
 }
