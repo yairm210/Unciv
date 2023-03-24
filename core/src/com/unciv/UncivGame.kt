b9bf07263a5fc682ea89c7f830ce3fa51d64a816
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2023-03-24 19:24:32 +0100

    Fix TileSet mods can lock user out

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -189,7 +189,9 @@ open class UncivGame(val isConsoleMode: Boolean = false) : Game(), PlatformSpeci
 
         ImageGetter.resetAtlases()
         ImageGetter.setNewRuleset(ImageGetter.ruleset)  // This needs to come after the settings, since we may have default visual mods
-        if (settings.tileSet !in ImageGetter.getAvailableTilesets()) { // If one of the tilesets is no longer available, default back
+        val availableTileSets = ImageGetter.getAvailableTilesets().toSet()
+            .intersect(TileSetCache.getAvailableTilesets().toSet())
+        if (settings.tileSet !in availableTileSets) { // If the configured tileset is no longer available, default back
             settings.tileSet = Constants.defaultTileset
         }
 
