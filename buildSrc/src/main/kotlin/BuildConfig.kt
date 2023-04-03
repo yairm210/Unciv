35d5b8c43cd52454675e6ffbeb171f26a66351b0
Yair Morgenstern <yairm210@hotmail.com>
2023-04-03 14:53:35 +0300

    Version rollout (#9100)
    * Bump version and create initial changelog entry
    
    * Updated Brazilian_Portuguese.properties (#9094)
    
    * Update Ukrainian.properties (#9093)
    
    * Update Catalan.properties (#9090)
    
    * Update Spanish.properties (#9088)
    
    All tutorial page text
    
    * Update Hungarian.properties (#9084)
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Simplified_Chinese.properties (#9083)
    
    - add more translation
    
    * Update French.properties (#9082)
    
    * Update Turkish.properties (#9081)
    
    Add and revise translations
    
    * Update Bulgarian.properties (#9080)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>
    Co-authored-by: Gualdimar <Gualdimar@users.noreply.github.com>
    Co-authored-by: Joan Josep <juanjo.ng.83@gmail.com>
    Co-authored-by: Caballero Arepa <78449553+Caballero-Arepa@users.noreply.github.com>
    Co-authored-by: balazsszalab <balazsszalab@freemail.hu>
    Co-authored-by: Tim <2884320+tim-hub@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: omerfaruk-cakmak <32402595+omerfaruk-cakmak@users.noreply.github.com>
    Co-authored-by: Bogomil Stefanov <101423783+vynaaa@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,8 +3,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.8.0"
     const val appName = "Unciv"
-    const val appCodeNumber = 840
-    const val appVersion = "4.5.15"
+    const val appCodeNumber = 841
+    const val appVersion = "4.5.16"
 
     const val gdxVersion = "1.11.0"
     const val roboVMVersion = "2.3.1"
