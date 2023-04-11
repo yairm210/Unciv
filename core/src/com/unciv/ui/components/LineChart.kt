a5cdf23068080380eeffb35853f018da1c41f8d2
WhoIsJohannes <johannesrummel@gmail.com>
2023-04-11 14:08:36 +0200

    Support negative values in the chart

--- core/src/com/unciv/ui/components/LineChart.kt
+++ core/src/com/unciv/ui/components/LineChart.kt
@@ -15,6 +15,7 @@ import com.unciv.ui.screens.victoryscreen.VictoryScreenCivGroup
 import com.unciv.ui.screens.victoryscreen.VictoryScreenCivGroup.DefeatedPlayerStyle
 import kotlin.math.ceil
 import kotlin.math.log10
+import kotlin.math.max
 import kotlin.math.pow
 
 private data class DataPoint(val x: Int, val y: Int, val civ: Civilization)
@@ -51,6 +52,9 @@ class LineChart(
     private val xLabels: List<Int>
     private val yLabels: List<Int>
 
+    private val hasNegativeYValues: Boolean
+    private val negativeYLabel: Int
+
     private val dataPoints: List<DataPoint> = data.flatMap { turn ->
         turn.value.map { (civ, value) ->
             DataPoint(turn.key, value, civ)
@@ -58,11 +62,21 @@ class LineChart(
     }
 
     init {
+        hasNegativeYValues = dataPoints.any { it.y < 0 }
         xLabels = generateLabels(dataPoints.maxOf { it.x })
         yLabels = generateLabels(dataPoints.maxOf { it.y })
+        val lowestValue = dataPoints.minOf { it.y }
+        negativeYLabel = if (hasNegativeYValues) -getNextNumberDivisibleByPowOfTen(-lowestValue) else 0
     }
 
     private fun generateLabels(maxValue: Int): List<Int> {
+        val maxLabelValue = getNextNumberDivisibleByPowOfTen(maxValue)
+        val stepSize = ceil(maxLabelValue.toFloat() / maxLabels).toInt()
+        // `maxLabels + 1` because we want to end at `maxLabels * stepSize`.
+        return (0 until maxLabels + 1).map { (it * stepSize) }
+    }
+
+    private fun getNextNumberDivisibleByPowOfTen(maxValue: Int): Int {
         val numberOfDigits = ceil(log10(maxValue.toDouble())).toInt()
         val maxLabelValue = when {
             numberOfDigits <= 0 -> 1
@@ -74,16 +88,14 @@ class LineChart(
                 ceil(maxValue.toDouble() / oneWithZeros).toInt() * oneWithZeros.toInt()
             }
         }
-        val stepSize = ceil(maxLabelValue.toFloat() / maxLabels).toInt()
-        // `maxLabels + 1` because we want to end at `maxLabels * stepSize`.
-        return (0 until maxLabels + 1).map { (it * stepSize) }
+        return maxLabelValue
     }
 
     override fun draw(batch: Batch, parentAlpha: Float) {
         super.draw(batch, parentAlpha)
 
         // Save the current batch transformation matrix
-         val oldTransformMatrix = batch.transformMatrix.cpy()
+        val oldTransformMatrix = batch.transformMatrix.cpy()
         // Set the batch transformation matrix to the local coordinates of the LineChart widget
         val stageCoords = localToStageCoordinates(Vector2(0f, 0f))
         batch.transformMatrix = Matrix4().translate(stageCoords.x, stageCoords.y, 0f)
@@ -93,7 +105,9 @@ class LineChart(
         val labelHeight = Label("123", Label.LabelStyle(Fonts.font, axisLabelColor)).height
         val yLabelsAsLabels =
                 yLabels.map { Label(it.toString(), Label.LabelStyle(Fonts.font, axisLabelColor)) }
-        val widestYLabelWidth = yLabelsAsLabels.maxOf { it.width }
+        val negativeYLabelAsLabel =
+                Label(negativeYLabel.toString(), Label.LabelStyle(Fonts.font, axisLabelColor))
+        val widestYLabelWidth = max(yLabelsAsLabels.maxOf { it.width }, negativeYLabelAsLabel.width)
         // We assume here that all labels have the same height. We need to deduct the height of
         // a label from the available height, because otherwise the label on the top would
         // overrun the height since the (x,y) coordinates define the bottom left corner of the
@@ -106,32 +120,38 @@ class LineChart(
                 xAxisLabelsHeight + axisToLabelPadding + axisLineWidth / 2 - zeroYAxisLabelHeight / 2
         val yAxisLabelYRange = yAxisLabelMaxY - yAxisLabelMinY
 
-        // We draw the y-axis labels second. They will take away some space on the left of the
+        // We draw the y-axis labels first. They will take away some space on the left of the
         // widget which we need to consider when drawing the rest of the graph.
-        yLabelsAsLabels.forEachIndexed { index, label ->
-            val yPos = yAxisLabelMinY + index * (yAxisLabelYRange / (yLabels.size - 1))
+        var yAxisYPosition = 0f
+        val negativeOrientationLineYPosition = yAxisLabelMinY + labelHeight / 2
+        val yLabelsToDraw = if (hasNegativeYValues) listOf(negativeYLabelAsLabel) + yLabelsAsLabels else yLabelsAsLabels
+        yLabelsToDraw.forEachIndexed { index, label ->
+            val yPos = yAxisLabelMinY + index * (yAxisLabelYRange / (yLabelsToDraw.size - 1))
             label.setPosition((widestYLabelWidth - label.width) / 2, yPos)
             label.draw(batch, 1f)
 
-            // Draw y-axis orientation lines
-            if (index > 0) drawLine(
+            // Draw y-axis orientation lines and x-axis
+            val zeroIndex = if (hasNegativeYValues) 1 else 0
+            drawLine(
                 batch,
                 widestYLabelWidth + axisToLabelPadding + axisLineWidth,
                 yPos + labelHeight / 2,
                 chartWidth,
                 yPos + labelHeight / 2,
-                orientationLineColor,
-                orientationLineWidth
+                if (index != zeroIndex) orientationLineColor else axisColor,
+                if (index != zeroIndex) orientationLineWidth else axisLineWidth
             )
+            if (index == zeroIndex) {
+                yAxisYPosition = yPos + labelHeight / 2
+            }
         }
 
         // Draw x-axis labels
         val xLabelsAsLabels =
                 xLabels.map { Label(it.toString(), Label.LabelStyle(Fonts.font, axisLabelColor)) }
-        val firstXAxisLabelWidth = xLabelsAsLabels[0].width
         val lastXAxisLabelWidth = xLabelsAsLabels[xLabelsAsLabels.size - 1].width
         val xAxisLabelMinX =
-                widestYLabelWidth + axisToLabelPadding + axisLineWidth / 2 - firstXAxisLabelWidth / 2
+                widestYLabelWidth + axisToLabelPadding + axisLineWidth / 2
         val xAxisLabelMaxX = chartWidth - lastXAxisLabelWidth / 2
         val xAxisLabelXRange = xAxisLabelMaxX - xAxisLabelMinX
         xLabels.forEachIndexed { index, labelAsInt ->
@@ -140,40 +160,31 @@ class LineChart(
             label.setPosition(xPos - label.width / 2, 0f)
             label.draw(batch, 1f)
 
-            // Draw x-axis orientation lines
-            if (index > 0) drawLine(
+            // Draw x-axis orientation lines and y-axis
+            drawLine(
                 batch,
                 xPos,
                 labelHeight + axisToLabelPadding + axisLineWidth,
                 xPos,
                 chartHeight,
-                orientationLineColor,
-                orientationLineWidth
+                if (index > 0) orientationLineColor else axisColor,
+                if (index >0) orientationLineWidth else axisLineWidth
             )
         }
-
-        // Draw y-axis
-        val yAxisX = widestYLabelWidth + axisToLabelPadding + axisLineWidth / 2
-        val xAxisY = labelHeight + axisToLabelPadding + axisLineWidth / 2
-        drawLine(batch, yAxisX, xAxisY, yAxisX, chartHeight, axisColor, axisLineWidth)
-
-        // Draw x-axis
-        val xAxisRight = chartWidth
-        drawLine(batch, yAxisX, xAxisY, xAxisRight, xAxisY, axisColor, axisLineWidth)
-
 
         // Draw line charts for each color
         val linesMinX = widestYLabelWidth + axisToLabelPadding + axisLineWidth
         val linesMaxX = chartWidth - lastXAxisLabelWidth / 2
-        val linesMinY = labelHeight + axisToLabelPadding + axisLineWidth
+        val linesMinY = yAxisYPosition
         val linesMaxY = chartHeight - labelHeight / 2
         val scaleX = (linesMaxX - linesMinX) / xLabels.max()
         val scaleY = (linesMaxY - linesMinY) / yLabels.max()
+        val negativeScaleY = if (hasNegativeYValues) (linesMinY - negativeOrientationLineYPosition) / -negativeYLabel else 0f
         val sortedPoints = dataPoints.sortedBy { it.x }
         val pointsByCiv = sortedPoints.groupBy { it.civ }
         // We want the current player civ to be drawn last, so it is never overlapped by another player.
         val civIterationOrder =
-        // By default the players with the highest points will be drawn last (i.e. they will
+                 // By default the players with the highest points will be drawn last (i.e. they will
                 // overlap others).
                 pointsByCiv.keys.toList().sortedBy { lastTurnDataPoints[it]!!.y }
                     .toMutableList()
@@ -187,13 +198,12 @@ class LineChart(
             for (i in 1 until points.size) {
                 val prevPoint = points[i - 1]
                 val currPoint = points[i]
-                // See TODO at the top of the file. We currently don't support negative values.
-                if (prevPoint.y < 0) continue
-                if (currPoint.y < 0) continue
+                val prevPointYScale = if (prevPoint.y < 0f) negativeScaleY else scaleY
+                val currPointYScale = if (currPoint.y < 0f) negativeScaleY else scaleY
                 drawLine(
                     batch,
-                    linesMinX + prevPoint.x * scaleX, linesMinY + prevPoint.y * scaleY,
-                    linesMinX + currPoint.x * scaleX, linesMinY + currPoint.y * scaleY,
+                    linesMinX + prevPoint.x * scaleX, linesMinY + prevPoint.y * prevPointYScale,
+                    linesMinX + currPoint.x * scaleX, linesMinY + currPoint.y * currPointYScale,
                     civ.nation.getOuterColor(), chartLineWidth
                 )
 
@@ -212,7 +222,7 @@ class LineChart(
                     selectedCivIcon.run {
                         setPosition(
                             linesMinX + currPoint.x * scaleX,
-                            linesMinY + currPoint.y * scaleY,
+                            linesMinY + currPoint.y * currPointYScale,
                             Align.center
                         )
                         setSize(33f, 33f) // Dead Civs need this
