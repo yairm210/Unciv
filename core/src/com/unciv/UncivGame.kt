2fbd24231f0fd9e415be212a45d5a27b1b3f2f9e
Yair Morgenstern <yairm210@hotmail.com>
2023-04-13 17:41:59 +0300

    Resolved #9177 - Tileset no longer reverts to default on Android after restarting game

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -189,8 +189,8 @@ open class UncivGame(val isConsoleMode: Boolean = false) : Game(), PlatformSpeci
 
         ImageGetter.resetAtlases()
         ImageGetter.setNewRuleset(ImageGetter.ruleset)  // This needs to come after the settings, since we may have default visual mods
-        val availableTileSets = ImageGetter.getAvailableTilesets().toSet()
-            .intersect(TileSetCache.getAvailableTilesets().toSet())
+        val imageGetterTilesets = ImageGetter.getAvailableTilesets()
+        val availableTileSets = TileSetCache.getAvailableTilesets(imageGetterTilesets)
         if (settings.tileSet !in availableTileSets) { // If the configured tileset is no longer available, default back
             settings.tileSet = Constants.defaultTileset
         }
