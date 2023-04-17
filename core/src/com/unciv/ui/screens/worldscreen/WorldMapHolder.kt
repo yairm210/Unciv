efb855f978287f1fb5272913b400ee7e5433cf0a
Yair Morgenstern <yairm210@hotmail.com>
2023-04-17 21:04:08 +0300

    4.6.3-patch1

--- core/src/com/unciv/ui/screens/worldscreen/WorldMapHolder.kt
+++ core/src/com/unciv/ui/screens/worldscreen/WorldMapHolder.kt
@@ -643,16 +643,16 @@ class WorldMapHolder(
                 if (tile.aerialDistanceTo(unit.getTile()) <= unit.getRange()) {
                     // The tile is within attack range
                     group.layerMisc.overlayTerrain(Color.RED)
-                } else if (tile.isExplored(worldScreen.viewingCiv)) {
+                } else if (tile.isExplored(worldScreen.viewingCiv) && tile.aerialDistanceTo(unit.getTile()) <= unit.getRange()*2) {
                     // The tile is within move range
-                    group.layerMisc.overlayTerrain(Color.BLUE)
+                    group.layerMisc.overlayTerrain(if (unit.movement.canMoveTo(tile)) Color.WHITE else Color.BLUE)
                 }
             }
 
             // Highlight tile unit can move to
             if (unit.movement.canMoveTo(tile) ||
                     unit.movement.isUnknownTileWeShouldAssumeToBePassable(tile) && !unit.baseUnit.movesLikeAirUnits()) {
-                val alpha = if (UncivGame.Current.settings.singleTapMove || isAirUnit) 0.7f else 0.3f
+                val alpha = if (UncivGame.Current.settings.singleTapMove) 0.7f else 0.3f
                 group.layerOverlay.showHighlight(moveTileOverlayColor, alpha)
             }
 
