0bf5cae401e6863ff9e5fac7eff28768c483e0c0
Yair Morgenstern <yairm210@hotmail.com>
2023-04-17 08:49:19 +0300

    Solved RAM crashes hopefully :)

--- core/src/com/unciv/ui/components/Fonts.kt
+++ core/src/com/unciv/ui/components/Fonts.kt
@@ -295,6 +295,9 @@ object Fonts {
         Gdx.gl.glReadPixels(0, 0, w, h, GL20.GL_RGBA, GL20.GL_UNSIGNED_BYTE, pixmap.pixels)
         frameBuffer.end()
 
+        spriteBatch.dispose()
+        frameBuffer.dispose()
+
         // Pixmap is now *upside down* so we need to flip it around the y axis
         for (i in 0..w)
             for (j in 0..h/2) {
