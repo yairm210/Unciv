7ccd0701df7fa13913cdcb601bae9a401e8a5256
alexban011 <alexban011@gmail.com>
2023-05-10 01:21:05 +0300

    fix: resolve EmptyCatchBlock warnings

--- core/src/com/unciv/models/ruleset/Ruleset.kt
+++ core/src/com/unciv/models/ruleset/Ruleset.kt
@@ -252,7 +252,9 @@ class Ruleset {
             try {
                 modOptions = json().fromJsonFile(ModOptions::class.java, modOptionsFile)
                 modOptions.updateDeprecations()
-            } catch (ex: Exception) {}
+            } catch (ex: Exception) {
+                Log.error("Failed to get modOptions from json file", ex)
+            }
             modOptions.uniqueObjects = modOptions.uniques.map { Unique(it, UniqueTarget.ModOptions) }
             modOptions.uniqueMap = modOptions.uniqueObjects.groupBy { it.placeholderText }
         }
