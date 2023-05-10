be20c917ccf0d4e7cfe3b91ae52adfa74842eb69
Yair Morgenstern <yairm210@hotmail.com>
2023-05-10 10:28:44 +0300

    4.6.10-patch2 Resolved #9363 #9356 - solved font crash exception

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -531,7 +531,7 @@ open class UncivGame(val isConsoleMode: Boolean = false) : Game(), PlatformSpeci
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.6.10-patch1", 863)
+        val VERSION = Version("4.6.10-patch2", 864)
         //endregion
 
         lateinit var Current: UncivGame
