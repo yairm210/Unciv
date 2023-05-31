ce6d3394bc6e10380cf1aae727ab81228e2283ca
Yair Morgenstern <yairm210@hotmail.com>
2023-05-31 14:58:00 +0300

    Added checksum to be added to uploaded multiplayer games (#9483)
    * Added checksum to be added to uploaded multiplayer games
    
    * Policy adoption triggers resource recalculation

--- core/src/com/unciv/logic/multiplayer/storage/OnlineMultiplayerServer.kt
+++ core/src/com/unciv/logic/multiplayer/storage/OnlineMultiplayerServer.kt
@@ -102,7 +102,7 @@ class OnlineMultiplayerServer(
      * @throws MultiplayerAuthException if the authentication failed
      */
     suspend fun tryUploadGame(gameInfo: GameInfo, withPreview: Boolean) {
-        val zippedGameInfo = UncivFiles.gameInfoToString(gameInfo, forceZip = true)
+        val zippedGameInfo = UncivFiles.gameInfoToString(gameInfo, forceZip = true, updateChecksum = true)
         fileStorage().saveFileData(gameInfo.gameId, zippedGameInfo)
 
         // We upload the preview after the game because otherwise the following race condition will happen:
