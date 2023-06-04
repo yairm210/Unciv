cd112b13973ab0e5e0c4e0bb7b0013e7d597489f
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2023-06-04 08:04:37 +0200

    Make ExpanderTab "expand" properly

--- core/src/com/unciv/ui/components/ExpanderTab.kt
+++ core/src/com/unciv/ui/components/ExpanderTab.kt
@@ -1,17 +1,22 @@
 package com.unciv.ui.components
 
 import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.math.Interpolation
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Touchable
 import com.badlogic.gdx.scenes.scene2d.actions.FloatAction
+import com.badlogic.gdx.scenes.scene2d.ui.Cell
+import com.badlogic.gdx.scenes.scene2d.ui.Container
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup
 import com.badlogic.gdx.utils.Align
 import com.unciv.Constants
 import com.unciv.UncivGame
-import com.unciv.ui.images.ImageGetter
 import com.unciv.ui.components.extensions.onClick
 import com.unciv.ui.components.extensions.toLabel
+import com.unciv.ui.images.IconCircleGroup
+import com.unciv.ui.images.ImageGetter
 import com.unciv.ui.screens.basescreen.BaseScreen
 
 /**
@@ -19,13 +24,14 @@ import com.unciv.ui.screens.basescreen.BaseScreen
  *
  * @param title The header text, automatically translated.
  * @param fontSize Size applied to header text (only)
- * @param icon Optional icon - please use [Image][com.badlogic.gdx.scenes.scene2d.ui.Image] or [IconCircleGroup]
+ * @param icon Optional icon - please use [Image] or [IconCircleGroup] and make sure size is set
  * @param defaultPad Padding between content and wrapper.
  * @param headerPad Default padding for the header Table.
  * @param expanderWidth If set initializes header width
+ * @param maxContentHeight If set and content measures higher, the wrapper will be a ScrollPane set to this height
  * @param persistenceID If specified, the ExpanderTab will remember its open/closed state for the duration of one app run
  * @param onChange If specified, this will be called after the visual change for a change in [isOpen] completes (e.g. to react to changed size)
- * @param initContent Optional lambda with [innerTable] as parameter, to help initialize content.
+ * @param initContent Optional lambda with [innerTable] as parameter, to help initialize content. Will be `pack()`ed when done!
  */
 class ExpanderTab(
     title: String,
@@ -36,6 +42,7 @@ class ExpanderTab(
     headerPad: Float = 10f,
     expanderWidth: Float = 0f,
     private val persistenceID: String? = null,
+    maxContentHeight: Float = 0f,
     private val onChange: (() -> Unit)? = null,
     initContent: ((Table) -> Unit)? = null
 ): Table(BaseScreen.skin) {
@@ -43,7 +50,9 @@ class ExpanderTab(
         const val arrowSize = 18f
         const val arrowImage = "OtherIcons/BackArrow"
         val arrowColor = Color(1f,0.96f,0.75f,1f)
-        const val animationDuration = 0.2f
+        const val animationDurationForStageHeight = 0.5f  // also serves as maximum
+
+        const val forceScroll = false
 
         val persistedStates = HashMap<String, Boolean>()
     }
@@ -51,7 +60,15 @@ class ExpanderTab(
     val header = Table(skin)  // Header with label and icon, touchable to show/hide
     private val headerLabel = title.toLabel(fontSize = fontSize)
     private val headerIcon = ImageGetter.getImage(arrowImage)
-    private val contentWrapper = Table()  // Wrapper for innerTable, this is what will be shown/hidden
+
+    private val wrapper: WidgetGroup
+    private val wrapperCell: Cell<WidgetGroup>
+    private val wrapperWidth: Float
+    private val wrapperHeight: Float
+
+    private var currentPercent = 0f
+    private val noAnimation = !UncivGame.Current.settings.continuousRendering
+    private var savedScrollY = 0f
 
     /** The container where the client should add the content to toggle */
     val innerTable = Table()
@@ -82,45 +99,70 @@ class ExpanderTab(
         header.add(headerIcon).size(arrowSize).align(Align.center)
         header.touchable= Touchable.enabled
         header.onClick { toggle() }
+
         if (expanderWidth != 0f)
             defaults().minWidth(expanderWidth)
         defaults().growX()
-        contentWrapper.defaults().growX().pad(defaultPad)
         innerTable.defaults().growX()
-        add(header).fillY().row()
-        add(contentWrapper)
-        contentWrapper.add(innerTable)      // update will revert this
         initContent?.invoke(innerTable)
-        if (expanderWidth == 0f) {
-            // Measure content width incl. pad, set header to same width
-            if (innerTable.needsLayout()) contentWrapper.pack()
-            getCell(header).minWidth(contentWrapper.width)
-        }
-        update(noAnimation = true)
+
+        innerTable.pack()
+        val contentWidth = innerTable.width
+        val contentHeight = innerTable.height
+
+        wrapperWidth = if (expanderWidth > 0f && contentWidth > expanderWidth)
+            expanderWidth else contentWidth
+        wrapperHeight = if (maxContentHeight > 0f && contentHeight > maxContentHeight)
+            maxContentHeight else contentHeight
+
+        wrapper = if (contentHeight > wrapperHeight || contentWidth > wrapperWidth || forceScroll)
+                AutoScrollPane(innerTable, skin).apply {
+                    setOverscroll(false, false)
+                    setScrollingDisabled(true, false)
+                }
+            else
+                Container(innerTable).apply {
+                    bottom()  // controls what is seen first on opening!
+                }
+
+        add(header).width(wrapperWidth).fillY().row()
+        wrapperCell = add(wrapper).maxHeight(wrapperHeight).pad(defaultPad)  // maxHeight only needed for ScrollPane wrapper
+
+        update(fromInit = true)
     }
 
-    private fun update(noAnimation: Boolean = false) {
+    override fun getPrefHeight() = header.prefHeight + wrapperHeight * currentPercent
+
+    private fun update(fromInit: Boolean = false) {
         if (persistenceID != null)
             persistedStates[persistenceID] = isOpen
-        if (noAnimation || !UncivGame.Current.settings.continuousRendering) {
-            contentWrapper.clear()
-            if (isOpen) contentWrapper.add(innerTable)
-            headerIcon.rotation = if (isOpen) 90f else 180f
-            if (!noAnimation) onChange?.invoke()
+
+        if (wrapper is ScrollPane && currentPercent == 1f)
+            savedScrollY = wrapper.scrollY
+
+        if (noAnimation || fromInit) {
+            updateContentVisibility(if (isOpen) 1f else 0f)
+            wrapper.isVisible = isOpen
+            if (!fromInit) onChange?.invoke()
             return
         }
-        val action = object: FloatAction ( 90f, 180f, animationDuration, Interpolation.linear) {
-            override fun update(percent: Float) {
-                super.update(percent)
-                headerIcon.rotation = this.value
-                if (this.isComplete) {
-                    contentWrapper.clear()
-                    if (isOpen) contentWrapper.add(innerTable)
-                    onChange?.invoke()
-                }
-            }
-        }.apply { isReverse = isOpen }
-        addAction(action)
+
+        clearActions()
+        addAction(ExpandAction())
+    }
+
+    private fun updateContentVisibility(percent: Float) {
+        currentPercent = percent
+        val height = percent * wrapperHeight
+        wrapperCell.height(height)  // needed for layout
+        wrapper.setSize(wrapperWidth, height)  // needed for clipping
+        if (wrapper is ScrollPane) {
+            // This simulates what Container.bottom() does
+            wrapper.scrollY = savedScrollY + (1f - currentPercent) * wrapperHeight
+            wrapper.updateVisualScroll()
+        }
+        headerIcon.rotation = 90f * (2f - percent)
+        invalidateHierarchy()
     }
 
     /** Toggle [isOpen], animated */
@@ -128,8 +170,36 @@ class ExpanderTab(
         isOpen = !isOpen
     }
 
-    /** Change header label text after initialization */
+    /** Change header label text after initialization - **no** auto-translation! */
     fun setText(text: String) {
         headerLabel.setText(text)
+    }
+
+    inner class ExpandAction : FloatAction() {
+        init {
+            start = currentPercent  // start from wherever we were if turned around midway
+            end = if (isOpen) 1f else 0f
+            duration = animationDurationForStageHeight *
+                (stage?.run { wrapperHeight.coerceAtMost(height) / height } ?: 0.5f) *  // shorter if less content height
+                (if (isOpen) 1f - currentPercent else currentPercent)                   // shorter if turned around midway
+        }
+
+        override fun begin() {
+            super.begin()
+            (wrapper as? Container<*>)?.clip(true)
+            wrapper.isVisible = true
+        }
+
+        override fun update(percent: Float) {
+            super.update(percent)
+            updateContentVisibility(value)
+        }
+
+        override fun end() {
+            // super.end() _is_ empty...
+            (wrapper as? Container<*>)?.clip(false)
+            wrapper.isVisible = isOpen   // allows turning clip off in closed state
+            onChange?.invoke()
+        }
     }
 }
