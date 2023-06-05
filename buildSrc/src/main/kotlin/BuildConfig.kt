5a652dc0d45911e0e14fca78e8a8d56b05df8202
Yair Morgenstern <yairm210@hotmail.com>
2023-06-05 19:38:11 +0300

    Version rollout (#9528)
    * Bump version and create initial changelog entry
    
    * Update German translation (#9525)
    
    * Update Japanese.properties (#9523)
    
    * Update Japanese.properties
    
    *Add new translations
    *Fixed some translations
    
    * Update Japanese.properties
    
    *Add new translations
    *Add Carthage city names
    *Add translations for messages - Netherlands, Carthage
    *Fix translations
    
    * Update Japanese.properties
    
    * Netherlands, Carthage message - revert translations to null
    
    * Update Japanese.properties
    
    *Fixed lack of praceholders
    
    * Update Italian.properties (#9520)
    
    * Update Polish.properties (#9517)
    
    * Update French.properties (#9512)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: mape6 <mape6@posteo.de>
    Co-authored-by: namazulake <130637228+namazulake@users.noreply.github.com>
    Co-authored-by: Giuseppe D'Addio <41149920+Smashfanful@users.noreply.github.com>
    Co-authored-by: Lesiakower <125187776+Lesiakower@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,8 +4,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.8.21"
     const val appName = "Unciv"
-    const val appCodeNumber = 874
-    const val appVersion = "4.6.17"
+    const val appCodeNumber = 875
+    const val appVersion = "4.6.18"
 
     const val gdxVersion = "1.11.0"
     const val roboVMVersion = "2.3.1"
