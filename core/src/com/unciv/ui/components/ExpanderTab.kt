1120e96875179a17066a3e5eef69fa0ea5519310
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2023-06-07 01:04:55 +0200

    Make ExpanderTab "expand" properly - enable dynamic content

--- core/src/com/unciv/ui/components/ExpanderTab.kt
+++ core/src/com/unciv/ui/components/ExpanderTab.kt
@@ -18,6 +18,10 @@ import com.unciv.ui.components.extensions.toLabel
 import com.unciv.ui.images.IconCircleGroup
 import com.unciv.ui.images.ImageGetter
 import com.unciv.ui.screens.basescreen.BaseScreen
+import kotlin.math.abs
+
+// Todo: Call onChange during animation??
+// Todo: DisableAnimation parameter
 
 /**
  * A widget with a header that when clicked shows/hides a sub-Table.
@@ -28,9 +32,9 @@ import com.unciv.ui.screens.basescreen.BaseScreen
  * @param startsOutOpened Default initial "open" state if no [persistenceID] set or no persistes state found
  * @param defaultPad Padding between content and wrapper.
  * @param headerPad Default padding for the header Table.
- * @param expanderWidth If set initializes header width
+ * @param expanderWidth If set initializes cell minWidth and wrapper width
  * @param persistenceID If specified, the ExpanderTab will remember its open/closed state for the duration of one app run
- * @param content An [Actor] supporting [Layout] with final content in place to display in expanded state. Will be `pack()`ed _and measured_ by the constructor!
+ * @param content An [Actor] supporting [Layout] with the content to display in expanded state. Will be `pack()`ed!
  * @param onChange If specified, this will be called after the visual change for a change in [isOpen] completes (e.g. to react to changed size)
  */
 class ExpanderTab(
@@ -40,13 +44,13 @@ class ExpanderTab(
     startsOutOpened: Boolean = true,
     defaultPad: Float = 10f,
     headerPad: Float = 10f,
-    expanderWidth: Float = 0f,
+    private val expanderWidth: Float = 0f,
     private val persistenceID: String? = null,
     private val content: WidgetGroup,
     private val onChange: (() -> Unit)? = null
 ) : Table(BaseScreen.skin) {
     /**
-     * @param initContent A lambda with the future [content] as parameter, to help initialize. [content] must not be empty and will be `pack()`ed when done!
+     * @param initContent A lambda with the future [content] as parameter, to help initialize. Will be `pack()`ed when done!
      * @see ExpanderTab
      */
     constructor(
@@ -85,14 +89,16 @@ class ExpanderTab(
         private val persistedStates = HashMap<String, Boolean>()
     }
 
+    //todo this is accessed _only_ by ModCheckTab to set alignment - rework header style control?
     val header = Table(skin)  // Header with label and icon, touchable to show/hide
     private val headerLabel = title.toLabel(fontSize = fontSize)
     private val arrowIcon = ImageGetter.getImage(arrowImage)
+    private val headerCell: Cell<Table>
 
     private val wrapper: Container<WidgetGroup>
     private val wrapperCell: Cell<Container<WidgetGroup>>
-    private val wrapperWidth: Float
-    private val wrapperHeight: Float
+    private var wrapperWidth: Float = 0f
+    private var wrapperHeight: Float = 0f
 
     private var currentPercent = 0f
     private val noAnimation = !UncivGame.Current.settings.continuousRendering
@@ -107,6 +113,8 @@ class ExpanderTab(
         }
 
     init {
+        setLayoutEnabled(false)
+
         header.defaults().pad(headerPad)
         arrowIcon.setSize(arrowSize, arrowSize)
         arrowIcon.setOrigin(Align.center)
@@ -124,32 +132,51 @@ class ExpanderTab(
         header.touchable= Touchable.enabled
         header.onClick { toggle() }
 
-        if (expanderWidth != 0f)
-            defaults().minWidth(expanderWidth)
-        defaults().growX()
-
         content.pack()
-        val contentWidth = content.width
-        wrapperWidth = if (expanderWidth > 0f && contentWidth > expanderWidth)
-            expanderWidth else contentWidth
-        wrapperHeight = content.height
-
-        if (wrapperHeight == 0f)
-            // Sorry, can't deal with that
-            throw IllegalStateException("ExpanderTab created with empty content")
+        measureContent()
 
         wrapper = Container(content).apply {
                 setRound(false)
                 bottom()  // controls what is seen first on opening!
+                setSize(wrapperWidth, 0f)
             }
 
-        add(header).width(wrapperWidth).fillY().row()
-        wrapperCell = add(wrapper).pad(defaultPad)
+        defaults().growX()
+        headerCell = add(header).minWidth(wrapperWidth)
+        row()
+        wrapperCell = add(wrapper).size(wrapperWidth, 0f).pad(defaultPad)
 
+        setLayoutEnabled(true)
         update(fromInit = true)
     }
 
     override fun getPrefHeight() = header.prefHeight + wrapperHeight * currentPercent
+
+    override fun layout() {
+        // Critical magic here! Key to allow dynamic content.
+        // However, I can't explain why an invalidated header also needs to trigger it. Without, the
+        // WorldScreenMusicPopup's expanders, which are width-controlled by their outer cell's fillX/expandX,
+        // start aligned and same width, but will slightly misalign by some 10f on opening/closing some of them.
+        if (content.needsLayout() || header.needsLayout())
+            contentHasChanged()
+        super.layout()
+    }
+
+    private fun contentHasChanged() {
+        val oldWidth = wrapperWidth
+        val oldHeight = wrapperHeight
+        content.pack()
+        measureContent()
+        if (wrapperWidth == oldWidth && wrapperHeight == oldHeight) return
+        headerCell.minWidth(wrapperWidth)
+        currentPercent *= oldHeight / wrapperHeight  // to animate smoothly to new height, >1f should work too
+        update()
+    }
+
+    private fun measureContent() {
+        wrapperWidth = if (expanderWidth > 0f) expanderWidth else content.width
+        wrapperHeight = content.height
+    }
 
     private fun update(fromInit: Boolean = false) {
         if (persistenceID != null)
@@ -169,10 +196,10 @@ class ExpanderTab(
     private fun updateContentVisibility(percent: Float) {
         currentPercent = percent
         val height = percent * wrapperHeight
-        wrapperCell.height(height)  // needed for layout
-        wrapper.setSize(wrapperWidth, height)  // needed for clipping
+        wrapperCell.size(wrapperWidth, height)  // needed for layout
+        wrapper.setSize(wrapperWidth, height)   // needed for clipping
         arrowIcon.rotation = 90f * (2f - percent)
-        (parent as? Layout)?.invalidateHierarchy()  // Not on ExpanderTab itself
+        invalidateHierarchy()
     }
 
     /** Toggle [isOpen], animated */
@@ -189,10 +216,12 @@ class ExpanderTab(
         init {
             start = currentPercent  // start from wherever we were if turned around midway
             end = if (isOpen) 1f else 0f
-            duration = animationDurationForStageHeight *
-                (stage?.run { wrapperHeight.coerceAtMost(height) / height } ?: 0.5f)  // shorter if less content height
-                .coerceAtLeast(0.2f) *
-                (if (isOpen) 1f - currentPercent else currentPercent)                 // shorter if turned around midway
+            // Duration: shorter if less content height...
+            val heightFactor = stage?.run { wrapperHeight.coerceAtMost(height) / height } ?: 0.5f
+            // ... and shorter if turned around midway
+            val distanceFactor = abs(end - currentPercent)
+            duration = (animationDurationForStageHeight * heightFactor)
+                .coerceAtLeast(0.15f) * distanceFactor
         }
 
         override fun begin() {
@@ -207,7 +236,6 @@ class ExpanderTab(
         }
 
         override fun end() {
-            // super.end() _is_ empty...
             (wrapper as? Container<*>)?.clip(false)
             wrapper.isVisible = isOpen   // allows turning clip off in closed state
             onChange?.invoke()
