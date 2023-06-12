b9cfb68d4af2e8eb86d4df7382fe4f4383e24660
Chris <chris@hopfen.space>
2023-06-12 17:03:31 +0200

    Updated the APIv2 file storage emulator

--- core/src/com/unciv/logic/multiplayer/apiv2/ApiV2.kt
+++ core/src/com/unciv/logic/multiplayer/apiv2/ApiV2.kt
@@ -567,7 +567,7 @@ class ApiV2(private val baseUrl: String) : ApiV2Wrapper(baseUrl), Disposable {
         }
         val success = auth.login(
             UncivGame.Current.settings.multiplayer.userName,
-            UncivGame.Current.settings.multiplayer.passwords[UncivGame.Current.onlineMultiplayer.baseUrl] ?: "",
+            UncivGame.Current.settings.multiplayer.passwords[UncivGame.Current.onlineMultiplayer.multiplayerServer.serverUrl] ?: "",
             suppress = true
         )
         if (success) {
--- core/src/com/unciv/logic/multiplayer/storage/ApiV2FileStorageEmulator.kt
+++ core/src/com/unciv/logic/multiplayer/storage/ApiV2FileStorageEmulator.kt
@@ -3,57 +3,90 @@ package com.unciv.logic.multiplayer.storage
 import com.unciv.logic.files.UncivFiles
 import com.unciv.logic.multiplayer.apiv2.ApiV2
 import com.unciv.utils.Log
+import kotlinx.coroutines.runBlocking
 import java.util.*
+
+private const val PREVIEW_SUFFIX = "_Preview"
 
 /**
  * Transition helper that emulates file storage behavior using the API v2
  */
 class ApiV2FileStorageEmulator(private val api: ApiV2): FileStorage {
 
-    override suspend fun saveGameData(gameId: String, data: String) {
+    private suspend fun saveGameData(gameId: String, data: String) {
         val uuid = UUID.fromString(gameId.lowercase())
         api.game.upload(uuid, data)
     }
 
-    override suspend fun savePreviewData(gameId: String, data: String) {
+    @Suppress("UNUSED_PARAMETER")
+    private suspend fun savePreviewData(gameId: String, data: String) {
         // Not implemented for this API
         Log.debug("Call to deprecated API 'savePreviewData'")
     }
 
-    override suspend fun loadGameData(gameId: String): String {
+    override fun saveFileData(fileName: String, data: String) {
+        return runBlocking {
+            if (fileName.endsWith(PREVIEW_SUFFIX)) {
+                savePreviewData(fileName.dropLast(8), data)
+            } else {
+                saveGameData(fileName, data)
+            }
+        }
+    }
+
+    private suspend fun loadGameData(gameId: String): String {
         val uuid = UUID.fromString(gameId.lowercase())
         return api.game.get(uuid, cache = false)!!.gameData
     }
 
-    override suspend fun loadPreviewData(gameId: String): String {
+    private suspend fun loadPreviewData(gameId: String): String {
         // Not implemented for this API
         Log.debug("Call to deprecated API 'loadPreviewData'")
         // TODO: This could be improved, since this consumes more resources than necessary
         return UncivFiles.gameInfoToString(UncivFiles.gameInfoFromString(loadGameData(gameId)).asPreview())
     }
 
-    override suspend fun getFileMetaData(fileName: String): FileMetaData {
-        TODO("Not implemented for this API")
+    override fun loadFileData(fileName: String): String {
+        return runBlocking {
+            if (fileName.endsWith(PREVIEW_SUFFIX)) {
+                loadPreviewData(fileName.dropLast(8))
+            } else {
+                loadGameData(fileName)
+            }
+        }
     }
 
-    override suspend fun deleteGameData(gameId: String) {
+    override fun getFileMetaData(fileName: String): FileMetaData {
         TODO("Not yet implemented")
     }
 
-    override suspend fun deletePreviewData(gameId: String) {
+    override fun deleteFile(fileName: String) {
+        return runBlocking {
+            if (fileName.endsWith(PREVIEW_SUFFIX)) {
+                deletePreviewData(fileName.dropLast(8))
+            } else {
+                deleteGameData(fileName)
+            }
+        }
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    private suspend fun deleteGameData(gameId: String) {
+        TODO("Not yet implemented")
+    }
+
+    private suspend fun deletePreviewData(gameId: String) {
         // Not implemented for this API
         Log.debug("Call to deprecated API 'deletedPreviewData'")
         deleteGameData(gameId)
     }
 
-    override suspend fun authenticate(userId: String, password: String): Boolean {
-        return api.auth.loginOnly(userId, password)
+    override fun authenticate(userId: String, password: String): Boolean {
+        return runBlocking { api.auth.loginOnly(userId, password) }
     }
 
-    override suspend fun setPassword(newPassword: String): Boolean {
-        api.account.setPassword(newPassword, suppress = true)
-        // TODO: Not yet implemented
-        return false
+    override fun setPassword(newPassword: String): Boolean {
+        return runBlocking { api.account.setPassword(newPassword, suppress = true) }
     }
 
 }
