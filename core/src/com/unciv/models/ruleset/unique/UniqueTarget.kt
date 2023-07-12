cc80f71225ee4e77d00c9953c56e5ce2b3c41204
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2023-07-12 11:11:39 +0200

    Change type check to use overrides using `is`

--- core/src/com/unciv/models/ruleset/unique/UniqueTarget.kt
+++ core/src/com/unciv/models/ruleset/unique/UniqueTarget.kt
@@ -1,21 +1,19 @@
 package com.unciv.models.ruleset.unique
 
 import com.unciv.models.ruleset.Belief
-import com.unciv.models.ruleset.PolicyBranch
 import com.unciv.models.ruleset.RuinReward
 import com.unciv.models.ruleset.tech.Technology
 import com.unciv.models.ruleset.tile.TileImprovement
 import com.unciv.models.ruleset.tile.TileResource
 import com.unciv.models.ruleset.unit.BaseUnit
-import kotlin.reflect.KClass
 
 /**
  * Expresses which RulesetObject types a UniqueType is applicable to, and manages automated checking of compliance.
  *
+ * Compliance check is done through [isAllowedOnObject] and appropriate overrides.
+ *
  * @param documentationString Copied to uniques.md by `UniqueDocsWriter`
  * @param inheritsFrom means that all such uniques are acceptable as well. For example, all Global uniques are acceptable for Nations, Eras, etc.
- * @param classes All (kotlin) classes this directly applies to. Indirect applicability is done through inheritsFrom.
- * @param additionalFilter If there's a match in classes, this can still forbid a target (e.g. uniques working on founder Beliefs won't on follower ones)
  * @param isModifier Marks types that are allowed _exclusively_ in a "<conditional>" style modifier, as standalone it is applicable to nothing.
  *
  * //todo implement "other half" of isModifier -> conditionals and triggers should always have it
@@ -23,8 +21,6 @@ import kotlin.reflect.KClass
 enum class UniqueTarget(
     val documentationString:String = "",
     val inheritsFrom: UniqueTarget? = null,
-    val classes: List<KClass<out IHasUniques>> = emptyList(),
-    val additionalFilter: ((target: IHasUniques) -> Boolean)? = null,
     val isModifier: Boolean = false
 ) {
 
@@ -38,47 +34,84 @@ enum class UniqueTarget(
 
     UnitTriggerable("Uniques that have immediate, one-time effects on a unit." +
         "They can be added to units (on unit, unit type, or promotion) to grant them the ability to trigger this effect as an action, " +
-        "which can be modified with UnitActionModifier and UnitTriggerCondition conditionals.", Triggerable),
+        "which can be modified with UnitActionModifier and UnitTriggerCondition conditionals.",
+        inheritsFrom = Triggerable),
 
     Global("Uniques that apply globally. " +
         "Civs gain the abilities of these uniques from nation uniques, reached eras, researched techs, adopted policies, " +
-        "built buildings, religion 'founder' uniques, owned resources, and ruleset-wide global uniques.", Triggerable),
+        "built buildings, religion 'founder' uniques, owned resources, and ruleset-wide global uniques.",
+        inheritsFrom = Triggerable),
 
     // Civilization-specific
-    Nation(inheritsFrom = Global, classes = listOf(com.unciv.models.ruleset.nation.Nation::class)),
-    Era(inheritsFrom = Global, classes = listOf(com.unciv.models.ruleset.tech.Era::class)),
-    Tech(inheritsFrom = Global, classes = listOf(Technology::class)),
-    Policy(inheritsFrom = Global, classes = listOf(com.unciv.models.ruleset.Policy::class, PolicyBranch::class)),
+    Nation(inheritsFrom = Global) {
+        override fun isAllowedOnObject(target: IHasUniques) = target is com.unciv.models.ruleset.nation.Nation
+    },
+    Era(inheritsFrom = Global) {
+        override fun isAllowedOnObject(target: IHasUniques) = target is com.unciv.models.ruleset.tech.Era
+    },
+    Tech(inheritsFrom = Global) {
+        override fun isAllowedOnObject(target: IHasUniques) = target is Technology
+    },
+    Policy(inheritsFrom = Global) {
+        override fun isAllowedOnObject(target: IHasUniques) = target is com.unciv.models.ruleset.Policy
+    },
     FounderBelief("Uniques for Founder and Enhancer type Beliefs, that will apply to the founder of this religion",
-        inheritsFrom = Global, classes = listOf(Belief::class),
-        additionalFilter = { belief -> (belief as Belief).type.isFounder } ),
-    FollowerBelief("Uniques for Pantheon and Follower type beliefs, that will apply to each city where the religion is the majority religion",
-        classes = listOf(Belief::class),
-        additionalFilter = { belief -> (belief as Belief).type.isFollower }),
+        inheritsFrom = Global) {
+        override fun isAllowedOnObject(target: IHasUniques) = target is Belief && target.type.isFounder
+    },
+    FollowerBelief("Uniques for Pantheon and Follower type beliefs, that will apply to each city where the religion is the majority religion") {
+        override fun isAllowedOnObject(target: IHasUniques) = target is Belief && target.type.isFollower
+    },
 
     // City-specific
-    Building(inheritsFrom = Global, classes = listOf(com.unciv.models.ruleset.Building::class),
-        additionalFilter = { building -> !(building as com.unciv.models.ruleset.Building).isAnyWonder() }),
-    Wonder(inheritsFrom = Building, classes = listOf(com.unciv.models.ruleset.Building::class),
-        additionalFilter = { building -> (building as com.unciv.models.ruleset.Building).isAnyWonder() }),
+    Building(inheritsFrom = Global) {
+        override fun isAllowedOnObject(target: IHasUniques) = target is com.unciv.models.ruleset.Building
+            // && !target.isAnyWonder() is redundant as long as UniqueTarget.Wonder inherits from this.
+            // For the same reason we "should" call super.isAllowedOnObject but don't actually need to.
+    },
+    Wonder(inheritsFrom = Building) {
+        override fun isAllowedOnObject(target: IHasUniques) = target is com.unciv.models.ruleset.Building && target.isAnyWonder()
+    },
 
     // Unit-specific
     Unit("Uniques that can be added to units, unit types, or promotions"
-        , inheritsFrom = UnitTriggerable, classes = listOf(BaseUnit::class)),
-    UnitType(inheritsFrom = Unit, classes = listOf(com.unciv.models.ruleset.unit.UnitType::class)),
-    Promotion(inheritsFrom = Unit, classes = listOf(com.unciv.models.ruleset.unit.Promotion::class)),
+        , inheritsFrom = UnitTriggerable) {
+        override fun isAllowedOnObject(target: IHasUniques) = target is BaseUnit || super.isAllowedOnObject(target)
+    },
+    UnitType(inheritsFrom = Unit) {
+        override fun isAllowedOnObject(target: IHasUniques) = target is com.unciv.models.ruleset.unit.UnitType
+    },
+    Promotion(inheritsFrom = Unit) {
+        override fun isAllowedOnObject(target: IHasUniques) = target is com.unciv.models.ruleset.unit.Promotion
+    },
 
     // Tile-specific
-    Terrain(classes = listOf(com.unciv.models.ruleset.tile.Terrain::class)),
-    Improvement(classes = listOf(TileImprovement::class)),
-    Resource(inheritsFrom = Global, classes = listOf(TileResource::class)),
-    Ruins(inheritsFrom = UnitTriggerable, classes = listOf(RuinReward::class)),
+    Terrain {
+        override fun isAllowedOnObject(target: IHasUniques) = target is com.unciv.models.ruleset.tile.Terrain
+    },
+    Improvement {
+        override fun isAllowedOnObject(target: IHasUniques) = target is TileImprovement
+    },
+    Resource(inheritsFrom = Global) {
+        override fun isAllowedOnObject(target: IHasUniques) = target is TileResource
+    },
+    Ruins(inheritsFrom = UnitTriggerable) {
+        override fun isAllowedOnObject(target: IHasUniques) = target is RuinReward
+    },
 
     // Other
-    Speed(classes = listOf(com.unciv.models.ruleset.Speed::class)),
-    Tutorial(classes = listOf(com.unciv.models.ruleset.Tutorial::class)),
-    CityState(inheritsFrom = Global, classes = listOf(com.unciv.models.ruleset.nation.Nation::class)),
-    ModOptions(classes = listOf(com.unciv.models.ruleset.ModOptions::class)),
+    Speed {
+        override fun isAllowedOnObject(target: IHasUniques) = target is com.unciv.models.ruleset.Speed
+    },
+    Tutorial {
+        override fun isAllowedOnObject(target: IHasUniques) = target is com.unciv.models.ruleset.Tutorial
+    },
+    CityState(inheritsFrom = Global) {
+        override fun isAllowedOnObject(target: IHasUniques) = target is com.unciv.models.ruleset.nation.Nation && target.isCityState
+    },
+    ModOptions {
+        override fun isAllowedOnObject(target: IHasUniques) = target is com.unciv.models.ruleset.ModOptions
+    },
 
     // Modifiers (using Conditionals syntax)
     Conditional("Modifiers that can be added to other uniques to limit when they will be active",
@@ -99,14 +132,13 @@ enum class UniqueTarget(
 
     /** To be used in RulesetValidator:
      *  A Unique with a target type `this` was found on [target] - is that OK?
-     *  (false is not necessarily final - the UniqueType may list other possible targets) */
-    fun isAllowedOnObject(target: IHasUniques): Boolean {
-        if (isModifier) return false
-        if (target::class in classes)
-            // Don't collapse to one return: If additionalFilter says "No", inheritsFrom should be next!
-            if (additionalFilter == null) return true
-            else if (additionalFilter.invoke(target)) return true
-        return values()
+     *  (false is not necessarily final - the UniqueType may list other possible targets)
+     *
+     *  Note: Overrides **should** call (own decision) `|| super.isAllowedOnObject` - but that is only
+     *  necessary if there actually **are** any UniqueTarget entries that [inherit from][inheritsFrom] the current instance.
+     */
+    open fun isAllowedOnObject(target: IHasUniques): Boolean {
+        return !isModifier && values()
             .filter { it.inheritsFrom == this }
             .any { it.isAllowedOnObject(target) }
     }
