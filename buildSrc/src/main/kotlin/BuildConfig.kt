33ae13479dd967a7924843aa501f1bf3405d8d1c
Yair Morgenstern <yairm210@hotmail.com>
2023-07-20 09:24:41 +0300

    4.7.9-patch1

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,8 +4,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.8.21"
     const val appName = "Unciv"
-    const val appCodeNumber = 892
-    const val appVersion = "4.7.9"
+    const val appCodeNumber = 893
+    const val appVersion = "4.7.9-patch1"
 
     const val gdxVersion = "1.11.0"
     const val ktorVersion = "2.2.3"
