0be6cd062f7fa21a20dcd698e487844ee3c66e04
Yair Morgenstern <yairm210@hotmail.com>
2023-09-13 11:17:57 +0300

    4.8.5

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -9,6 +9,6 @@ object BuildConfig {
 
     const val gdxVersion = "1.11.0"
     const val ktorVersion = "2.2.3"
-    const val coroutinesVersion = "1.6.4"
+    const val coroutinesVersion = "1.7.2"
     const val roboVMVersion = "2.3.1"
 }
