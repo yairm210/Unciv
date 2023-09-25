f3f30c1f3208aea2917ef9747e43bf3a00125557
Oskar Niesen <oskar-github@niesens.com>
2023-09-24 21:31:28 -0500

    Added AI defensive pact check on their friends

--- core/src/com/unciv/logic/automation/civilization/NextTurnAutomation.kt
+++ core/src/com/unciv/logic/automation/civilization/NextTurnAutomation.kt
@@ -933,6 +933,12 @@ object NextTurnAutomation {
                 && thirdCiv.getDiplomacyManager(otherCiv).isRelationshipLevelLT(RelationshipLevel.Favorable))
                 return false
         }
+        // If they have bad relations with any of thier friends, don't consider it
+        for(thirdCiv in commonknownCivs) {
+            if (otherCiv.getDiplomacyManager(thirdCiv).isRelationshipLevelGE(RelationshipLevel.Friend)
+                && thirdCiv.getDiplomacyManager(civInfo).isRelationshipLevelLT(RelationshipLevel.Favorable))
+                return false
+        }
 
         val defensivePacts = civInfo.diplomacy.count { it.value.hasFlag(DiplomacyFlags.DefensivePact) }
         val otherCivNonOverlappingDefensivePacts = otherCiv.diplomacy.values.count { it.hasFlag(DiplomacyFlags.DefensivePact)
