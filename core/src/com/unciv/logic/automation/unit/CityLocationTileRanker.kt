b5096ae47b78198101d1c57bf344aeeb4e0298ba
Oskar Niesen <oskar-github@niesens.com>
2023-11-03 11:58:51 -0500

    Adjusted tile ranking values

--- core/src/com/unciv/logic/automation/unit/CityLocationTileRanker.kt
+++ core/src/com/unciv/logic/automation/unit/CityLocationTileRanker.kt
@@ -24,7 +24,7 @@ object CityLocationTileRanker {
             .filter { canSettleTile(it, unit.civ, nearbyCities) && (unit.currentTile == it || unit.movement.canMoveTo(it)) }
         val uniqueCache = LocalUniqueCache()
         val baseTileMap = HashMap<Tile, Float>()
-        val tileRankMap = HashMap<Tile,Float>()
+        val tileRankMap = HashMap<Tile, Float>()
         var maxValueTile: Tile? = null
         var maxValueRank: Float = 0f
         for (tile in possibleCityLocations) {
@@ -52,7 +52,7 @@ object CityLocationTileRanker {
         return true
     }
 
-    private fun rankTileToSettle(tile: Tile, civ: Civilization, nearbyCities: Sequence<City>, 
+    private fun rankTileToSettle(tile: Tile, civ: Civilization, nearbyCities: Sequence<City>,
                                  baseTileMap: HashMap<Tile, Float>, uniqueCache: LocalUniqueCache): Float {
         var tileValue = 0f
         for (city in nearbyCities) {
@@ -65,8 +65,9 @@ object CityLocationTileRanker {
                 else -> 0
             }
             // It is worse to settle cities near our own compare to near another civ
-            if (city.civ == civ)
             // Do not settle near our capital unless really necessary
+            // Having a strong capital is esential to constructing wonders
+            if (city.civ == civ)
                 distanceToCityModifier *= if (city.isCapital()) 5 else 2
             tileValue -= distanceToCityModifier
         }
@@ -75,35 +76,39 @@ object CityLocationTileRanker {
 
         fun rankTile(rankTile: Tile): Float {
             var locationSpecificTileValue = 0f
-            // Don't settle near by not on the coast
-            if (tile.isWater && !onCoast) locationSpecificTileValue -= 20
+            // Don't settle near but not on the coast
+            if (tile.isCoastalTile() && !onCoast) locationSpecificTileValue -= 20
             // Check if everything else has been calculated, if so return it
             if (baseTileMap.containsKey(rankTile)) return locationSpecificTileValue + baseTileMap[rankTile]!!
             if (rankTile.getOwner() != null && rankTile.getOwner() != civ) return 0f
-            
+
             var rankTileValue = Automation.rankStatsValue(rankTile.stats.getTileStats(null, civ, uniqueCache), civ)
 
             if (rankTile.resource != null) {
-                if (rankTile.tileResource.resourceType == ResourceType.Strategic) {
-                    rankTileValue += 3 * rankTile.resourceAmount
-                } else if (rankTile.tileResource.resourceType == ResourceType.Luxury) {
-                    rankTileValue += if (civ.hasResource(rankTile.resource!!)) 10 * rankTile.resourceAmount
-                    else 30 + 10 * (rankTile.resourceAmount - 1)
+                rankTileValue += when (rankTile.tileResource.resourceType) {
+                    ResourceType.Bonus -> 2
+                    ResourceType.Strategic -> 3 * rankTile.resourceAmount
+                    ResourceType.Luxury ->
+                        // For all normal maps, lets just assume resourceAmmount = 1
+                        if (civ.hasResource(rankTile.resource!!)) 5 * rankTile.resourceAmount
+                        else 12 + 5 * (rankTile.resourceAmount - 1)
                 }
             }
-            
-            if (rankTile.isNaturalWonder()) rankTileValue += 15
+
+            if (rankTile.isNaturalWonder()) rankTileValue += 10
 
             baseTileMap[rankTile] = rankTileValue
 
             return rankTileValue + locationSpecificTileValue
         }
 
-        if (onCoast) tileValue += 30
-        if (tile.isAdjacentToRiver()) tileValue += 5
+        if (onCoast) tileValue += 10
+        if (tile.isAdjacentToRiver()) tileValue += 3
         if (tile.terrainHasUnique(UniqueType.FreshWater)) tileValue += 5
         // We want to found the city on an oasis because it can't be improved otherwise
-        if (tile.terrainHasUnique(UniqueType.Unbuildable)) tileValue += 5
+        if (tile.terrainHasUnique(UniqueType.Unbuildable)) tileValue += 3
+        // If we build the city on a resource tile, then we can't build any special improvements on it
+        if (tile.resource != null) tileValue -= 2
 
         for (i in 0..3) {
             for (nearbyTile in tile.getTilesInDistanceRange(IntRange(i, i))) {
