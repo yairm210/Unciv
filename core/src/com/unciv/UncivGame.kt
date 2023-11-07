bc1d30fb64d113d9ff56fbf6785785ae53c89457
yairm210 <yairm210@users.noreply.github.com>
2023-11-07 08:42:37 +0000

    Bump version and create initial changelog entry

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -544,7 +544,7 @@ open class UncivGame(val isConsoleMode: Boolean = false) : Game(), PlatformSpeci
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.8.16", 926)
+        val VERSION = Version("4.8.17", 927)
         //endregion
 
         lateinit var Current: UncivGame
