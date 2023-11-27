7893eb5f600f1110d27d862dddc07450e289efb2
dHannasch <David.A.Hannasch@gmail.com>
2023-11-26 21:55:15 -0700

    Update BasicTests.kt

--- tests/src/com/unciv/testing/BasicTests.kt
+++ tests/src/com/unciv/testing/BasicTests.kt
@@ -68,7 +68,7 @@ class BasicTests {
         val units: Collection<BaseUnit> = ruleset.units.values
         var allObsoletingUnitsHaveUpgrades = true
         for (unit in units) {
-            if (unit.obsoletingTechs.any() && unit.upgradesTo == null && unit.name !="Scout" ) {
+            if (unit.obsoletingTechs().any() && unit.upgradesTo == null && unit.name !="Scout" ) {
                 debug("%s obsoletes but has no upgrade", unit.name)
                 allObsoletingUnitsHaveUpgrades = false
             }
