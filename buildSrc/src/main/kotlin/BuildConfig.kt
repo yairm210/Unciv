9d411a16bc94a660bf8815f4bee6d09add0d4ae7
yairm210 <yairm210@users.noreply.github.com>
2023-11-28 10:15:53 +0000

    4.9.2-patch5

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,8 +4,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.8.21"
     const val appName = "Unciv"
-    const val appCodeNumber = 936
-    const val appVersion = "4.9.2-patch4"
+    const val appCodeNumber = 937
+    const val appVersion = "4.9.2-patch5"
 
     const val gdxVersion = "1.12.1"
     const val ktorVersion = "2.2.3"
