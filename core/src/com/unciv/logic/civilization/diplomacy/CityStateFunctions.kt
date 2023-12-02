811f6f4e76d0f285cf9fa766f2affb8241531561
dHannasch <David.A.Hannasch@gmail.com>
2023-12-02 10:20:43 -0700

    Update CityStateFunctions.kt

--- core/src/com/unciv/logic/civilization/diplomacy/CityStateFunctions.kt
+++ core/src/com/unciv/logic/civilization/diplomacy/CityStateFunctions.kt
@@ -53,8 +53,8 @@ class CityStateFunctions(val civInfo: Civilization) {
         if (uniqueTypes.contains(UniqueType.CityStateMilitaryUnits)) {
             val possibleUnits = ruleset.units.values.filter {
                 return@filter !it.availableInEra(ruleset, startingEra) // Not from the start era or before
-                && it.uniqueTo != null && it.uniqueTo in unusedMajorCivs // Must be from a major civ not in the game
-                && ruleset.unitTypes[it.unitType]!!.isLandUnit() && ( it.strength > 0 || it.rangedStrength > 0 ) // Must be a land military unit
+                    && it.uniqueTo != null && it.uniqueTo in unusedMajorCivs // Must be from a major civ not in the game
+                    && ruleset.unitTypes[it.unitType]!!.isLandUnit() && ( it.strength > 0 || it.rangedStrength > 0 ) // Must be a land military unit
             }
             if (possibleUnits.isNotEmpty())
                 civInfo.cityStateUniqueUnit = possibleUnits.random().name
