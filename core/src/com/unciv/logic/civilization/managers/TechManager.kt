7078d0eba068386f842d1753e1603f83eb450561
dHannasch <David.A.Hannasch@gmail.com>
2023-12-02 11:35:15 -0700

    Update TechManager.kt

--- core/src/com/unciv/logic/civilization/managers/TechManager.kt
+++ core/src/com/unciv/logic/civilization/managers/TechManager.kt
@@ -153,7 +153,7 @@ class TechManager : IsPartOfGameInfoSerialization {
 
     fun isResearched(techName: String): Boolean = techsResearched.contains(techName)
 
-    fun isResearched(construction: INonPerpetualConstruction): Boolean = construction.requiredTechs().all{ requiredTech -> !isResearched(requiredTech) }
+    fun isResearched(construction: INonPerpetualConstruction): Boolean = construction.requiredTechs().all{ requiredTech -> isResearched(requiredTech) }
 
     fun canBeResearched(techName: String): Boolean {
         val tech = getRuleset().technologies[techName]!!
