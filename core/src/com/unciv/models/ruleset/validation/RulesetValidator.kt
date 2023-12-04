8a03f9df12eea042306ebedb723165f1576328b6
dHannasch <David.A.Hannasch@gmail.com>
2023-12-04 11:26:15 -0700

    Rename BaseUnit.techsAtWhichNoLongerAvailable() (#10663)
    * Update BaseUnit.kt
    
    * Update BaseUnit.kt
    
    * Update RulesetValidator.kt
    
    * Update BaseUnit.kt

--- core/src/com/unciv/models/ruleset/validation/RulesetValidator.kt
+++ core/src/com/unciv/models/ruleset/validation/RulesetValidator.kt
@@ -658,7 +658,7 @@ class RulesetValidator(val ruleset: Ruleset) {
         for (requiredTech: String in unit.requiredTechs())
             if (!ruleset.technologies.containsKey(requiredTech))
                 lines += "${unit.name} requires tech ${requiredTech} which does not exist!"
-        for (obsoleteTech: String in unit.techsThatObsoleteThis())
+        for (obsoleteTech: String in unit.techsAtWhichNoLongerAvailable())
             if (!ruleset.technologies.containsKey(obsoleteTech))
                 lines += "${unit.name} obsoletes at tech ${obsoleteTech} which does not exist!"
         if (unit.upgradesTo != null && !ruleset.units.containsKey(unit.upgradesTo!!))
