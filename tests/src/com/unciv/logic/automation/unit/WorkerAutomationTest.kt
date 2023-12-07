5840c7112fc172245542f6401a951f58e472aed3
Oskar Niesen <oskar-github@niesens.com>
2023-12-07 12:13:12 -0600

    Created a partial Test

--- tests/src/com/unciv/logic/automation/unit/WorkerAutomationTest.kt
+++ tests/src/com/unciv/logic/automation/unit/WorkerAutomationTest.kt
@@ -1,7 +1,11 @@
 package com.unciv.logic.automation.unit
 
+import com.unciv.Constants
+import com.unciv.logic.automation.civilization.NextTurnAutomation
 import com.unciv.logic.civilization.Civilization
+import com.unciv.logic.civilization.managers.TurnManager
 import com.unciv.logic.map.tile.RoadStatus
+import com.unciv.models.ruleset.PerpetualConstruction
 import com.unciv.testing.GdxTestRunner
 import com.unciv.testing.TestGame
 import org.junit.Assert.assertEquals
@@ -48,4 +52,45 @@ internal class WorkerAutomationTest {
             "Mine", currentTile.improvementInProgress)
         assertTrue(currentTile.turnsToImprovement > 0)
     }
+
+    @Test
+    fun `should build improvements in turns`() {
+        // Add the needed tech to construct the improvements below
+        for (improvement in listOf(RoadStatus.Road.name, "Farm")) {
+            civInfo.tech.techsResearched.add(testGame.ruleset.tileImprovements[improvement]!!.techRequired!!)
+        }
+        civInfo.tech.techsResearched.add(testGame.ruleset.tileResources["Iron"]!!.revealedBy!!)
+
+        val centerTile = testGame.tileMap[0,0] // owned by city
+        val city = testGame.addCity(civInfo, centerTile)
+        civInfo.addGold(100000000)
+        for (tile in centerTile.getTilesInDistanceRange(2..3)) {
+            city.expansion.buyTile(tile)
+        }
+        for (tile in centerTile.getTilesInDistance(3)) {
+            tile.baseTerrain = Constants.plains
+        }
+        val worker = testGame.addUnit("Worker", civInfo, centerTile)
+        for(i in 0..37) {
+            worker.currentMovement = 2f
+            NextTurnAutomation.automateCivMoves(civInfo)
+            city.cityConstructions.constructionQueue.clear()
+//            city.cityConstructions.constructionQueue.add(PerpetualConstruction.idle.name)
+            TurnManager(civInfo).endTurn()
+//            testGame.gameInfo.nextTurn()
+        }
+
+        var finishedCount = 0      
+        var inProgressCount = 0
+        for (tile in centerTile.getTilesInDistance(3)) {
+            if (tile.improvement != null) finishedCount++
+            if (tile.turnsToImprovement != 0) inProgressCount++
+        }
+
+        assertTrue("Worker improvements in progress was greater than 1, actual: $inProgressCount", 
+            inProgressCount <= 1)
+        assertTrue("Worker should have built over 6 improvements but only built $finishedCount", 
+            finishedCount >= 6)
+    }
+
 }
