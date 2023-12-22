0fccc9dd78bb864f1b8f4e440bc36eefa798f041
yairm210 <yairm210@users.noreply.github.com>
2023-12-22 08:09:29 +0000

    Bump version and create initial changelog entry

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,8 +4,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.8.21"
     const val appName = "Unciv"
-    const val appCodeNumber = 946
-    const val appVersion = "4.9.10-patch1"
+    const val appCodeNumber = 947
+    const val appVersion = "4.9.11"
 
     const val gdxVersion = "1.12.1"
     const val ktorVersion = "2.2.3"
