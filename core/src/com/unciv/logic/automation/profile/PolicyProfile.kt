df234b7d9948708de59b24126b6cecd441f44ac8
MioBestWaifu <Fenix1603@hotmail.com>
2023-12-21 21:05:16 -0300

    Update PolicyProfile.kt

--- core/src/com/unciv/logic/automation/profile/PolicyProfile.kt
+++ core/src/com/unciv/logic/automation/profile/PolicyProfile.kt
@@ -7,6 +7,7 @@ class PolicyProfile {
     var decrease = ArrayList<String>()
 
     fun getPriotiyModifier(policy: PolicyBranch): Int {
+        //Half the value of the most basic policy priority
         if (increase.contains(policy.name))
             return 5
         if (decrease.contains(policy.name))
