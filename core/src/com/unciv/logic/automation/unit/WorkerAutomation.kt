7c3b6574cfea7ca6bdf1676f84499b98c9d0bb51
Oskar Niesen <oskar-github@niesens.com>
2023-12-30 10:45:37 -0600

    Fixed getBasePriority being negative

--- core/src/com/unciv/logic/automation/unit/WorkerAutomation.kt
+++ core/src/com/unciv/logic/automation/unit/WorkerAutomation.kt
@@ -500,7 +500,7 @@ class WorkerAutomation(
      * This is a cheap guess on how helpful it might be to do work on this tile
      */
     fun getBasePriority(tile: Tile, unit: MapUnit): Float {
-        val unitSpecificPriority = 2 - tile.aerialDistanceTo(unit.getTile()).coerceAtMost(4)
+        val unitSpecificPriority = 2 - (tile.aerialDistanceTo(unit.getTile()) / 2.0f).coerceIn(0f, 2f)
         if (tileRankings.containsKey(tile)) 
             return tileRankings[tile]!!.tilePriority + unitSpecificPriority
         
