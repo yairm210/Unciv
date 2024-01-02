8a781da236e105197fd8cbaf2ae6f02bf02ef3e3
Oskar Niesen <oskar-github@niesens.com>
2024-01-02 17:50:05 -0600

    Accounted for the repair case as well

--- core/src/com/unciv/logic/automation/unit/WorkerAutomation.kt
+++ core/src/com/unciv/logic/automation/unit/WorkerAutomation.kt
@@ -564,9 +564,9 @@ class WorkerAutomation(
                 }
             }
         }
-
         // A better tile than this unit can build might have been stored in the cache
-        if (rank.bestImprovement == null || !unit.canBuildImprovement(rank.bestImprovement!!, tile)) return -100f
+        if (!rank.repairImprovment!! && (rank.bestImprovement == null || 
+                !unit.canBuildImprovement(rank.bestImprovement!!, tile))) return -100f
         return rank.improvementPriority!!
     }
 
