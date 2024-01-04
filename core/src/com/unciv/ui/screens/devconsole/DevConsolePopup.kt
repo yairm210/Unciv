3633e955845172aa35f89be3a22f0f3cb038391c
Yair Morgenstern <yairm210@hotmail.com>
2024-01-04 09:16:38 +0200

    City/civ names match ignore case and match with spaces or -

--- core/src/com/unciv/ui/screens/devconsole/DevConsolePopup.kt
+++ core/src/com/unciv/ui/screens/devconsole/DevConsolePopup.kt
@@ -88,7 +88,7 @@ class DevConsolePopup(val screen: WorldScreen) : Popup(screen) {
         return commandRoot.autocomplete(params) ?: ""
     }
 
-    internal fun getCivByName(name: String) = gameInfo.civilizations.firstOrNull { it.civName.toCliInput() == name }
+    internal fun getCivByName(name: String) = gameInfo.civilizations.firstOrNull { it.civName.toCliInput() == name.toCliInput() }
         ?: throw ConsoleErrorException("Unknown civ: $name")
 
     internal fun getSelectedTile() = screen.mapHolder.selectedTile ?: throw ConsoleErrorException("Select tile")
@@ -96,7 +96,7 @@ class DevConsolePopup(val screen: WorldScreen) : Popup(screen) {
     /** Gets city by selected tile */
     internal fun getSelectedCity() = getSelectedTile().getCity() ?: throw ConsoleErrorException("Select tile belonging to city")
 
-    internal fun getCity(cityName:String) = gameInfo.getCities().firstOrNull { it.name.toCliInput() == cityName }
+    internal fun getCity(cityName:String) = gameInfo.getCities().firstOrNull { it.name.toCliInput() == cityName.toCliInput() }
         ?: throw ConsoleErrorException("Unknown city: $cityName")
 
     internal fun getSelectedUnit(): MapUnit {
