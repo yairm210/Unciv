08e1f5704ba6d466ba8ba25576f37199b0f14905
Yair Morgenstern <yairm210@hotmail.com>
2024-01-07 16:16:31 +0200

    Version rollout (#10884)
    * Bump version and create initial changelog entry
    
    * Update French.properties (#10875)
    
    * Update Italian.properties (#10873)
    
    * Update Korean.properties (#10872)
    
    * Update Korean.properties
    
    * Update Korean.properties
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: Giuseppe D'Addio <41149920+Smashfanful@users.noreply.github.com>
    Co-authored-by: Yattong the Mackerel <saud2410@naver.com>

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -464,7 +464,7 @@ open class UncivGame(val isConsoleMode: Boolean = false) : Game(), PlatformSpeci
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.9.16", 952)
+        val VERSION = Version("4.9.17", 953)
         //endregion
 
         lateinit var Current: UncivGame
