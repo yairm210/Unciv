dce866c0e0e1a79a74d75c597687950713f235a1
SeventhM <127357473+SeventhM@users.noreply.github.com>
2024-01-14 20:05:18 -0800

    Add getter/setter for easier access to focuses

--- core/src/com/unciv/models/ruleset/nation/Personality.kt
+++ core/src/com/unciv/models/ruleset/nation/Personality.kt
@@ -3,6 +3,19 @@ package com.unciv.models.ruleset.nation
 import com.unciv.Constants
 import com.unciv.models.ruleset.RulesetObject
 import com.unciv.models.ruleset.unique.UniqueTarget
+import kotlin.reflect.KMutableProperty0
+
+enum class PersonalityValue{
+    Production,
+    Food,
+    Gold,
+    Science,
+    Culture,
+    Happiness,
+    Faith,
+    Military,
+    WarMongering,
+}
 
 class Personality : RulesetObject() {
     var production: Float = 5f
@@ -13,9 +26,39 @@ class Personality : RulesetObject() {
     var happiness: Float = 5f
     var faith: Float = 5f
     var military: Float = 5f
+    var warMongering: Float = 5f
     var policy = LinkedHashMap<String, Int>()
     var preferredVictoryType: String = Constants.neutralVictoryType
-    
+
+    private fun nameToVariable(value: PersonalityValue):KMutableProperty0<Float> {
+        return when(value) {
+            PersonalityValue.Production -> ::production
+            PersonalityValue.Food -> ::food
+            PersonalityValue.Gold -> ::gold
+            PersonalityValue.Science -> ::science
+            PersonalityValue.Culture -> ::culture
+            PersonalityValue.Happiness -> ::happiness
+            PersonalityValue.Faith -> ::faith
+            PersonalityValue.Military -> ::military
+            PersonalityValue.WarMongering -> ::warMongering
+        }
+    }
+
+    /**
+     * Scales the value to a more meaningful range, where 10 is 2, and 5 is 1
+     */
+    fun scaledFocus(value: PersonalityValue): Float {
+        return nameToVariable(value).get() / 5
+    }
+
+    operator fun get(value: PersonalityValue){
+        nameToVariable(value).get()
+    }
+
+    operator fun set(personalityValue: PersonalityValue, value: Float){
+        nameToVariable(personalityValue).set(value)
+    }
+
     override fun getUniqueTarget() = UniqueTarget.Personailty
 
     override fun makeLink(): String {
