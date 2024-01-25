09651f711e6a5eca4538b3406ed666eb116c3970
Yair Morgenstern <yairm210@hotmail.com>
2024-01-25 23:36:56 +0200

    Version rollout (#11007)
    * Bump version and create initial changelog entry
    
    * Update French.properties (#11006)
    
    * updated belarusian translation (#11005)
    
    * update android/assets/jsons/translations/Belarusian.properties
    
    * update android/assets/jsons/translations/Belarusian.properties
    
    * update android/assets/jsons/translations/Belarusian.properties
    
    * update android/assets/jsons/translations/Belarusian.properties
    
    * update android/assets/jsons/translations/Belarusian.properties
    
    * update android/assets/jsons/translations/Belarusian.properties
    
    * update android/assets/jsons/translations/Belarusian.properties
    
    * update android/assets/jsons/translations/Belarusian.properties
    
    * update android/assets/jsons/translations/Belarusian.properties
    
    * update android/assets/jsons/translations/Belarusian.properties
    
    * Update Brazilian_Portuguese.properties (#11003)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: flac <87523240+twinflac@users.noreply.github.com>
    Co-authored-by: Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -464,7 +464,7 @@ open class UncivGame(val isConsoleMode: Boolean = false) : Game(), PlatformSpeci
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.10.2", 960)
+        val VERSION = Version("4.10.3", 961)
         //endregion
 
         lateinit var Current: UncivGame
