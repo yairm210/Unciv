ca14619a4291c0e25f1f21a739ce6f0d9fb85b7d
SeventhM <127357473+SeventhM@users.noreply.github.com>
2024-01-25 20:28:14 -0800

    Add additional comments to be added to kdocs later

--- core/src/com/unciv/models/ruleset/construction/RejectionReason.kt
+++ core/src/com/unciv/models/ruleset/construction/RejectionReason.kt
@@ -9,6 +9,7 @@ class RejectionReason(
 
     // Used in Battle.addXp - true means it's _not_ blocking a unit from being awarded for Great General points
     // Used in UnitUpgradeManager.canUpgrade - true means the `ignoreRequirements` parameter will filter these away
+    // true means it's limited by tech, policy, or era in some manner
     fun techPolicyEraWonderRequirements(): Boolean = type.isTechPolicyEraWonderRequirement
 
     // Used in CityConstructions.validateInProgressConstructions - true means remove+refund or replace with upgrade + transfer points
@@ -18,6 +19,7 @@ class RejectionReason(
     fun isImportantRejection(): Boolean = type.rejectionPrecedence >= 0
 
     // Used in Battle.addXp - true means it's _not_ blocking a unit from being awarded for Great General points
+    // true means the reason is specific to the city construction screen. Contains only the ones for units, not buildings 
     fun isConstructionRejection(): Boolean = type.isConstructionRejection
 
     /** Smaller numbers have the highest precedence (unless it's -1 in which case [isImportantRejection] returns `false`) */
