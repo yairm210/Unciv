a1dbf2d7277eb4218238ec13cc99092cc64df25f
Oskar Niesen <oskar-github@niesens.com>
2024-01-31 10:47:22 -0600

    Added a test for formation idle units

--- tests/src/com/unciv/logic/map/UnitFomationTests.kt
+++ tests/src/com/unciv/logic/map/UnitFomationTests.kt
@@ -3,6 +3,7 @@ package com.unciv.logic.map
 import com.badlogic.gdx.math.Vector2
 import com.unciv.logic.automation.unit.WorkerAutomation
 import com.unciv.logic.civilization.Civilization
+import com.unciv.logic.map.tile.RoadStatus
 import com.unciv.models.UnitActionType
 import com.unciv.testing.GdxTestRunner
 import com.unciv.testing.TestGame
@@ -33,9 +34,11 @@ internal class UnitFormationTests {
         assertFalse(civilianUnit.isEscorting())
         civilianUnit.startEscorting()
         assertTrue(civilianUnit.isEscorting())
+        assertTrue(militaryUnit.isEscorting())
         assertTrue(civilianUnit.getOtherEscortUnit() != null)
         civilianUnit.stopEscorting()
         assertFalse(civilianUnit.isEscorting())
+        assertFalse(militaryUnit.isEscorting())
         assertTrue(civilianUnit.getOtherEscortUnit() != null)
     }
 
@@ -49,9 +52,11 @@ internal class UnitFormationTests {
         assertFalse(militaryUnit.isEscorting())
         militaryUnit.startEscorting()
         assertTrue(militaryUnit.isEscorting())
+        assertTrue(civilianUnit.isEscorting())
         assertTrue(militaryUnit.getOtherEscortUnit() != null)
         militaryUnit.stopEscorting()
         assertFalse(militaryUnit.isEscorting())
+        assertFalse(civilianUnit.isEscorting())
         assertTrue(militaryUnit.getOtherEscortUnit() != null)
     }
 
@@ -70,5 +75,26 @@ internal class UnitFormationTests {
         assertFalse(militaryUnit.isEscorting())
         militaryUnit.startEscorting()
         assertFalse(militaryUnit.isEscorting())
+    }
+
+    @Test
+    fun `formation idle units`() {
+        setUp(1)
+        val centerTile = testGame.getTile(Vector2(0f,0f))
+        val civilianUnit = testGame.addUnit("Worker", civInfo, centerTile)
+        val militaryUnit = testGame.addUnit("Warrior", civInfo, centerTile)
+        civilianUnit.startEscorting()
+        assertTrue(civilianUnit.isIdle())
+        assertTrue(militaryUnit.isIdle())
+        civilianUnit.currentMovement = 0f
+        assertFalse(civilianUnit.isIdle())
+        assertFalse(militaryUnit.isIdle())
+        civilianUnit.currentMovement = 2f
+        for (improvement in listOf("Farm")) {
+            civInfo.tech.techsResearched.add(testGame.ruleset.tileImprovements[improvement]!!.techRequired!!)
+        }
+        centerTile.startWorkingOnImprovement(testGame.ruleset.tileImprovements["Farm"]!!, civInfo, civilianUnit)
+        assertFalse(civilianUnit.isIdle())
+        assertFalse(militaryUnit.isIdle())
     }
 }
