09413a3182b37d4ecfbe26ebcb09af25019f9029
SeventhM <127357473+SeventhM@users.noreply.github.com>
2024-02-09 02:19:42 -0800

    Add unique for increasing price every time it's built (#11100)
    * Add unique for increasing price every time it's built
    
    * Update name to be more accurate
    
    * whoops

--- core/src/com/unciv/logic/civilization/CivConstructions.kt
+++ core/src/com/unciv/logic/civilization/CivConstructions.kt
@@ -21,6 +21,9 @@ class CivConstructions : IsPartOfGameInfoSerialization {
     /** Maps construction names to the amount of times bought */
     val boughtItemsWithIncreasingPrice: Counter<String> = Counter()
 
+    /** Maps construction names to the amount of times built */
+    val builtItemsWithIncreasingCost: Counter<String> = Counter()
+
     /** Maps cities by id to a set of all free buildings by name they contain.
      *  The building name is the Nation-specific equivalent if available.
      *  Sources: [UniqueType.FreeStatBuildings] **and** [UniqueType.FreeSpecificBuildings]
@@ -51,6 +54,7 @@ class CivConstructions : IsPartOfGameInfoSerialization {
         toReturn.freeStatBuildingsProvided.putAll(freeStatBuildingsProvided)
         toReturn.freeSpecificBuildingsProvided.putAll(freeSpecificBuildingsProvided)
         toReturn.boughtItemsWithIncreasingPrice.add(boughtItemsWithIncreasingPrice)  // add copies
+        toReturn.builtItemsWithIncreasingCost.add(builtItemsWithIncreasingCost)
         return toReturn
     }
 
--- core/src/com/unciv/models/ruleset/Building.kt
+++ core/src/com/unciv/models/ruleset/Building.kt
@@ -105,6 +105,9 @@ class Building : RulesetStatsObject(), INonPerpetualConstruction {
     override fun getProductionCost(civInfo: Civilization): Int {
         var productionCost = cost.toFloat()
 
+        for (unique in getMatchingUniques(UniqueType.CostIncreasesWhenBuilt, StateForConditionals(civInfo)))
+            productionCost += civInfo.civConstructions.builtItemsWithIncreasingCost[name] * unique.params[0].toInt()
+
         for (unique in getMatchingUniques(UniqueType.CostIncreasesPerCity, StateForConditionals(civInfo)))
             productionCost += civInfo.cities.size * unique.params[0].toInt()
 
