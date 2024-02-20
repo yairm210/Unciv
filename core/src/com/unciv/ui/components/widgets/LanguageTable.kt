7d17b9296dcb7d0ed26f7c817f051ba1afcda888
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2024-02-20 22:24:54 +0100

    External links: Right-click and some housecleaning (#11155)
    * Right-click external links to copy url
    
    * Add hint about right-clicking links to UI Tips tutorial
    
    * Add wiki link to about and centralize repo/wiki external link bases

--- core/src/com/unciv/ui/components/widgets/LanguageTable.kt
+++ core/src/com/unciv/ui/components/widgets/LanguageTable.kt
@@ -59,7 +59,7 @@ internal class LanguageTable(val language:String, val percentComplete: Int) : Ta
                 text = "Please note that translations are a community-based work in progress and are" +
                     " INCOMPLETE! The percentage shown is how much of the language is translated in-game." +
                     " If you want to help translating the game into your language, click here.",
-                link = "https://yairm210.github.io/Unciv/Other/Translating/",
+                link = "${Constants.wikiURL}Translating/",
                 size = 15
             )
             add(MarkupRenderer.render(listOf(translationDisclaimer),expectedWidth)).pad(5f).row()
