5b04d1b0d6c2efd4d4265d80dc1a881af5607236
Yair Morgenstern <yairm210@hotmail.com>
2024-02-23 10:53:17 +0200

    Make terraform unique triggerable from improvements

--- core/src/com/unciv/models/ruleset/unique/UniqueType.kt
+++ core/src/com/unciv/models/ruleset/unique/UniqueType.kt
@@ -761,7 +761,7 @@ enum class UniqueType(
     SkipPromotion("Doing so will consume this opportunity to choose a Promotion", UniqueTarget.Promotion),
     FreePromotion("This Promotion is free", UniqueTarget.Promotion),
 
-    OneTimeChangeTerrain("Turn this tile into a [terrainName] tile", UniqueTarget.UnitTriggerable),
+    OneTimeChangeTerrain("Turn this tile into a [terrainName] tile", UniqueTarget.Triggerable),
 
     UnitsGainPromotion("[mapUnitFilter] units gain the [promotion] promotion", UniqueTarget.Triggerable),  // Not used in Vanilla
     FreeStatBuildings("Provides the cheapest [stat] building in your first [positiveAmount] cities for free", UniqueTarget.Triggerable),  // used in Policy
