d5fda541ad8b042ba4763a9ee56800b66135892e
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2024-03-05 22:04:40 +0100

    Competition quests in progress display tied leaders (and your place if you're behind) (#11224)
    * Minor Quest/QuestManager linting
    
    * Cache Quest and QuestName references and use them
    
    * Nicer randomWeighted and fix UniqueType.ResourceWeighting
    
    * Integrate @soggerr's #10739 - show tied leaders and your position if you aren't leading
    
    * One more lint

--- core/src/com/unciv/ui/screens/diplomacyscreen/CityStateDiplomacyTable.kt
+++ core/src/com/unciv/ui/screens/diplomacyscreen/CityStateDiplomacyTable.kt
@@ -81,7 +81,7 @@ class CityStateDiplomacyTable(private val diplomacyScreen: DiplomacyScreen) {
         val diplomaticMarriageButton = getDiplomaticMarriageButton(otherCiv)
         if (diplomaticMarriageButton != null) diplomacyTable.add(diplomaticMarriageButton).row()
 
-        for (assignedQuest in otherCiv.questManager.assignedQuests.filter { it.assignee == viewingCiv.civName }) {
+        for (assignedQuest in otherCiv.questManager.getAssignedQuestsFor(viewingCiv.civName)) {
             diplomacyTable.addSeparator()
             diplomacyTable.add(getQuestTable(assignedQuest)).row()
         }
@@ -464,8 +464,8 @@ class CityStateDiplomacyTable(private val diplomacyScreen: DiplomacyScreen) {
         if (quest.duration > 0)
             questTable.add("[${remainingTurns}] turns remaining".toLabel()).row()
         if (quest.isGlobal()) {
-            val leaderString = viewingCiv.gameInfo.getCivilization(assignedQuest.assigner).questManager.getLeaderStringForQuest(assignedQuest.questName)
-            if (leaderString != "")
+            val leaderString = viewingCiv.gameInfo.getCivilization(assignedQuest.assigner).questManager.getScoreStringForGlobalQuest(assignedQuest)
+            if (leaderString.isNotEmpty())
                 questTable.add(leaderString.toLabel()).row()
         }
 
