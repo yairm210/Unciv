24a1289bc4b77d493bd74a54db73970f537b4792
Oskar Niesen <oskar-github@niesens.com>
2024-04-05 21:11:47 -0500

    Added another test

--- tests/src/com/unciv/logic/civilization/EspionageTests.kt
+++ tests/src/com/unciv/logic/civilization/EspionageTests.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.math.Vector2
 import com.unciv.testing.GdxTestRunner
 import com.unciv.testing.TestGame
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -38,6 +39,19 @@ class EspionageTests {
         assertEquals(1.0, spy.getEfficiencyModifier(), 0.1)
         city.cityConstructions.addBuilding("Constabulary")
         assertEquals(0.75, spy.getEfficiencyModifier(), 0.1)
+    }
+
+    @Test
+    fun `Spy effectiveness can't go below zero`() {
+        val espionageManagerA = civA.espionageManager
+        val spy = espionageManagerA.addSpy()
+        val city = civB.addCity(Vector2(1f,1f))
+        spy.moveTo(city)
+        city.cityConstructions.addBuilding("Constabulary")
+        city.cityConstructions.addBuilding("Police Station")
+        city.cityConstructions.addBuilding("National Intelligence Agency")
+        city.cityConstructions.addBuilding("Great Firewall")
+        assertTrue(spy.getEfficiencyModifier() >= 0)
     }
 
 }
