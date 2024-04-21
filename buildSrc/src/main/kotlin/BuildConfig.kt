9d96b62189638c95246ce13661a13ce4bc821ff2
Yair Morgenstern <yairm210@hotmail.com>
2024-04-21 15:48:21 +0300

    Version rollout (#11500)
    * Bump version and create initial changelog entry
    
    * Update Spanish.properties (#11498)
    
    * Update Simplified_Chinese.properties (#11497)
    
    * Update Norwegian.properties (#11494)
    
    * Update Norwegian.properties
    
    Added multipe new translations, and edited older translations
    
    * Update Norwegian.properties
    
    Fixed missing placeholders
    
    * Update Norwegian.properties
    
    Added more translations
    
    * Update Norwegian.properties
    
    Fixed missing placeholders.
    
    * Update Persian_(Pinglish-UN).properties (#11493)
    
    * Update Filipino.properties (#11492)
    
    * Update Filipino.properties
    
    * Update Filipino.properties
    
    * Update Italian.properties (#11488)
    
    * Update Brazilian_Portuguese.properties (#11487)
    
    * Update French.properties (#11484)
    
    * Update Indonesian.properties (#11482)
    
    New translations for spies and others
    Fix other translations
    
    * Fix tests
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Caballero Arepa <78449553+Caballero-Arepa@users.noreply.github.com>
    Co-authored-by: AutumnPizazz <114050365+AutumnPizazz@users.noreply.github.com>
    Co-authored-by: Floxudoxu <67422605+Floxudoxu@users.noreply.github.com>
    Co-authored-by: Ilia <81512402+ilia-abbasi@users.noreply.github.com>
    Co-authored-by: Edizen P. Buguina <111935236+zen-pb@users.noreply.github.com>
    Co-authored-by: Giuseppe D'Addio <41149920+Smashfanful@users.noreply.github.com>
    Co-authored-by: Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: kensvin <kensvin28@gmail.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,8 +4,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.8.21"
     const val appName = "Unciv"
-    const val appCodeNumber = 988
-    const val appVersion = "4.11.5"
+    const val appCodeNumber = 989
+    const val appVersion = "4.11.6"
 
     const val gdxVersion = "1.12.1"
     const val ktorVersion = "2.2.3"
