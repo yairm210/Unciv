d4e5d63821f17f9870452bdac5146ae9baf2252d
Oskar Niesen <oskar-github@niesens.com>
2024-05-03 11:21:30 -0500

    Fixed a test

--- tests/src/com/unciv/logic/battle/BattleTest.kt
+++ tests/src/com/unciv/logic/battle/BattleTest.kt
@@ -381,8 +381,8 @@ class BattleTest {
         Battle.attackOrNuke(MapUnitCombatant(attackerUnit), AttackableTile(attackerUnit.getTile(), defaultDefenderUnit.currentTile, 0f, null))
 
         // then
-        assertEquals(-75f, defenderCiv.getDiplomacyManager(attackerCiv).opinionOfOtherCiv()) // 50 for nuke, 25 for war declaration
-        assertEquals(-55f, thirdCiv.getDiplomacyManager(attackerCiv).opinionOfOtherCiv()) // 50 for nuke, 5 for warmongering
+        assertEquals(-90f, defenderCiv.getDiplomacyManager(attackerCiv).opinionOfOtherCiv()) // 50 for nuke, 20 for war declaration, 20 for warmongering
+        assertEquals(-70f, thirdCiv.getDiplomacyManager(attackerCiv).opinionOfOtherCiv()) // 50 for nuke, 20 for warmongering
     }
 
     @Test
