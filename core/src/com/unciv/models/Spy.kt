02a09bf38135f28a9a84d26999dd97fb7c46850c
Oskar Niesen <oskar-github@niesens.com>
2024-05-13 16:55:06 -0500

    Fixed "failed staged" notification

--- core/src/com/unciv/models/Spy.kt
+++ core/src/com/unciv/models/Spy.kt
@@ -282,11 +282,11 @@ class Spy private constructor() : IsPartOfGameInfoSerialization {
             val allyCiv = cityState.getAllyCiv()?.let { civInfo.gameInfo.getCivilization(it) }
             val spy = allyCiv?.espionageManager?.getSpyAssignedToCity(getCity())
             cityState.getDiplomacyManager(civInfo).addInfluence(-20f)
-            allyCiv?.addNotification("A spy from [${civInfo.civName}] failed staged a coup in our ally [${cityState.civName}] and was killed!", getCity().location,
+            allyCiv?.addNotification("A spy from [${civInfo.civName}] failed to stag a coup in our ally [${cityState.civName}] and was killed!", getCity().location,
                     NotificationCategory.Espionage, civInfo.civName,  NotificationIcon.Spy, cityState.civName)
             allyCiv?.getDiplomacyManager(civInfo)?.addModifier(DiplomaticModifiers.SpiedOnUs, -10f)
 
-            civInfo.addNotification("Our spy [$name] failed staged a coup in [${cityState.civName}] and was killed!", getCity().location,
+            civInfo.addNotification("Our spy [$name] failed to stag a coup in [${cityState.civName}] and was killed!", getCity().location,
                     NotificationCategory.Espionage, civInfo.civName,  NotificationIcon.Spy, cityState.civName)
 
             killSpy()
