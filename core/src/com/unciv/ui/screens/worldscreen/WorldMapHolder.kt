d4cfd4e563da6a4e2b148a9b56a13ada0cc6c189
Oskar Niesen <tuvus@users.noreply.github.com>
2024-05-15 23:02:20 -0500

    AI doesn't settle very unfavorable locations (#11305)
    * AI doesn't settle locations with less than 50 value
    
    * Settlers try and found cities at a distance of 7 more
    
    * Reduced the value of being on the coast
    
    * RankTile quits early if the tile is already owned

--- core/src/com/unciv/ui/screens/worldscreen/WorldMapHolder.kt
+++ core/src/com/unciv/ui/screens/worldscreen/WorldMapHolder.kt
@@ -858,7 +858,7 @@ class WorldMapHolder(
         // Highlight best tiles for city founding
         if (unit.hasUnique(UniqueType.FoundCity)
                 && UncivGame.Current.settings.showSettlersSuggestedCityLocations) {
-            CityLocationTileRanker.getBestTilesToFoundCity(unit).tileRankMap.asSequence()
+            CityLocationTileRanker.getBestTilesToFoundCity(unit, minimumValue = 50f).tileRankMap.asSequence()
                 .filter { it.key.isExplored(unit.civ) }.sortedByDescending { it.value }.take(3).forEach {
                 tileGroups[it.key]!!.layerOverlay.showGoodCityLocationIndicator()
             }
