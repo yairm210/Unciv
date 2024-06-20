e4a40165fb852489ada3a1114ed4ec17db87f536
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2024-06-20 16:26:15 +0200

    ImprovementPickerScreen fully Civilopedia-linked

--- core/src/com/unciv/ui/screens/pickerscreens/ImprovementPickerScreen.kt
+++ core/src/com/unciv/ui/screens/pickerscreens/ImprovementPickerScreen.kt
@@ -2,7 +2,10 @@ package com.unciv.ui.screens.pickerscreens
 
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.Touchable
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.badlogic.gdx.utils.Align
 import com.unciv.Constants
 import com.unciv.logic.map.mapunit.MapUnit
@@ -76,6 +79,11 @@ class ImprovementPickerScreen(
             accept(selectedImprovement)
         }
 
+        descriptionLabel.onClick {
+            val link = selectedImprovement?.makeLink()
+            if (!link.isNullOrEmpty()) openCivilopedia(link)
+        }
+
         val regularImprovements = Table()
         regularImprovements.defaults().pad(5f)
 
@@ -96,92 +104,7 @@ class ImprovementPickerScreen(
             if (!unit.canBuildImprovement(improvement)) continue
             val problemReport = getProblemReport(improvement) ?: continue
 
-            val image = ImageGetter.getImprovementPortrait(improvement.name, 30f)
-
-            // allow multiple key mappings to technologically supersede each other
-            var shortcutKey = improvement.shortcutKey
-            if (shortcutKey != null) {
-                val techLevel = getRequiredTechColumn(improvement)
-                val isSuperseded = ruleset.tileImprovements.values.asSequence()
-                    // *other* improvements with same shortcutKey
-                    .filter { it.shortcutKey == improvement.shortcutKey && it != improvement }
-                    // civ can build it (checks tech researched)
-                    .filter { tile.improvementFunctions.canBuildImprovement(it, currentPlayerCiv) }
-                    // is technologically more advanced
-                    .filter { getRequiredTechColumn(it) > techLevel }
-                    .any()
-                // another supersedes this - ignore key binding
-                if (isSuperseded) shortcutKey = null
-            }
-
-            var labelText = improvement.name.tr(true)
-            val turnsToBuild = if (tile.improvementInProgress == improvement.name) tile.turnsToImprovement
-            else improvement.getTurnsToBuild(currentPlayerCiv, unit)
-
-            if (turnsToBuild > 0) labelText += " - $turnsToBuild${Fonts.turn}"
-            val provideResource = tile.hasViewableResource(currentPlayerCiv) && tile.tileResource.isImprovedBy(improvement.name)
-            if (provideResource) labelText += "\n" + "Provides [${tile.resource}]".tr()
-            val removeImprovement = (!improvement.isRoad()
-                    && !improvement.name.startsWith(Constants.remove)
-                    && improvement.name != Constants.cancelImprovementOrder)
-            if (tile.improvement != null && removeImprovement) labelText += "\n" + "Replaces [${tile.improvement}]".tr()
-
-            val statIcons = getStatIconsTable(provideResource, removeImprovement)
-
-            // get benefits of the new improvement
-            val stats = tile.stats.getStatDiffForImprovement(
-                improvement,
-                currentPlayerCiv,
-                tile.getCity(),
-                cityUniqueCache
-            )
-
-            //Warn when the current improvement will increase a stat for the tile,
-            // but the tile is outside of the range (> 3 tiles from any city center) that can be
-            // worked by a city's population
-            if (tile.owningCity != null
-                && !improvement.isRoad()
-                    && stats.values.any { it > 0f }
-                    && !improvement.name.startsWith(Constants.remove)
-                    && !tile.getTilesInDistance(currentPlayerCiv.modConstants.cityWorkRange)
-                        .any { it.isCityCenter() && it.getCity()!!.civ == currentPlayerCiv }
-            )
-                labelText += "\n" + "Not in city work range".tr()
-
-            val statsTable = getStatsTable(stats)
-            statIcons.add(statsTable).padLeft(13f)
-
-            regularImprovements.add(statIcons).align(Align.right)
-
-            val improvementButton = PickerPane.getPickerOptionButton(image, labelText)
-            // This is onClick without ActivationTypes.Keystroke equivalence - keys should select *and* close:
-            improvementButton.onActivation(type = ActivationTypes.Tap, noEquivalence = true) {
-                selectedImprovement = improvement
-                pick(improvement.name.tr())
-                descriptionLabel.setText(improvement.getDescription(ruleset))
-            }
-
-            improvementButton.onDoubleClick { accept(improvement) }
-
-            when {
-                improvement.name == tile.improvementInProgress ->
-                    improvementButton.color = Color.GREEN
-                problemReport.isQueueable() ->
-                    // TODO should be a skin ButtonStyle, this mixes with the style override from disable() below - which is a very wrong approach anyway
-                    improvementButton.setColor(0.625f, 1f, 0.625f, 1f) // #a0ffa0 - brightened GREEN
-            }
-
-            if (!problemReport.isEmpty() || tileMarkedForCreatesOneImprovement) {
-                improvementButton.disable()
-            } else if (shortcutKey != null) {
-                // Shortcut keys trigger what onDoubleClick does, not equivalent to single Click:
-                improvementButton.keyShortcuts.add(shortcutKey) { accept(improvement) }
-                improvementButton.addTooltip(shortcutKey)
-            }
-
-            regularImprovements.add(improvementButton)
-            regularImprovements.add(getExplanationActor(improvement, problemReport)).padLeft(10f)
-            regularImprovements.row()
+            regularImprovements.addImprovementRow(improvement, problemReport, cityUniqueCache)
         }
 
         val ownerTable = Table()
@@ -192,10 +115,14 @@ class ImprovementPickerScreen(
             button.onClick {
                 this.game.pushScreen(CityScreen(tile.getCity()!!, null, tile))
             }
-            ownerTable.add("Tile owned by [${tile.getOwner()!!.civName}] (You)".toLabel()).padLeft(10f)
+            val label = "Tile owned by [${tile.getOwner()!!.civName}] (You)".toLabel()
+            label.onClick { openCivilopedia(tile.getOwner()!!.nation.makeLink()) }
+            ownerTable.add(label).padLeft(10f)
             ownerTable.add(button).padLeft(20f)
         } else {
-            ownerTable.add("Tile owned by [${tile.getOwner()!!.civName}] - [${tile.getCity()!!.name}]".toLabel()).padLeft(10f)
+            val label = "Tile owned by [${tile.getOwner()!!.civName}] - [${tile.getCity()!!.name}]".toLabel()
+            label.onClick { openCivilopedia(tile.getOwner()!!.nation.makeLink()) }
+            ownerTable.add(label).padLeft(10f)
         }
 
         topTable.add(ownerTable)
@@ -203,12 +130,118 @@ class ImprovementPickerScreen(
         topTable.add(regularImprovements)
     }
 
+    private fun Table.addImprovementRow(improvement: TileImprovement, problemReport: ProblemReport, cityUniqueCache: LocalUniqueCache) {
+        val image = ImageGetter.getImprovementPortrait(improvement.name, 30f)
+
+        // allow multiple key mappings to technologically supersede each other
+        var shortcutKey = improvement.shortcutKey
+        if (shortcutKey != null) {
+            val techLevel = getRequiredTechColumn(improvement)
+            val isSuperseded = ruleset.tileImprovements.values.asSequence()
+                // *other* improvements with same shortcutKey
+                .filter { it.shortcutKey == improvement.shortcutKey && it != improvement }
+                // civ can build it (checks tech researched)
+                .filter { tile.improvementFunctions.canBuildImprovement(it, currentPlayerCiv) }
+                // is technologically more advanced
+                .filter { getRequiredTechColumn(it) > techLevel }
+                .any()
+            // another supersedes this - ignore key binding
+            if (isSuperseded) shortcutKey = null
+        }
+
+        var labelText = improvement.name.tr(true)
+        val turnsToBuild = if (tile.improvementInProgress == improvement.name) tile.turnsToImprovement
+        else improvement.getTurnsToBuild(currentPlayerCiv, unit)
+
+        if (turnsToBuild > 0) labelText += " - $turnsToBuild${Fonts.turn}"
+        val provideResource = tile.hasViewableResource(currentPlayerCiv) && tile.tileResource.isImprovedBy(improvement.name)
+        if (provideResource) labelText += "\n" + "Provides [${tile.resource}]".tr()
+        val removeImprovement = (!improvement.isRoad()
+            && !improvement.name.startsWith(Constants.remove)
+            && improvement.name != Constants.cancelImprovementOrder)
+        if (tile.improvement != null && removeImprovement) labelText += "\n" + "Replaces [${tile.improvement}]".tr()
+
+        val statIcons = getStatIconsTable(provideResource, removeImprovement)
+
+        // get benefits of the new improvement
+        val stats = tile.stats.getStatDiffForImprovement(
+            improvement,
+            currentPlayerCiv,
+            tile.getCity(),
+            cityUniqueCache
+        )
+
+        //Warn when the current improvement will increase a stat for the tile,
+        // but the tile is outside of the range (> 3 tiles from any city center) that can be
+        // worked by a city's population
+        if (tile.owningCity != null
+            && !improvement.isRoad()
+            && stats.values.any { it > 0f }
+            && !improvement.name.startsWith(Constants.remove)
+            && !tile.getTilesInDistance(currentPlayerCiv.modConstants.cityWorkRange)
+                .any { it.isCityCenter() && it.getCity()!!.civ == currentPlayerCiv }
+        )
+            labelText += "\n" + "Not in city work range".tr()
+
+        val statsTable = getStatsTable(stats)
+        statIcons.add(statsTable).padLeft(13f)
+
+        add(statIcons).align(Align.right)
+
+        val improvementButton = PickerPane.getPickerOptionButton(image, labelText)
+        // This is onClick without ActivationTypes.Keystroke equivalence - keys should select *and* close:
+        improvementButton.onActivation(type = ActivationTypes.Tap, noEquivalence = true) {
+            setDescription(improvement, Color.WHITE)
+            pick(improvement.name.tr())
+        }
+
+        when {
+            improvement.name == tile.improvementInProgress ->
+                improvementButton.color = Color.GREEN
+            problemReport.isQueueable() ->
+                // TODO should be a skin ButtonStyle, this mixes with the style override from disable() below - which is a very wrong approach anyway
+                improvementButton.setColor(0.625f, 1f, 0.625f, 1f) // #a0ffa0 - brightened GREEN
+        }
+
+        if (!problemReport.isEmpty() || tileMarkedForCreatesOneImprovement) {
+            improvementButton.disable()
+            // Now a little backhanded trick: We want to allow access to information on a disabled improvement
+            // isDisabled still prevents the Button class from firing its event, but our own ClickListener bypasses that and is fired from Actor code
+            improvementButton.touchable = Touchable.enabled
+            improvementButton.addListener(object : ClickListener() {
+                override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                    setDescription(improvement, Color.LIGHT_GRAY)
+                }
+            })
+        } else if (shortcutKey != null) {
+            improvementButton.onDoubleClick { accept(improvement) }
+            // Shortcut keys trigger what onDoubleClick does, not equivalent to single Click:
+            improvementButton.keyShortcuts.add(shortcutKey) { accept(improvement) }
+            improvementButton.addTooltip(shortcutKey)
+        }
+
+        add(improvementButton)
+        add(getExplanationActor(improvement, problemReport)).padLeft(10f)
+        row()
+    }
+
+    /** Sets the PickerPane's description and where in Civilopedia a click on it should go - but not the right side button */
+    private fun setDescription(improvement: TileImprovement, color: Color) {
+        selectedImprovement = improvement
+        descriptionLabel.setText(improvement.getDescription(ruleset))
+        descriptionLabel.color = color
+    }
+
     private fun getStatIconsTable(provideResource: Boolean, removeImprovement: Boolean): Table {
         val statIcons = Table()
 
         // icon for adding the resource by improvement
-        if (provideResource)
-            statIcons.add(ImageGetter.getResourcePortrait(tile.resource.toString(), 30f)).pad(3f)
+        if (provideResource) {
+            val resourceIcon = ImageGetter.getResourcePortrait(tile.resource!!, 30f) // `!!` is covered by provideResource
+            val link = ruleset.tileResources[tile.resource]?.makeLink()
+            if (!link.isNullOrEmpty()) resourceIcon.onClick { openCivilopedia(link) }
+            statIcons.add(resourceIcon).pad(3f)
+        }
 
         // icon for removing the resource by replacing improvement
         if (removeImprovement && tile.hasViewableResource(currentPlayerCiv) && tile.improvement != null && tile.tileResource.isImprovedBy(tile.improvement!!)) {
@@ -238,10 +271,10 @@ class ImprovementPickerScreen(
     private class ProblemReport {
         var suggestRemoval = false
         var removalImprovement: TileImprovement? = null
-        val proposedSolutions = mutableSetOf<String>()
+        /** `first` is the text, `second` the Civilopedia link */
+        val proposedSolutions = mutableSetOf<Pair<String, String?>>()
         fun isEmpty() = proposedSolutions.isEmpty()
         fun isQueueable() = removalImprovement != null && proposedSolutions.size == 1
-        fun toLabel() = proposedSolutions.joinToString("}\n{", "{", "}").toLabel()
     }
 
     private fun getProblemReport(improvement: TileImprovement) = getProblemReport(tile, tileWithoutLastTerrain, improvement)
@@ -263,7 +296,7 @@ class ImprovementPickerScreen(
                     val cannotRemoveReport = getProblemReport(tileWithoutLastTerrain!!, null, removalImprovement!!)
                         ?: return null
                     proposedSolutions.addAll(cannotRemoveReport.proposedSolutions)
-                    proposedSolutions.add("${Constants.remove}[${tile.lastTerrain.name}] first")
+                    proposedSolutions.add("${Constants.remove}[${tile.lastTerrain.name}] first" to removalImprovement!!.makeLink())
                 }
             }
 
@@ -272,17 +305,17 @@ class ImprovementPickerScreen(
                 for (tech in improvement.requiredTechnologies(ruleset)) {
                     val techEra = tech?.era(ruleset) ?: continue
                     if (techEra.eraNumber > maxEraNumber) return null
-                    proposedSolutions.add("Research [${tech.name}] first")
+                    proposedSolutions.add("Research [${tech.name}] first" to tech.makeLink())
                 }
             }
             if (ImprovementBuildingProblem.NotJustOutsideBorders in unbuildableBecause)
-                proposedSolutions.add("Have this tile close to your borders")
+                proposedSolutions.add("Have this tile close to your borders" to null)
             if (ImprovementBuildingProblem.OutsideBorders in unbuildableBecause)
-                proposedSolutions.add("Have this tile inside your empire")
+                proposedSolutions.add("Have this tile inside your empire" to null)
             if (ImprovementBuildingProblem.MissingResources in unbuildableBecause) {
                 val resources = improvement.getMatchingUniques(UniqueType.ConsumesResources)
                     .filter { currentPlayerCiv.getResourceAmount(it.params[1]) < it.params[0].toInt() }
-                    .map { "Acquire more [${it.params[1]}]" }
+                    .map { "Acquire more [${it.params[1]}]" to ruleset.tileResources[it.params[1]]?.makeLink() }
                 proposedSolutions.addAll(resources)
             }
         }
@@ -299,13 +332,15 @@ class ImprovementPickerScreen(
             else -> getPickNowButton { accept(improvement) }
         }
 
-        val label = report.toLabel()
-        if (!report.isQueueable()) return label
-
         return Table().apply {
             defaults().center()
-            add(label).padBottom(5f).row()
-            add(getPickNowButton { accept(report.removalImprovement, improvement) })
+            for ((text, link) in report.proposedSolutions) {
+                val label = text.toLabel()
+                if (!link.isNullOrEmpty()) label.onClick { openCivilopedia(link) }
+                add(label).padBottom(5f).row()
+            }
+            if (report.isQueueable())
+                add(getPickNowButton { accept(report.removalImprovement, improvement) }).padTop(5f)
         }
     }
 }
