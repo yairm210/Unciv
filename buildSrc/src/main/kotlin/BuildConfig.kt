cbd81938676286d92f02c6abca89c632ef6e5daf
Yair Morgenstern <yairm210@hotmail.com>
2024-06-22 23:54:31 +0300

    Version rollout (#11812)
    * Bump version and create initial changelog entry
    
    * Provide translations to Ukrainian (#11809)
    
    Co-authored-by: Kostiantyn Ko <mormat.iam@gmail.com>
    
    * Update Simplified_Chinese.properties (#11805)
    
    * Update Simplified_Chinese.properties
    
    Has been a long time since my last translation. (Seems to be, a few months?)
    
    * Update Simplified_Chinese.properties
    
    * Update Spanish.properties (#11797)
    
    * Update French.properties (#11796)
    
    * Update Indonesian.properties (#11794)
    
    Translated the new words.
    
    * Update Hungarian.properties (#11793)
    
    * Update Hungarian.properties
    
    * Update Hungarian.properties
    
    * Update Brazilian_Portuguese.properties (#11792)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Kostiantyn Ko <test9@mormat.name>
    Co-authored-by: Kostiantyn Ko <mormat.iam@gmail.com>
    Co-authored-by: heipizhu4 <100454479+heipizhu4@users.noreply.github.com>
    Co-authored-by: Caballero Arepa <78449553+Caballero-Arepa@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: X2003 <158255876+Xenia2003@users.noreply.github.com>
    Co-authored-by: balazsszalab <balazsszalab@freemail.hu>
    Co-authored-by: Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,8 +4,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.8.21"
     const val appName = "Unciv"
-    const val appCodeNumber = 1009
-    const val appVersion = "4.12.1"
+    const val appCodeNumber = 1010
+    const val appVersion = "4.12.2"
 
     const val gdxVersion = "1.12.1"
     const val ktorVersion = "2.2.3"
