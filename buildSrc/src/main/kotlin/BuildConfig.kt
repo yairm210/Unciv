d6ee828e4e59cc11ea99e8e14819b9698844015a
Yair Morgenstern <yairm210@hotmail.com>
2024-06-27 23:37:04 +0300

    Version rollout (#11871)
    * Bump version and create initial changelog entry
    
    * Update Catalan.properties (#11869)
    
    * Update Simplified_Chinese.properties (#11868)
    
    * Update Simplified_Chinese.properties
    
    * Update Simplified_Chinese.properties
    
    * Provide translations to Ukrainian (#11867)
    
    Co-authored-by: Kostiantyn Ko <mormat.iam@gmail.com>
    
    * Update Italian.properties (#11866)
    
    * Update French.properties (#11862)
    
    * galician translate (#11848)
    
    * Add files via upload
    
    * Update Galician.properties
    
    under construction
    
    * Update Galician.properties
    
    corrections and translated more
    
    * Update Galician.properties
    
    all lines translated, errors missing
    
    * Update Galician.properties
    
    Complete translation, revised brackets, pending text revision but already functional
    
    * Update Galician.properties
    
    * Update Galician.properties
    
    correxida liña 637 posto [time] que e o correcto.
    
    * Update Galician.properties
    
    revisado ata 2000
    
    * Update Galician.properties
    
    repair line 1496 and 1905
    
    * Update Galician.properties
    
    lines 1496 to 1499 repair
    
    * Update Galician.properties
    
    hei por he varias, liña 995 correxida
    
    * Update Galician.properties
    
    + correccions
    
    * Update Galician.properties
    
    starting the translation again and little by little to see if we can make it through this one, the other one pains me to say but it was real garbage.
    
    Only the first 500 lines have been translated to check for errors
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Joan Josep <juanjo.ng.83@gmail.com>
    Co-authored-by: 焕晨HChen <123531821+HChenX@users.noreply.github.com>
    Co-authored-by: Kostiantyn Ko <test9@mormat.name>
    Co-authored-by: Kostiantyn Ko <mormat.iam@gmail.com>
    Co-authored-by: Giuseppe D'Addio <41149920+Smashfanful@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: ninjum <138787523+ninjum@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,8 +4,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.9.24"
     const val appName = "Unciv"
-    const val appCodeNumber = 1013
-    const val appVersion = "4.12.4"
+    const val appCodeNumber = 1014
+    const val appVersion = "4.12.5"
 
     const val gdxVersion = "1.12.1"
     const val ktorVersion = "2.3.12"
