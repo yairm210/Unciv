809b0fc1f2376364c9988b5467c3d7b6e0c18b9e
EmperorPinguin <99119424+EmperorPinguin@users.noreply.github.com>
2024-06-28 14:16:34 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${com.unciv.build.BuildConfig.kotlinVersion}")
-        classpath("com.android.tools.build:gradle:8.5.0")
+        classpath("com.android.tools.build:gradle:8.2.2")
     }
 }
 
