df80b101735e9692134b9bdd544aac4106eced6b
DW8030 <99119424+EmperorPinguin@users.noreply.github.com>
2024-06-28 20:26:41 +0200

    Update CityLocationTileRanker.kt

--- core/src/com/unciv/logic/automation/unit/CityLocationTileRanker.kt
+++ core/src/com/unciv/logic/automation/unit/CityLocationTileRanker.kt
@@ -135,7 +135,6 @@ object CityLocationTileRanker {
             // We want a defensive ring around our capital
             if (city.civ == civ) distanceToCityModifier *= if (city.isCapital()) 2 else 1
             modifier += distanceToCityModifier
-
         }
         return modifier
     }
