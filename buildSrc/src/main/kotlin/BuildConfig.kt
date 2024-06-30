c2ca4e6acb7bacfaab03cf27e55952aae5daa668
yairm210 <yairm210@users.noreply.github.com>
2024-06-30 07:36:02 +0000

    4.12.6-patch1

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,8 +4,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.9.24"
     const val appName = "Unciv"
-    const val appCodeNumber = 1016
-    const val appVersion = "4.12.6"
+    const val appCodeNumber = 1017
+    const val appVersion = "4.12.6-patch1"
 
     const val gdxVersion = "1.12.1"
     const val ktorVersion = "2.3.12"
