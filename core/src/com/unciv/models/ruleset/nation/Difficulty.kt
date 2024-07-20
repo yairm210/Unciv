c402fe21a937d5dcedbaca45b11b40020e345c5d
Oskar Niesen <oskar-github@niesens.com>
2024-07-19 22:26:10 -0500

    Added AIDifficultyLevel to the difficulty object

--- core/src/com/unciv/models/ruleset/nation/Difficulty.kt
+++ core/src/com/unciv/models/ruleset/nation/Difficulty.kt
@@ -22,6 +22,7 @@ class Difficulty: INamed, ICivilopediaText {
     var barbarianSpawnDelay: Int = 0
     var playerBonusStartingUnits = ArrayList<String>()
 
+    var aiDifficultyLevel: String? = null
     var aiCityGrowthModifier: Float = 1f
     var aiUnitCostModifier: Float = 1f
     var aiBuildingCostModifier: Float = 1f
@@ -71,6 +72,7 @@ class Difficulty: INamed, ICivilopediaText {
 
         lines += FormattedLine()
         lines += FormattedLine("AI settings", header = 3)
+        lines += FormattedLine("{AI difficulty level}: ${aiDifficultyLevel}%", indent = 1)
         lines += FormattedLine("{AI city growth modifier}: ${aiCityGrowthModifier.toPercent()}% ${Fonts.food}", indent = 1)
         lines += FormattedLine("{AI unit cost modifier}: ${aiUnitCostModifier.toPercent()}% ${Fonts.production}", indent = 1)
         lines += FormattedLine("{AI building cost modifier}: ${aiBuildingCostModifier.toPercent()}% ${Fonts.production}", indent = 1)
