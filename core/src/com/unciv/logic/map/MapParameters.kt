fd850a8f6908b8570d261c66a91f3a03f51233c7
yairm210 <yairm210@hotmail.com>
2024-07-29 12:17:42 +0300

    Changed 'default map' parameters to rectangular + world wrap

--- core/src/com/unciv/logic/map/MapParameters.kt
+++ core/src/com/unciv/logic/map/MapParameters.kt
@@ -41,12 +41,12 @@ object MapType {
 class MapParameters : IsPartOfGameInfoSerialization {
     var name = ""
     var type = MapType.pangaea
-    var shape = MapShape.hexagonal
+    var shape = MapShape.rectangular
     var mapSize = MapSize.Medium
     var mapResources = MapResourceSetting.default.label
     var noRuins = false
     var noNaturalWonders = false
-    var worldWrap = false
+    var worldWrap = true
     var strategicBalance = false
     var legendaryStart = false
 
