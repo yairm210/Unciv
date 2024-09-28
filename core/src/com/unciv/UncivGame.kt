37bf6711c553e525369c0ef96e3d1cfca21fb5de
Yair Morgenstern <yairm210@hotmail.com>
2024-09-28 22:08:56 +0300

    Version rollout (#12249)
    * Bump version and create initial changelog entry
    
    * Update French.properties (#12246)
    
    * Update Italian.properties (#12244)
    
    * Update German translation (#12243)
    
    * Update Brazilian_Portuguese.properties (#12242)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: Giuseppe D'Addio <41149920+Smashfanful@users.noreply.github.com>
    Co-authored-by: mape6 <mape6@posteo.de>
    Co-authored-by: Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -482,7 +482,7 @@ open class UncivGame(val isConsoleMode: Boolean = false) : Game(), PlatformSpeci
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.13.11", 1053)
+        val VERSION = Version("4.13.12", 1054)
         //endregion
 
         /** Global reference to the one Gdx.Game instance created by the platform launchers - do not use without checking [isCurrentInitialized] first. */
