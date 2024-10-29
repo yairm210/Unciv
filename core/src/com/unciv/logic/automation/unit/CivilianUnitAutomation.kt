e6da28b5d38bbe2adc2c3de480240994a1a8f3e6
DW8030 <99119424+EmperorPinguin@users.noreply.github.com>
2024-10-29 18:03:33 +0100

    Update CivilianUnitAutomation.kt

--- core/src/com/unciv/logic/automation/unit/CivilianUnitAutomation.kt
+++ core/src/com/unciv/logic/automation/unit/CivilianUnitAutomation.kt
@@ -18,8 +18,6 @@ object CivilianUnitAutomation {
         && unit.civ.units.getCivUnits().any { unit.hasUnique(UniqueType.AddInCapital) }
 
     fun automateCivilianUnit(unit: MapUnit, dangerousTiles: HashSet<Tile>) {
-        if (tryRunAwayIfNeccessary(unit)) return
-
         if (shouldClearTileForAddInCapitalUnits(unit, unit.currentTile)) {
             // First off get out of the way, then decide if you actually want to do something else
             val tilesCanMoveTo = unit.movement.getDistanceToTiles()
@@ -30,6 +28,8 @@ object CivilianUnitAutomation {
 
         if (unit.hasUnique(UniqueType.FoundCity))
             return SpecificUnitAutomation.automateSettlerActions(unit, dangerousTiles)
+        
+        if (tryRunAwayIfNeccessary(unit)) return
 
         if (unit.isAutomatingRoadConnection())
             return unit.civ.getWorkerAutomation().roadToAutomation.automateConnectRoad(unit, dangerousTiles)
