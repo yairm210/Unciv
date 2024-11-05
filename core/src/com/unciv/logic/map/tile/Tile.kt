f1f37fcd74c3b8a88909b5ddfe4d7dcca03d5d96
yairm210 <yairm210@hotmail.com>
2024-11-05 14:48:54 +0200

    Don't allocate memory until you have to

--- core/src/com/unciv/logic/map/tile/Tile.kt
+++ core/src/com/unciv/logic/map/tile/Tile.kt
@@ -366,8 +366,8 @@ class Tile : IsPartOfGameInfoSerialization, Json.Serializable {
 
     fun isRoughTerrain() = allTerrains.any { it.isRough() }
 
-    @Transient
-    private val stateThisTile: StateForConditionals = StateForConditionals(tile = this)
+    @delegate:Transient
+    private val stateThisTile: StateForConditionals by lazy { StateForConditionals(tile = this) }
     /** Checks whether any of the TERRAINS of this tile has a certain unique */
     fun terrainHasUnique(uniqueType: UniqueType, state: StateForConditionals = stateThisTile) =
         terrainUniqueMap.getMatchingUniques(uniqueType, state).any()
