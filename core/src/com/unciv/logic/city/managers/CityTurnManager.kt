d632f04a5bb3b2ab0b2b0c08499e1ab4f2b2966c
yairm210 <yairm210@hotmail.com>
2024-11-11 13:16:57 +0200

    Fix 'raze city'

--- core/src/com/unciv/logic/city/managers/CityTurnManager.kt
+++ core/src/com/unciv/logic/city/managers/CityTurnManager.kt
@@ -124,9 +124,8 @@ class CityTurnManager(val city: City) {
             val removedPopulation =
                     1 + city.civ.getMatchingUniques(UniqueType.CitiesAreRazedXTimesFaster)
                         .sumOf { it.params[0].toInt() - 1 }
-            city.population.addPopulation(-1 * removedPopulation)
 
-            if (city.population.population <= 0) {
+            if (city.population.population <= removedPopulation) {
                 city.espionage.removeAllPresentSpies(SpyFleeReason.CityCaptured)
                 city.civ.addNotification(
                     "[${city.name}] has been razed to the ground!",
@@ -135,6 +134,7 @@ class CityTurnManager(val city: City) {
                 )
                 city.destroyCity()
             } else { //if not razed yet:
+                city.population.addPopulation(-1 * removedPopulation)
                 if (city.population.foodStored >= city.population.getFoodToNextPopulation()) { //if surplus in the granary...
                     city.population.foodStored =
                             city.population.getFoodToNextPopulation() - 1 //...reduce below the new growth threshold
