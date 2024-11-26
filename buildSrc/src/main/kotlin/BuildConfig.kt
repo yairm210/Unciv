45e86fef5d8be0e6d35ac71108de8b74d1d9ad9b
Yair Morgenstern <yairm210@hotmail.com>
2024-11-26 08:49:10 +0200

    Version rollout (#12525)
    * Bump version and create initial changelog entry
    
    * Update Latin translation (#12520)
    
    * Update translation
    
    * Update translation
    
    * Fix translation
    
    * Update translation
    
    * Update Brazilian_Portuguese.properties (#12518)
    
    * Update German translation (#12515)
    
    * Update Croatian.properties (#12513)
    
    * Update Croatian.properties
    
    * fix invalid placeholder
    
    * Update Croatian translation
    
    * revisited existing translation
    
    * Update 2 Croatian translation
    
    * Update 3 Croatian translation
    
    * check grammar and consistency of existing translation
    
    * Update 4 Croatian translation
    
    * Update 5 Croatian translation
    
    * fix invalid placeholders
    
    * fix missing placeholder
    
    * Update 6 Croatian translation
    
    * Update 7 Croatian translation
    
    * fix invalid placeholders
    
    * Update 8 Croatian translation
    
    * Translate buildings, difficulties and eras
    
    * check word usage consistency of existing translation
    
    * Partial translation of nations
    
    * Partial translation of nations 2
    
    * finish translation of nations
    
    * fix typos and grammar for translation of nations
    
    * Translate policies, quests, ruins and specialists
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: sls1005 <90055573+sls1005@users.noreply.github.com>
    Co-authored-by: Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>
    Co-authored-by: mape6 <mape6@posteo.de>
    Co-authored-by: metablaster <44481081+metablaster@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,8 +4,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.9.24"
     const val appName = "Unciv"
-    const val appCodeNumber = 1074
-    const val appVersion = "4.14.9"
+    const val appCodeNumber = 1075
+    const val appVersion = "4.14.10"
 
     const val gdxVersion = "1.12.1"
     const val ktorVersion = "2.3.12"
