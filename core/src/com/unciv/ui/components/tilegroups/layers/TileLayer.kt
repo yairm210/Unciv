907f0bc4eb2f824c94b39ef00016c5e1271d01f9
yairm210 <yairm210@hotmail.com>
2024-12-03 16:32:06 +0200

    Resolved #12555 - Fixed city names disappearing

--- core/src/com/unciv/ui/components/tilegroups/layers/TileLayer.kt
+++ core/src/com/unciv/ui/components/tilegroups/layers/TileLayer.kt
@@ -1,19 +1,20 @@
 package com.unciv.ui.components.tilegroups.layers
 
+import com.badlogic.gdx.scenes.scene2d.Group
 import com.badlogic.gdx.scenes.scene2d.Touchable
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.unciv.logic.civilization.Civilization
 import com.unciv.logic.map.tile.Tile
 import com.unciv.models.ruleset.unique.LocalUniqueCache
 import com.unciv.models.tilesets.TileSetCache
-import com.unciv.ui.components.NonTransformGroup
 import com.unciv.ui.components.tilegroups.TileGroup
 import com.unciv.ui.components.tilegroups.TileSetStrings
 
-abstract class TileLayer(val tileGroup: TileGroup, size: Float) : NonTransformGroup() {
+abstract class TileLayer(val tileGroup: TileGroup, size: Float) : Group() {
 
     init {
         touchable = Touchable.disabled
+        isTransform = false
         @Suppress("LeakingThis")
         setSize(size, size)
     }
