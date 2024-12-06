a84cdd4e8ed93e185021ce505f5c59dee9fc352c
yairm210 <yairm210@users.noreply.github.com>
2024-12-06 10:18:17 +0000

    4.14.13-patch1

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -486,7 +486,7 @@ open class UncivGame(val isConsoleMode: Boolean = false) : Game(), PlatformSpeci
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.14.13", 1080)
+        val VERSION = Version("4.14.13-patch1", 1081)
         //endregion
 
         /** Global reference to the one Gdx.Game instance created by the platform launchers - do not use without checking [isCurrentInitialized] first. */
