ee35a17cde7bcdc2aa293de0ae1f8faf8f0ed723
Md. Touhidur Rahman <touhidurrr@pm.me>
2024-12-09 20:54:08 +0600

    revert 855479f

--- core/src/com/unciv/ui/screens/worldscreen/NotificationsScroll.kt
+++ core/src/com/unciv/ui/screens/worldscreen/NotificationsScroll.kt
@@ -203,8 +203,6 @@ class NotificationsScroll(
             coveredNotificationsBottom + restoreButtonPad,
             Align.bottomRight)
     }
-    
-    private val serverIconsToIgnore = hashSetOf("NotificationIcons/RobotArm", "NotificationIcons/ServerRack", "NotificationIcons/DollarSign")
 
     private fun updateContent(
         notifications: List<Notification>,
@@ -266,9 +264,6 @@ class NotificationsScroll(
                 notificationsTable.add(header).right().row()
             }
             for (notification in categoryNotifications) {
-                // Temporarily ignore touhidurrr notifications
-                if (notification.icons.any { it in serverIconsToIgnore }) continue
-                
                 val item = ListItem(notification, backgroundDrawable)
                 itemWidths.add(item.itemWidth)
                 val itemCell = notificationsTable.add(item)
