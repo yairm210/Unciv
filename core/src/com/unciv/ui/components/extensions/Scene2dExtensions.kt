9082e2ab3b5d32430af6a0519b8b7e3f06fe5c54
Toxile <simonm.silver@protonmail.com>
2024-12-14 16:10:02 +0000

    UI tweaks and fixes: dividers, checkbox-to-text spacing, multiplayer tab connection button (#12646)
    * [1]
    
    * [2]
    
    * [3]
    
    * [1]
    
    * Revert "[2]"
    
    This reverts commit b9b88d8a1593c1d0e4387d94f2b3e0d976d8e14b.
    
    * [2] global

--- core/src/com/unciv/ui/components/extensions/Scene2dExtensions.kt
+++ core/src/com/unciv/ui/components/extensions/Scene2dExtensions.kt
@@ -206,7 +206,7 @@ private fun getSeparatorImage(color: Color) = Image(ImageGetter.getWhiteDotDrawa
  * Create a horizontal separator as an empty Container with a colored background.
  * @param colSpan Optionally override [colspan][Cell.colspan] which defaults to the current column count.
  */
-fun Table.addSeparator(color: Color = Color.WHITE, colSpan: Int = 0, height: Float = 2f): Cell<Image> {
+fun Table.addSeparator(color: Color = BaseScreen.skin.getColor("color"), colSpan: Int = 0, height: Float = 1f): Cell<Image> {
     if (!cells.isEmpty && !cells.last().isEndRow) row()
     val separator = getSeparatorImage(color)
     val cell = add(separator)
@@ -323,7 +323,7 @@ fun String.toCheckBox(startsOutChecked: Boolean = false, changeAction: ((Boolean
         }
         // Add a little distance between the icon and the text. 0 looks glued together,
         // 5 is about half an uppercase letter, and 1 about the width of the vertical line in "P".
-        imageCell.padRight(1f)
+        imageCell.padRight(Constants.defaultFontSize / 2.0f)
     }
 
 /** Sets the [font color][Label.LabelStyle.fontColor] on a [Label] and returns it to allow chaining */
