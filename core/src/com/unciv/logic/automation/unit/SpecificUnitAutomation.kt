bcd2ec77022070ffdb817c57733042803547356d
EmperorPinguin <99119424+EmperorPinguin@users.noreply.github.com>
2024-12-15 10:29:42 +0100

    Update SpecificUnitAutomation.kt

--- core/src/com/unciv/logic/automation/unit/SpecificUnitAutomation.kt
+++ core/src/com/unciv/logic/automation/unit/SpecificUnitAutomation.kt
@@ -190,9 +190,14 @@ object SpecificUnitAutomation {
                 throw Exception("City within distance")
             return
         }
+
+        val shouldSettle = (unit.getTile() == bestCityLocation && unit.hasMovement())
+        if (shouldSettle) return foundCityAction.action.invoke()
+        //Settle if we're already on the best tile, before looking if we should retreat from barbarians
+        if (tryRunAwayIfNeccessary(unit)) return 
         unit.movement.headTowards(bestCityLocation)
-        if (unit.getTile() == bestCityLocation && unit.hasMovement())
-            foundCityAction.action.invoke()
+        if (shouldSettle) foundCityAction.action.invoke() 
+        //TODO: evaluate 1-tile move with settle on same turn as "safe"
     }
 
     /** @return whether there was any progress in placing the improvement. A return value of `false`
