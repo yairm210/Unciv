70baf930122e67cec41d4bfe61811a14be7086d5
Yair Morgenstern <yairm210@hotmail.com>
2025-01-07 22:48:49 +0200

    Version rollout (#12766)
    * Bump version and create initial changelog entry
    
    * Update Indonesian.properties (#12764)
    
    * Update Indonesian.properties
    
    Translate new strings
    
    * Update Indonesian.properties
    
    Fix space error
    
    * Update Indonesian.properties
    
    Fix space
    
    * Update Indonesian.properties
    
    Try fixing space error
    
    * Update German translation (#12763)
    
    * translate battle action lines (#12760)
    
    * Update Dutch.properties (#12759)
    
    * Update Dutch.properties
    
    * Update Dutch.properties
    
    * Update French.properties (#12758)
    
    * Update Italian.properties (#12757)
    
    * Update Italian.properties
    
    * Update Italian.properties
    
    * Update Spanish.properties (#12751)
    
    I found some extra spaces " ".
    
    * Update Brazilian_Portuguese.properties (#12750)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: kensvin <kensvin28@gmail.com>
    Co-authored-by: mape6 <mape6@posteo.de>
    Co-authored-by: metablaster <44481081+metablaster@users.noreply.github.com>
    Co-authored-by: dutchwolfgirl <110766475+dutchwolfgirl@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: Giuseppe D'Addio <41149920+Smashfanful@users.noreply.github.com>
    Co-authored-by: Nahuel Geno <142752140+NahuelGeno@users.noreply.github.com>
    Co-authored-by: Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,8 +4,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.9.24"
     const val appName = "Unciv"
-    const val appCodeNumber = 1088
-    const val appVersion = "4.15.0"
+    const val appCodeNumber = 1089
+    const val appVersion = "4.15.1"
 
     const val gdxVersion = "1.12.1"
     const val ktorVersion = "2.3.12"
