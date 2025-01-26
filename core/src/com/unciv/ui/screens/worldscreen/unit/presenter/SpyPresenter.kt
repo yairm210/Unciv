477c6c5df56ee04010f92d1b40024e8ad05cfc26
sulai <6741822+sulai@users.noreply.github.com>
2025-01-26 10:24:46 +0100

    UnitTable: show a summary when no unit is selected (#12832)
    * Show a summary when no unit is selected. Allows to start cycling units any time. Refactor UnitTable to have separate Presenters for units, cities, spies and no selection.
    
    * fix SummaryPresenter hiding close button
    
    ---------
    
    Co-authored-by: M. Rittweger <m.rittweger@mvolution.de>

---
+++ core/src/com/unciv/ui/screens/worldscreen/unit/presenter/SpyPresenter.kt
@@ -0,0 +1,44 @@
+package com.unciv.ui.screens.worldscreen.unit.presenter
+
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.math.Vector2
+import com.unciv.models.Spy
+import com.unciv.ui.images.ImageGetter
+import com.unciv.ui.screens.worldscreen.unit.UnitTable
+
+class SpyPresenter(private val unitTable: UnitTable) : UnitTable.Presenter {
+
+    var selectedSpy: Spy? = null
+
+    override val position: Vector2?
+        get() = selectedSpy?.getCityOrNull()?.location
+    
+    fun selectSpy(spy: Spy?) {
+        selectedSpy = spy
+    }
+
+    override fun updateWhenNeeded() = with(unitTable) {
+        val spy = selectedSpy!!
+        unitNameLabel.clearListeners()
+        unitNameLabel.setText(spy.name)
+        descriptionTable.clear()
+
+        unitIconHolder.clear()
+        unitIconHolder.add(ImageGetter.getImage("OtherIcons/Spy_White").apply {
+            color = Color.WHITE
+        }).size(30f)
+
+        separator.isVisible = true
+        val color = when (spy.rank) {
+            1 -> Color.BROWN
+            2 -> Color.LIGHT_GRAY
+            3 -> Color.GOLD
+            else -> ImageGetter.CHARCOAL
+        }
+        repeat(spy.rank) {
+            val star = ImageGetter.getImage("OtherIcons/Star")
+            star.color = color
+            descriptionTable.add(star).size(20f).pad(1f)
+        }
+    }
+}
