53db1ce53107580ed381b4cde29b62507ce89fad
Yair Morgenstern <yairm210@hotmail.com>
2025-02-01 23:25:07 +0200

    Version rollout (#12895)
    * Bump version and create initial changelog entry
    
    * Update Latin translation (#12891)
    
    * Update Catalan.properties (#12887)
    
    * Update Spanish.properties (#12886)
    
    * Update Thai.properties (#12885)
    
    Add more Thai translated
    
    * Update French.properties (#12884)
    
    * fixing a word (#12883)
    
    * translate new lines (#12881)
    
    * Update Italian.properties (#12880)
    
    * Uppdate German translation (#12879)
    
    * Update Indonesian.properties (#12878)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: sls1005 <90055573+sls1005@users.noreply.github.com>
    Co-authored-by: Joan Josep <juanjo.ng.83@gmail.com>
    Co-authored-by: Nahuel Geno <142752140+NahuelGeno@users.noreply.github.com>
    Co-authored-by: Tanetpon phiewluang <61853754+AikaChan132@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: João Pedro Kuhn <pedrojoaojpk@gmail.com>
    Co-authored-by: metablaster <44481081+metablaster@users.noreply.github.com>
    Co-authored-by: Giuseppe D'Addio <41149920+Smashfanful@users.noreply.github.com>
    Co-authored-by: mape6 <mape6@posteo.de>
    Co-authored-by: X2003 <158255876+Xenia2003@users.noreply.github.com>

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -487,7 +487,7 @@ open class UncivGame(val isConsoleMode: Boolean = false) : Game(), PlatformSpeci
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.15.6-patch1", 1097)
+        val VERSION = Version("4.15.7", 1098)
         //endregion
 
         /** Global reference to the one Gdx.Game instance created by the platform launchers - do not use without checking [isCurrentInitialized] first. */
