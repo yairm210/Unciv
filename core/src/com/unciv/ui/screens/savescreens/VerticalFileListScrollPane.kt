6a8a5e82f6687a9909b9d14992e610b29bb435f6
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2025-04-08 17:20:02 +0200

    Change VerticalFileListScrollPane button handling to eliminate Cell lookup

--- core/src/com/unciv/ui/screens/savescreens/VerticalFileListScrollPane.kt
+++ core/src/com/unciv/ui/screens/savescreens/VerticalFileListScrollPane.kt
@@ -14,6 +14,7 @@ import com.unciv.ui.components.widgets.LoadingImage
 import com.unciv.ui.screens.basescreen.BaseScreen
 import com.unciv.utils.Concurrency
 import com.unciv.utils.launchOnGLThread
+import kotlin.math.abs
 
 //todo key auto-repeat for navigation keys?
 
@@ -26,9 +27,12 @@ class VerticalFileListScrollPane(
     private val existingSavesTable: Table = Table()
 ) : AutoScrollPane(existingSavesTable) {
 
-    private val savesPerButton = mutableMapOf<TextButton, FileHandle>()
+    private class FileHandleButton(val file: FileHandle, val index: Int) : TextButton(file.name(), BaseScreen.skin)
 
-    private var previousSelection: TextButton? = null
+    private val buttonIndex = ArrayList<FileHandleButton>()
+
+    private var selectedIndex = -1
+    private var selectedButton: FileHandleButton? = null
 
     private var onChangeListener: ((FileHandle) -> Unit)? = null
     private var onDoubleClickListener: ((FileHandle) -> Unit)? = null
@@ -58,7 +62,8 @@ class VerticalFileListScrollPane(
     /** repopulate from a FileHandle Sequence - for other sources than saved games */
     fun update(files: Sequence<FileHandle>) {
         existingSavesTable.clear()
-        previousSelection = null
+        selectedIndex = -1
+        selectedButton = null
         val loadingImage = LoadingImage(62f, LoadingImage.Style(innerSizeFactor = 0.8f))
         existingSavesTable.add(loadingImage)
         loadingImage.show()
@@ -72,83 +77,120 @@ class VerticalFileListScrollPane(
             launchOnGLThread {
                 loadingImage.hide()
                 existingSavesTable.clear()
-                savesPerButton.clear()
-                for (saveGameFile in saves) {
-                    val textButton = TextButton(saveGameFile.name(), BaseScreen.skin)
-                    savesPerButton[textButton] = saveGameFile
-                    textButton.onClick {
-                        selectExistingSave(textButton)
+                buttonIndex.clear()
+                buttonIndex.ensureCapacity(saves.size)
+                for ((index, saveGameFile) in saves.withIndex()) {
+                    val button = FileHandleButton(saveGameFile, index)
+                    buttonIndex.add(button)
+                    button.onClick {
+                        selectExistingSave(index)
                     }
-                    textButton.onDoubleClick {
-                        selectExistingSave(textButton)
+                    button.onDoubleClick {
+                        selectExistingSave(index)
                         onDoubleClickListener?.invoke(saveGameFile)
                     }
-                    existingSavesTable.add(textButton).pad(5f).row()
+                    existingSavesTable.add(button).pad(5f).row()
                 }
             }
         }
     }
 
-    private fun selectExistingSave(textButton: TextButton) {
-        previousSelection?.color = Color.WHITE
-        textButton.color = Color.GREEN
-        previousSelection = textButton
+    private fun selectExistingSave(index: Int) = selectExistingSave(index, buttonIndex[index])
+    private fun selectExistingSave(button: FileHandleButton) = selectExistingSave(button.index, button)
+    private fun selectExistingSave(index: Int, button: FileHandleButton) {
+        selectedIndex = index
+        selectedButton?.color = Color.WHITE
+        button.color = Color.GREEN
+        selectedButton = button
 
-        val saveGameFile = savesPerButton[textButton] ?: return
-        onChangeListener?.invoke(saveGameFile)
+        onChangeListener?.invoke(button.file)
     }
 
     //region Keyboard scrolling
 
+    // Note: We don't put the "scroll into view" functionality into selectExistingSave,
+    //       because the Page/Home+End keys scroll differently or simpler.
+    //       PgUp/PgDown will keep the vertical position of the selection mostly constant in long lists.
+
     // Helpers to simplify Scroll positioning - ScrollPane.scrollY goes down, normal Gdx Y goes up
     // These functions all operate in the scrollY 'coordinate system'
-    private fun Table.getVerticalSpan(button: TextButton): ClosedFloatingPointRange<Float> {
+    private fun Table.getVerticalSpan(button: FileHandleButton): ClosedFloatingPointRange<Float> {
         val invertedY = height - button.y
         return (invertedY - button.height)..invertedY
     }
     private fun getVerticalSpan() = scrollY..(scrollY + height)
-    private fun Table.getButtonAt(y: Float) = cells[getRow(height - y)].actor as TextButton
 
-    private fun onArrowKey(direction: Int) {
-        if (existingSavesTable.rows == 0) return
-        val rowIndex = if (previousSelection == null)
-            if (direction == 1) -1 else 0
-        else existingSavesTable.getCell(previousSelection).row
-        val newRow = (rowIndex + direction).let {
-            if (it < 0) existingSavesTable.rows - 1
-            else if (it >= existingSavesTable.rows) 0
-            else it
+    private fun getButtonAt(y: Float): FileHandleButton {
+        // Note: existingSavesTable.getRow(y) has no advantage
+        var l = 0
+        var h = buttonIndex.size - 1
+        var bestIndex = 0
+        var bestDistance = 1e6f
+        while (l <= h) {
+            val index = (l + h) / 2
+            val button = buttonIndex[index]
+            val span = existingSavesTable.getVerticalSpan(button)
+            val distance = if (span.start > y) y - span.start // go up
+                else if (span.endInclusive < y) y - span.endInclusive // go down
+                else return button // hit
+            if (abs(distance) < bestDistance) {
+                bestDistance = abs(distance)
+                bestIndex = index
+            }
+            if (distance < 0) h = index - 1 else l = index + 1
         }
-        val button = existingSavesTable.cells[newRow].actor as TextButton
-        selectExistingSave(button)
+        return buttonIndex[bestIndex]
+    }
 
+    private fun scrollTo() {
         // Make ScrollPane follow the selection
+        // similar to ScrollPane.scrollTo, but that method fails with some vertical offset
+        val button = selectedButton ?: return
         val buttonSpan = existingSavesTable.getVerticalSpan(button)
         val scrollSpan = getVerticalSpan()
         if (buttonSpan.start < scrollSpan.start)
-            scrollY = buttonSpan.start
+            scrollY = buttonSpan.start - 10f
         if (buttonSpan.endInclusive > scrollSpan.endInclusive)
-            scrollY = buttonSpan.endInclusive - height
+            scrollY = buttonSpan.endInclusive - height + 10f
+    }
+
+    private fun onArrowKey(direction: Int) {
+        if (buttonIndex.size == 0) return
+
+        fun wrapAround(index: Int) = when {
+            index < 0 -> buttonIndex.size - 1
+            index >= buttonIndex.size -> 0
+            else -> index
+        } 
+        val newIndex = wrapAround(selectedIndex + direction)
+        selectExistingSave(newIndex)
+        scrollTo()
     }
 
     private fun onPageKey(direction: Int) {
-        scrollY += (height - 60f) * direction  // ScrollPane does the clamping to 0..maxY
-        val buttonHeight = previousSelection?.height ?: return
-        val buttonSpan = existingSavesTable.getVerticalSpan(previousSelection!!)
+        val distance = (height - 60f) * direction
+        scrollY += distance  // ScrollPane does the clamping to 0..maxY
+        if (selectedButton == null) return
+        val buttonSpan = existingSavesTable.getVerticalSpan(selectedButton!!)
+        val newButtonY = (buttonSpan.start + buttonSpan.endInclusive) / 2 + distance;
+/*
+        // Alternate behaviour: Selection follows "reluctantly" by moving it the least distance enough to stay in viewport
+        val buttonHeight = selectedButton?.height ?: return
         val scrollSpan = getVerticalSpan()
         val newButtonY = if (buttonSpan.start < scrollSpan.start)
             scrollSpan.start + buttonHeight
         else if (buttonSpan.endInclusive > scrollSpan.endInclusive)
             scrollSpan.endInclusive - buttonHeight
         else return
-        selectExistingSave(existingSavesTable.getButtonAt(newButtonY))
+*/
+        selectExistingSave(getButtonAt(newButtonY))
+        scrollTo() // mostly not needed, but some drift may end up selecting a button slightly outside the viewport
     }
 
     private fun onHomeEndKey(direction: Int) {
         scrollY = direction * maxY
-        if (existingSavesTable.rows == 0) return
-        val row = (existingSavesTable.rows - 1) * direction
-        selectExistingSave(existingSavesTable.cells[row].actor as TextButton)
+        if (buttonIndex.size == 0) return
+        selectExistingSave((buttonIndex.size - 1) * direction)
     }
     //endregion
 }
