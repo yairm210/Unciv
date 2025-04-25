0220adcb22282f40cdc0dca875f19d7f64b1b108
yairm210 <yairm210@hotmail.com>
2025-04-25 15:33:07 +0300

    Resolved #13243 - Triggerable uniques on promotions will only activate once when added to unit on creation

--- core/src/com/unciv/logic/civilization/managers/UnitManager.kt
+++ core/src/com/unciv/logic/civilization/managers/UnitManager.kt
@@ -9,6 +9,7 @@ import com.unciv.logic.civilization.NotificationCategory
 import com.unciv.logic.civilization.transients.CivInfoTransientCache
 import com.unciv.logic.map.mapunit.MapUnit
 import com.unciv.logic.map.tile.Tile
+import com.unciv.models.ruleset.unique.UniqueTarget
 import com.unciv.models.ruleset.unique.UniqueTriggerActivation
 import com.unciv.models.ruleset.unique.UniqueType
 import com.unciv.models.ruleset.unit.BaseUnit
@@ -100,7 +101,9 @@ class UnitManager(val civInfo: Civilization) {
         if (unit != null) {
             val triggerNotificationText = "due to gaining a [${unit.name}]"
             for (unique in unit.getUniques())
-                if (!unique.hasTriggerConditional() && unique.conditionalsApply(unit.cache.state))
+                // Promotion triggerables are ALREADY handled in placeUnitNearTile -> addPromotion, where they were added
+                if (!unique.hasTriggerConditional() && unique.sourceObjectType != UniqueTarget.Promotion
+                    && unique.conditionalsApply(unit.cache.state))
                     UniqueTriggerActivation.triggerUnique(unique, unit, triggerNotificationText = triggerNotificationText)
 
             for (unique in civInfo.getTriggeredUniques(UniqueType.TriggerUponGainingUnit, unit.cache.state) 
