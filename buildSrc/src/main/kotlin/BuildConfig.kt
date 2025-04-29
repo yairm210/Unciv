6618d03bb76b6e571aff272b7583083ac9492966
Yair Morgenstern <yairm210@hotmail.com>
2025-04-29 22:47:33 +0300

    Version rollout (#13260)
    * Bump version and create initial changelog entry
    
    * Update Japanese.properties (#13255)
    
    * Update French.properties (#13252)
    
    * Update Brazilian_Portuguese.properties (#13251)
    
    * Update Italian.properties (#13249)
    
    * Update Indonesian.properties (#13247)
    
    * Update Catalan.properties and linux/unciv.desktop (#13256)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: cknight828 <128895471+cknight828@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>
    Co-authored-by: Giuseppe D'Addio <41149920+Smashfanful@users.noreply.github.com>
    Co-authored-by: X2003 <158255876+Xenia2003@users.noreply.github.com>
    Co-authored-by: Joan Josep <juanjo.ng.83@gmail.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,8 +4,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.9.24"
     const val appName = "Unciv"
-    const val appCodeNumber = 1120
-    const val appVersion = "4.16.5"
+    const val appCodeNumber = 1121
+    const val appVersion = "4.16.6"
 
     const val gdxVersion = "1.13.1"
     const val ktorVersion = "2.3.12"
