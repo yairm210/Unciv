9ab83b6a4289f7fe96b82c6aae4b4d23c9bc7ca6
Emandac <121508218+Emandac@users.noreply.github.com>
2025-06-09 19:59:17 +0200

    update 0

--- core/src/com/unciv/ui/screens/worldscreen/unit/actions/UnitActionsFromUniques.kt
+++ core/src/com/unciv/ui/screens/worldscreen/unit/actions/UnitActionsFromUniques.kt
@@ -44,13 +44,28 @@ object UnitActionsFromUniques {
             UniqueType.FoundCity).firstOrNull() ?: 
             UnitActionModifiers.getUsableUnitActionUniques(unit,
             UniqueType.FoundPuppetCity).firstOrNull() ?: return null
+        
+        val canFoundCityOnCoastTile = UnitActionModifiers.getUsableUnitActionUniques(unit,
+            UniqueType.FoundCityOnCoastTile).firstOrNull()
+        
+        fun canSettleOnCoastTile(): Boolean { // I put this in a function to be more readable :/
+            return canFoundCityOnCoastTile != null && tile.canSettledWaterTile()
+                && tile.isAdjacentTo("Land") && unit.isEmbarked()
+                && tile.isWater
+        }
 
-        if (tile.isWater || tile.isImpassible()) return null
+        println(tile.canSettledWaterTile())
+        println("tile os cosat ${tile.isAdjacentTo("Land")}")
+        
+        if (tile.isWater && canFoundCityOnCoastTile == null || tile.isImpassible()) return null
         // Spain should still be able to build Conquistadors in a one city challenge - but can't settle them
         if (unit.civ.isOneCityChallenger() && unit.civ.hasEverOwnedOriginalCapital) return null
 
-        if (!unit.hasMovement() || !tile.canBeSettled())
-            return UnitAction(UnitActionType.FoundCity, 80f, action = null)
+        
+        if (!canSettleOnCoastTile()) {
+            if (!unit.hasMovement() || !tile.canBeSettled())
+                return UnitAction(UnitActionType.FoundCity, 80f, action = null)
+        }
 
         val hasActionModifiers = unique.modifiers.any { it.type?.targetTypes?.contains(
             UniqueTarget.UnitActionModifier
