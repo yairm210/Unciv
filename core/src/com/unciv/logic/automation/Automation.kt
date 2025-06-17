aefbeae6bc6cb6c0deca7ee7d57810c45a2a1bde
EmperorPinguin <99119424+EmperorPinguin@users.noreply.github.com>
2025-06-17 15:09:56 +0200

    Autoplay: don't assign citizens according to AI Personality (#13453)
    * Update Automation.kt
    
    * Update core/src/com/unciv/logic/automation/Automation.kt
    
    ---------
    
    Co-authored-by: Yair Morgenstern <yairm210@hotmail.com>

--- core/src/com/unciv/logic/automation/Automation.kt
+++ core/src/com/unciv/logic/automation/Automation.kt
@@ -170,9 +170,11 @@ object Automation {
             yieldStats.production /= 6
         }
 
-        for (stat in Stat.entries) {
-            val scaledFocus = civPersonality.scaledFocus(PersonalityValue[stat])
-            if (scaledFocus != 1f) yieldStats[stat] *= scaledFocus
+        if (!city.civ.isHuman()) { // Don't mess things up with a single turn of Autoplay
+            for (stat in Stat.entries) {
+                val scaledFocus = civPersonality.scaledFocus(PersonalityValue[stat])
+                if (scaledFocus != 1f) yieldStats[stat] *= scaledFocus
+            }
         }
 
         // Apply City focus
