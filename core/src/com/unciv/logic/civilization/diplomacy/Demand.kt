1ff10700b9a65de388949cb9c911fb9e0c59ed73
yairm210 <yairm210@hotmail.com>
2025-06-21 23:27:02 +0300

    chore: Generic'd demand alerts

--- core/src/com/unciv/logic/civilization/diplomacy/Demand.kt
+++ core/src/com/unciv/logic/civilization/diplomacy/Demand.kt
@@ -3,7 +3,7 @@ package com.unciv.logic.civilization.diplomacy
 import com.unciv.logic.civilization.AlertType
 
 /** After creating the required flags, modifiers, and alert type, the only remaining work should be
- * - Adding the new alerts in AlertPopup.kt
+ * - Adding the new alerts in the when() of AlertPopup.kt
  * - Triggering the violation (adding the violationOccurred flag) from somewhere in the code
  */
 
@@ -18,6 +18,9 @@ enum class Demand(
     val demandAlert: AlertType,
     val violationDiscoveredAlert: AlertType,
     val demandText: String,
+    val acceptDemandText: String,
+    val refuseDemandText: String,
+    val violationNoticedText: String,
     /** Must contain 1 parameter, to be replaced with civ name */
     val agreedToDemandText: String,
     /** Must contain 1 parameter, to be replaced with civ name */
@@ -36,8 +39,11 @@ enum class Demand(
         demandAlert = AlertType.DemandToStopSpyingOnUs,
         violationDiscoveredAlert = AlertType.SpyingOnUsDespiteOurPromise,
         demandText = "Stop spying on us.",
+        acceptDemandText = "We see our people are not welcome in your lands... we will take our attention elsewhere.",
+        refuseDemandText = "I'll do what's necessary for my empire to survive.",
         agreedToDemandText = "[civName] agreed to stop spying on us!",
         refusedDemandText = "[civName] refused to stop spying on us!",
+        violationNoticedText = "Take back your spy and your broken promises.",
         wePromisedText = "We promised not to send spies to them ([turns] turns remaining)",
         theyPromisedText = "They promised not to send spies to us ([turns] turns remaining)"
     ),
@@ -51,8 +57,11 @@ enum class Demand(
         demandAlert = AlertType.DemandToStopSpreadingReligion,
         violationDiscoveredAlert = AlertType.ReligionSpreadDespiteOurPromise,
         demandText = "Please don't spread your religion to us.",
+        acceptDemandText = "Very well, we shall spread our faith elsewhere.",
+        refuseDemandText = "We shall do as we please.",
         agreedToDemandText = "[civName] agreed to stop spreading religion to us!",
         refusedDemandText = "[civName] refused to stop spreading religion to us!",
+        violationNoticedText = "We noticed you have continued spreading your faith, despite your promise. This will have....consequences.",
         wePromisedText = "We promised not to spread religion to them ([turns] turns remaining)",
         theyPromisedText = "They promised not to spread religion to us ([turns] turns remaining)",
     ),
@@ -66,8 +75,11 @@ enum class Demand(
         demandAlert = AlertType.DemandToStopSettlingCitiesNear,
         violationDiscoveredAlert = AlertType.CitySettledNearOtherCivDespiteOurPromise,
         demandText = "Please don't settle new cities near us.",
+        acceptDemandText = "Very well, we shall look for new lands to settle.",
+        refuseDemandText = "We shall do as we please.",
         agreedToDemandText = "[civName] agreed to stop settling cities near us!",
         refusedDemandText = "[civName] refused to stop settling cities near us!",
+        violationNoticedText = "We noticed your new city near our borders, despite your promise. This will have....implications.",
         wePromisedText = "We promised not to settle near them ([turns] turns remaining)",
         theyPromisedText = "They promised not to settle near us ([turns] turns remaining)"
     )
