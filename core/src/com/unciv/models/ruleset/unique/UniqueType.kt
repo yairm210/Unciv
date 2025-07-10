c1dc8f1e07b9897d6b12c9d1437566112e22de4b
Rob Loach <RobLoach@users.noreply.github.com>
2025-07-09 20:28:17 -0400

    Apply suggestion from @RobLoach

--- core/src/com/unciv/models/ruleset/unique/UniqueType.kt
+++ core/src/com/unciv/models/ruleset/unique/UniqueType.kt
@@ -377,7 +377,7 @@ enum class UniqueType(
     PreventSpreadingReligion("Prevents spreading of religion to the city it is next to", UniqueTarget.Unit),
     RemoveOtherReligions("Removes other religions when spreading religion", UniqueTarget.Unit),
 
-    @Deprecated("As of 4.17.3", ReplaceWith("May Paradrop to [Land] up to [positiveAmount] tiles away <in [{Friendly} {Land}] tiles>"), DeprecationLevel.WARNING)
+    @Deprecated("As of 4.17.4", ReplaceWith("May Paradrop to [Land] up to [positiveAmount] tiles away <in [{Friendly} {Land}] tiles>"), DeprecationLevel.WARNING)
     MayParadropOld("May Paradrop up to [positiveAmount] tiles from inside friendly territory", UniqueTarget.Unit),
     MayParadrop("May Paradrop to [tileFilter] up to [positiveAmount] tiles away", UniqueTarget.Unit),
     CanAirsweep("Can perform Air Sweep", UniqueTarget.Unit),
