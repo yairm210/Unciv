c343764426b9f7036b408929146c346986c9ba10
yairm210 <yairm210@users.noreply.github.com>
2025-07-11 12:58:01 +0000

    Bump version and create initial changelog entry

--- changelog.md
+++ changelog.md
@@ -1,3 +1,17 @@
+## 4.17.4
+
+Readonly round 2
+
+Added Purity for readonly validation  
+
+Autogenerate demand texts from Demand enum - 
+
+AI: swap spaceship parts into capital - By EmperorPinguin
+
+Allow proposing peace between warring civs in trade window - By metablaster
+
+Unique: `May Paradrop to [tileFilter] tiles up to [positiveAmount] tiles away` - By RobLoach
+
 ## 4.17.3
 
 Added global tiles countable  - By PhiRite
--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -487,7 +487,7 @@ open class UncivGame(val isConsoleMode: Boolean = false) : Game(), PlatformSpeci
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.17.3", 1141)
+        val VERSION = Version("4.17.4", 1142)
         //endregion
 
         /** Global reference to the one Gdx.Game instance created by the platform launchers - do not use without checking [isCurrentInitialized] first. */
