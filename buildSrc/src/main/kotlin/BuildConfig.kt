bffde2eaeedcf979115e2697ff436c6f71932c20
Yair Morgenstern <yairm210@hotmail.com>
2025-07-11 16:46:23 +0300

    Version rollout (#13601)
    * Bump version and create initial changelog entry
    
    * Update French.properties (#13596)
    
    * Update German.properties (#13593)
    
    * Update German.properties
    
    * Added new translations
    
    * corrected placeholder
    
    * url query
    
    * Update German.properties
    
    * Corrected translations for "Great Person" in German.properties
    
    * update croatian translation (#13592)
    
    * Update Brazilian_Portuguese.properties (#13590)
    
    * Update Indonesian.properties (#13589)
    
    * Update Italian.properties (#13588)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: Felix Kröhnert <70710128+cy-elec@users.noreply.github.com>
    Co-authored-by: metablaster <44481081+metablaster@users.noreply.github.com>
    Co-authored-by: Vitor Gabriel <59321138+Ranbut@users.noreply.github.com>
    Co-authored-by: X2003 <158255876+Xenia2003@users.noreply.github.com>
    Co-authored-by: Giuseppe D'Addio <41149920+Smashfanful@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,8 +4,8 @@ package com.unciv.build
 object BuildConfig {
     const val kotlinVersion = "1.9.24"
     const val appName = "Unciv"
-    const val appCodeNumber = 1141
-    const val appVersion = "4.17.3"
+    const val appCodeNumber = 1142
+    const val appVersion = "4.17.4"
 
     const val gdxVersion = "1.13.1"
     const val ktorVersion = "2.3.12"
