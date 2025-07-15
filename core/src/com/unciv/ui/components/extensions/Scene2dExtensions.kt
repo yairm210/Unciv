a9faab0e7178290c323d6d1cfbf88b434b5aac72
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2025-07-15 17:01:45 +0600

    Update Scene2dExtensions.kt

--- core/src/com/unciv/ui/components/extensions/Scene2dExtensions.kt
+++ core/src/com/unciv/ui/components/extensions/Scene2dExtensions.kt
@@ -93,12 +93,20 @@ fun colorFromHex(hexColor: Int): Color {
 fun colorFromRGB(r: Int, g: Int, b: Int) = Color(r / 255f, g / 255f, b / 255f, 1f)
 /** Create a new [Color] instance from r/g/b given as Integers in the range 0..255 in the form of a 3-element List [rgb] */
 fun colorFromRGB(rgb: List<Int>) = colorFromRGB(rgb[0], rgb[1], rgb[2])
-/** Linearly interpolates between this [Color] and [BLACK][ImageGetter.CHARCOAL] by [t] which is in the range [[0,1]].
- * The result is returned as a new instance. */
-fun Color.darken(t: Float): Color = Color(this).lerp(Color.BLACK, t)
+/** Linearly interpolates between this [Color] and [BLACK][ImageGetter.CHARCOAL] by [t] which is in the range [[0,1]],
+ * preserving color ratio in RGB. The result is returned as a new instance. */
+fun Color.darken(t: Float): Color = Color(this).mul(t)
 /** Linearly interpolates between this [Color] and [WHITE][Color.WHITE] by [t] which is in the range [[0,1]].
  * The result is returned as a new instance. */
 fun Color.brighten(t: Float): Color = Color(this).lerp(Color.WHITE, t)
+
+/** Increases the lightness value of given color by [t] which is in the range [[0,1]] preserving color ration in RGBAc
+ * where 0 is current lightness value and 1 is the maximum possible. The result is returned as a new instance. */
+fun Color.lighten(t: Float): Color = Color(this).let {
+    val lightness = max(r, max(g, b))
+    val targetRatio = (lightness + t * (1 - lightness)) / lightness
+    return it.mul(targetRatio)
+}
 
 
 fun Actor.centerX(parent: Actor) { x = parent.width / 2 - width / 2 }
