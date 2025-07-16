3ddb5cfb15c2572e3dc988d267733793fc3508b2
Md. Touhidur Rahman <touhidurrr@pm.me>
2025-07-16 20:30:32 +0600

    better `Color.brighten()` and `Color.darken()` but this time fixed!

--- core/src/com/unciv/ui/components/extensions/Scene2dExtensions.kt
+++ core/src/com/unciv/ui/components/extensions/Scene2dExtensions.kt
@@ -91,14 +91,21 @@ fun colorFromHex(hexColor: Int): Color {
 
 /** Create a new [Color] instance from [r]/[g]/[b] given as Integers in the range 0..255 */
 fun colorFromRGB(r: Int, g: Int, b: Int) = Color(r / 255f, g / 255f, b / 255f, 1f)
+
 /** Create a new [Color] instance from r/g/b given as Integers in the range 0..255 in the form of a 3-element List [rgb] */
 fun colorFromRGB(rgb: List<Int>) = colorFromRGB(rgb[0], rgb[1], rgb[2])
-/** Linearly interpolates between this [Color] and [BLACK][ImageGetter.CHARCOAL] by [t] which is in the range [[0,1]].
- * The result is returned as a new instance. */
-fun Color.darken(t: Float): Color = Color(this).lerp(Color.BLACK, t)
-/** Linearly interpolates between this [Color] and [WHITE][Color.WHITE] by [t] which is in the range [[0,1]].
- * The result is returned as a new instance. */
-fun Color.brighten(t: Float): Color = Color(this).lerp(Color.WHITE, t)
+
+/** Linearly interpolates between this [Color] and [BLACK][ImageGetter.CHARCOAL] by [t] which is in the range [[0,1]],
+ * preserving color ratio in RGB. The result is returned as a new instance. */
+fun Color.darken(t: Float): Color = Color(this).mul(1 - t)
+
+/** Linearly interpolates between this [Color] and [WHITE][Color.WHITE] by [t] which is in the range [[0,1]],
+ * preserving color ratio in RGB. The result is returned as a new instance. */
+fun Color.brighten(t: Float): Color = Color(this).let {
+    val lightness = maxOf(r, g, b)
+    val targetRatio = (lightness + t * (1 - lightness)) / lightness
+    return it.mul(targetRatio)
+}
 
 
 fun Actor.centerX(parent: Actor) { x = parent.width / 2 - width / 2 }
