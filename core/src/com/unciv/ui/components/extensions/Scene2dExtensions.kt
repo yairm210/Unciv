c3fc281a9db6138c742cd650a712e9add26e783d
Md. Touhidur Rahman <touhidurrr@pm.me>
2025-07-17 11:48:14 +0600

    revert comment

--- core/src/com/unciv/ui/components/extensions/Scene2dExtensions.kt
+++ core/src/com/unciv/ui/components/extensions/Scene2dExtensions.kt
@@ -95,8 +95,8 @@ fun colorFromRGB(r: Int, g: Int, b: Int) = Color(r / 255f, g / 255f, b / 255f, 1
 /** Create a new [Color] instance from r/g/b given as Integers in the range 0..255 in the form of a 3-element List [rgb] */
 fun colorFromRGB(rgb: List<Int>) = colorFromRGB(rgb[0], rgb[1], rgb[2])
 
-/** Linearly interpolates between this [Color] and [BLACK][ImageGetter.CHARCOAL] by [t] which is in the range [[0,1]],
- * preserving color ratio in RGB. The result is returned as a new instance. */
+/** Linearly interpolates between this [Color] and [BLACK][ImageGetter.CHARCOAL] by [t] which is in the range [[0,1]].
+ * The result is returned as a new instance. */
 fun Color.darken(t: Float): Color = Color(this).lerp(Color.BLACK, t)
 
 /** Linearly interpolates between this [Color] and [WHITE][Color.WHITE] by [t] which is in the range [[0,1]],
