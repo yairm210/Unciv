bbf93946c2a296705a3494dab3073cb4f68b0f7a
Rob Loach <robloach@gmail.com>
2025-07-20 18:43:52 -0400

    Update docs

--- core/src/com/unciv/ui/components/input/CursorHoverInputListener.kt
+++ core/src/com/unciv/ui/components/input/CursorHoverInputListener.kt
@@ -6,10 +6,13 @@ import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
 
+/**
+ * Provides an input listener that will change the cursor when the element is hovered.
+ */
 class CursorHoverInputListener(
-        private val hoverCursor: Cursor.SystemCursor = Cursor.SystemCursor.Hand,
-        private val exitCursor: Cursor.SystemCursor = Cursor.SystemCursor.Arrow
-    ) : InputListener() {
+    private val hoverCursor: Cursor.SystemCursor = Cursor.SystemCursor.Hand,
+    private val exitCursor: Cursor.SystemCursor = Cursor.SystemCursor.Arrow
+) : InputListener() {
     override fun enter(event: InputEvent?, x: Float, y: Float, pointer: Int, fromActor: Actor?) {
         Gdx.graphics.setSystemCursor(hoverCursor)
     }
