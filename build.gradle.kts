11de0ae4a33e132f6dfa810ab1e766dfa42a78c0
yairm210 <yairm210@hotmail.com>
2025-07-23 00:05:28 +0300

    chore(purity): MultiFilter

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ plugins {
     // This is *with* gradle 8.2 downloaded according the project specs, no idea what that's about
     kotlin("multiplatform") version "1.9.24"
     kotlin("plugin.serialization") version "1.9.24"
-    id("io.github.yairm210.purity-plugin") version "0.0.33" apply(false)
+    id("io.github.yairm210.purity-plugin") version "0.0.34" apply(false)
 }
 
 allprojects {
