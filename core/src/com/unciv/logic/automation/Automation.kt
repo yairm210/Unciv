e5be79b0e877547f2b682e74b9e10f0cd13e308b
EmperorPinguin <99119424+EmperorPinguin@users.noreply.github.com>
2025-07-31 22:23:39 +0200

    Update Automation.kt

--- core/src/com/unciv/logic/automation/Automation.kt
+++ core/src/com/unciv/logic/automation/Automation.kt
@@ -84,7 +84,7 @@ object Automation {
             for (unique in localUniqueCache.forCityGetMatchingUniques(city, UniqueType.UnhappinessFromPopulationTypePercentageChange))
                 if (unique.params[1] == "Specialists" && city.matchesFilter(unique.params[2]))
                     yieldStats.happiness -= (unique.params[0].toFloat() / 100f)  // relative val is negative, make positive
-            if (yieldStats.science == 3f || yieldStats.science >= 5f )
+           if (!city.civ.isHuman() && (yieldStats.science == 3f || yieldStats.science >= 5f ))
                 yieldStats.science *= 2f
         }
 
