b7206764cfe5195e1d75d0ebb15e6aafa55a5307
Md. Touhidur Rahman <touhidurrr@pm.me>
2025-08-09 02:04:17 +0600

    check if gradle and agp updates fixes the encountered issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${com.unciv.build.BuildConfig.kotlinVersion}")
-        classpath("com.android.tools.build:gradle:8.9.1")
+        classpath("com.android.tools.build:gradle:8.12.0")
     }
 }
 
