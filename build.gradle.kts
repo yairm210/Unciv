54a790b86d123de4720c70d271573a387c7aaa65
Md. Touhidur Rahman <touhidurrr@pm.me>
2025-08-09 02:45:56 +0600

    dowgrade agp to `8.10.0`

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${com.unciv.build.BuildConfig.kotlinVersion}")
-        classpath("com.android.tools.build:gradle:8.11.0")
+        classpath("com.android.tools.build:gradle:8.10.0")
     }
 }
 
