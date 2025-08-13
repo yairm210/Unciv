1280b8c46db2d51ad205d906fbee691aabf3029d
Rob Loach <robloach@gmail.com>
2025-08-13 11:43:23 -0400

    Update

--- core/src/com/unciv/models/ruleset/unique/Countables.kt
+++ core/src/com/unciv/models/ruleset/unique/Countables.kt
@@ -84,10 +84,8 @@ enum class Countables(
     },
 
     StatPerTurn("[stat] Per Turn", shortDocumentation = "The amount of a stat gained per turn") {
-        override val documentationHeader = "Stat name (${Stat.names().niceJoin()}) Per Turn"
-        override val documentationStrings = listOf(
-            "Gets the amount of a stat gained per turn."
-        )
+        override val documentationHeader = "Stat name Per Turn (${Stat.names().niceJoin()})"
+        override val documentationStrings = listOf("Gets the amount of a stat the civilization gains per turn")
         override fun matches(parameterText: String) = Stat.isStat(parameterText.getPlaceholderParameters().firstOrNull() ?: "")
         override fun eval(parameterText: String, gameContext: GameContext): Int? {
             val statName = parameterText.getPlaceholderParameters().firstOrNull() ?: return null
@@ -96,9 +94,7 @@ enum class Countables(
             val nextTurnStats = civ.stats.getStatMapForNextTurn()
             return nextTurnStats.values.map { it[relevantStat] }.sum().toInt()
         }
-        override val example: String = "[Culture] Per Turn"
         override fun getKnownValuesForAutocomplete(ruleset: Ruleset) = Stat.names().map { "[$it] Per Turn" }.toSet()
-
         private fun Iterable<String>.niceJoin() = joinToString("`, `", "`", "`").run {
             val index = lastIndexOf("`, `")
             substring(0, index) + "` or `" + substring(index + 4)
