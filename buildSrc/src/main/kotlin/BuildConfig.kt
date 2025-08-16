7a46de4fb37f030cf420fbda9f4c6bdb50909d5b
Md. Touhidur Rahman <touhidurrr@pm.me>
2025-08-16 20:11:42 +0600

    move dependency versions to `gradle.properties`

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -2,15 +2,8 @@
 package com.unciv.build
 
 object BuildConfig {
-    const val kotlinVersion = "2.1.20"
     const val appName = "Unciv"
     const val appCodeNumber = 1155
     const val appVersion = "4.17.14"
-
-    const val gdxVersion = "1.13.1"
-    const val ktorVersion = "3.2.3"
-    const val coroutinesVersion = "1.8.1"
-    const val jnaVersion = "5.17.0"
-
     const val identifier = "com.unciv.app"
 }
