b91ff9d6122668e1c45bb143cbcef1932703ba36
Md. Touhidur Rahman <touhidurrr@pm.me>
2025-09-29 03:02:28 +0600

    revert `desktop/build.gradle.kts` changes

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -17,10 +17,9 @@ kotlin {
     compilerOptions {
         jvmTarget = JvmTarget.JVM_21
     }
-    java {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
+}
+java {
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 val mainClassName = "com.unciv.app.desktop.DesktopLauncher"
