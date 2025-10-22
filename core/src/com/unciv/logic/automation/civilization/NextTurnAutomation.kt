7c32e694ce07bdda27a3e5699b377975bcc89f3c
Emandac <121508218+Emandac@users.noreply.github.com>
2025-10-22 12:53:47 +0200

    Last changes before splitting the PR into 2
    One to allow to player to settle on Water/Mountain and other to allow the ai to do the same.

--- core/src/com/unciv/logic/automation/civilization/NextTurnAutomation.kt
+++ core/src/com/unciv/logic/automation/civilization/NextTurnAutomation.kt
@@ -514,7 +514,7 @@ object NextTurnAutomation {
 
         // This is a tough one - if we don't ignore conditionals we could have units that can found only on certain tiles that are ignored
         // If we DO ignore conditionals we could get a unit that can only found if there's a certain tech, or something
-        if (civInfo.units.getCivUnits().any { it.hasUnique(UniqueType.FoundCity, GameContext.IgnoreConditionals)}) return
+        if (civInfo.units.getCivUnits().any { it.hasUnique(UniqueType.FoundCity, GameContext.IgnoreConditionals) }) return
         if (civInfo.cities.any {
                 val currentConstruction = it.cityConstructions.getCurrentConstruction()
                 currentConstruction is BaseUnit && currentConstruction.isCityFounder()
