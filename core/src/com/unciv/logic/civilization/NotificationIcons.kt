a6038c11ccc1f584c0b23d8ba798a7b0e18a816a
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2025-10-30 18:56:04 +0100

    Various code readability improvements around Notifications (#14098)
    * Various code readability improvements around Notifications
    
    * Moved code lost in merge
    
    * Oops forgot to add the additions to the doc

--- core/src/com/unciv/logic/civilization/NotificationIcons.kt
+++ core/src/com/unciv/logic/civilization/NotificationIcons.kt
@@ -1,5 +1,13 @@
 package com.unciv.logic.civilization
 
+import com.unciv.models.ruleset.Ruleset
+import com.unciv.ui.images.ImageGetter
+
+@Suppress("ConstPropertyName") // We want enum-like names
+/**
+ *  For use in the `notificationIcons` parameter of [addNotification][Civilization.addNotification],
+ *  which accepts texture paths or some ruleset object names (nation, tech, unit) - this will be translated through [getImage].
+ */
 object NotificationIcon {
     // Remember: The typical white-on-transparency icon will not be visible on Notifications
 
@@ -25,4 +33,27 @@ object NotificationIcon {
     const val Spy = "OtherIcons/Spy"
     const val Trade = "StatIcons/Acquire"
     const val War = "OtherIcons/Pillage"
+
+    /** Get the image for a [Notification] icon, translating:
+     *  - A [Baseunit][com.unciv.models.ruleset.unit.BaseUnit] name to [ImageGetter.getUnitIcon]
+     *  - A [Nation][com.unciv.models.ruleset.nation.Nation] name to [ImageGetter.getNationPortrait]
+     *  - A [Promotion][com.unciv.models.ruleset.unit.Promotion] name to [ImageGetter.getPromotionPortrait]
+     *  - A [Reource][com.unciv.models.ruleset.tile.TileResource] name to [ImageGetter.getResourcePortrait]
+     *  - A [Technology][com.unciv.models.ruleset.tech.Technology] name to [ImageGetter.getTechIconPortrait]
+     *  - Otherwise, the string must be a direct texture path resolvable by [ImageGetter.getImage]
+     */
+    fun getImage(icon: String, ruleset: Ruleset, iconSize: Float) = when {
+        ruleset.technologies.containsKey(icon) ->
+            ImageGetter.getTechIconPortrait(icon, iconSize)
+        ruleset.nations.containsKey(icon) ->
+            ImageGetter.getNationPortrait(ruleset.nations[icon]!!, iconSize)
+        ruleset.units.containsKey(icon) ->
+            ImageGetter.getUnitIcon(ruleset.units[icon]!!)
+        ruleset.unitPromotions.containsKey(icon) ->
+            ImageGetter.getPromotionPortrait(icon, iconSize)
+        ruleset.tileResources.containsKey(icon) ->
+            ImageGetter.getResourcePortrait(icon, iconSize)
+        else ->
+            ImageGetter.getImage(icon)
+    }
 }
