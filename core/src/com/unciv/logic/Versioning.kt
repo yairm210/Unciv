4c49ac49b11be5b2b96ad087a3a3c3fed80057fd
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2025-11-08 21:46:17 +0100

    Revert Comparable<Version> implementation

--- core/src/com/unciv/logic/Versioning.kt
+++ core/src/com/unciv/logic/Versioning.kt
@@ -9,9 +9,7 @@ import yairm210.purity.annotations.Readonly
  *  Wrapper for a release version.
  *
  *  - An instance holding current values is created as [UncivGame.VERSION] via a workflow editing source.
- *  - Supports >, <, == comparisons by comparing the [number] field only.
- *  - Formatters: [toNiceString] or [toSerializeString] - localized and non-localized.
- *  - The non-localized format can be parsed back using [parse].
+ *  - Formatter: [toNiceString] - localized
  *
  *  @property text corresponds to BuildConfig.appVersion
  *  @property number corresponds to BuildConfig.appCodeNumber
@@ -19,29 +17,12 @@ import yairm210.purity.annotations.Readonly
 data class Version(
     val text: String,
     val number: Int
-) : Comparable<Version>, IsPartOfGameInfoSerialization {
+) : IsPartOfGameInfoSerialization {
     @Suppress("unused") // used by json serialization
     internal constructor() : this("", -1)
 
     @Pure fun toNiceString() = "[$text] (Build [$number])".tr()
     @Pure fun toSerializeString() = "$text (Build $number)"
-
-    override operator fun compareTo(other: Version) = number.compareTo(other.number)
-    override fun equals(other: Any?) = other is Version && number == other.number
-    override fun hashCode() = number
-
-    companion object {
-        private val parseVersionRegex = Regex("""^(.*) \(Build ([0-9,]+)\)$""")
-
-        /** This can parse e.g. "createdWithVersion":"4.18.11 (Build 1173)" as set by MapGenerator
-         *  with limited backwards compatibility for thousands-separator commas */
-        @Readonly
-        fun parse(s: String): Version {
-            val match = parseVersionRegex.matchEntire(s)
-                ?: throw IllegalArgumentException("\"$s\" does not fit the version (Build number) pattern")
-            return Version(match.groupValues[1], match.groupValues[2].replace(",","").toInt())
-        }
-    }
 }
 
 interface HasGameInfoSerializationVersion {
