69ac9286720bacf04bf8fe9aa747c3c94fba52a4
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2025-11-20 12:38:28 +0100

    chore: Minor linting I happened to come across (#14244)
    * chore: Minor linting I happened to come across
    
    * UnitIconGroup linting

--- core/src/com/unciv/models/ImmutableColor.kt
+++ core/src/com/unciv/models/ImmutableColor.kt
@@ -2,14 +2,18 @@ package com.unciv.models
 
 import com.badlogic.gdx.graphics.Color
 
-class ImmutableColor(color: Color): Color(color.r, color.g, color.b, color.a) {
+class ImmutableColor(color: Color) : Color(color.r, color.g, color.b, color.a) {
     private fun throwModificationError(): Nothing =
         throw UnsupportedOperationException("ImmutableColor cannot be modified - use .cpy() first to create a mutable copy")
+    override fun set(color: Color?): Color? = throwModificationError()
+    override fun set(r: Float, g: Float, b: Float, a: Float): Color? = throwModificationError()
+    override fun set(rgba: Int): Color? = throwModificationError()
     override fun add(color: Color?): Color = throwModificationError()
     override fun mul(color: Color?): Color = throwModificationError()
     override fun mul(r: Float, g: Float, b: Float, a: Float): Color = throwModificationError()
+    override fun mul(value: Float): Color? = throwModificationError()
+    override fun sub(color: Color?): Color = throwModificationError()
     override fun premultiplyAlpha(): Color = throwModificationError()
-    override fun sub(color: Color?): Color = throwModificationError()   
     override fun lerp(target: Color?, t: Float): Color = throwModificationError()
     override fun lerp(r: Float, g: Float, b: Float, a: Float, t: Float): Color = throwModificationError()
 }
