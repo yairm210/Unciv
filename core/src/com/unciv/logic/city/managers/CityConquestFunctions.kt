028505d5c5023840cf88c95507631e448b73988b
PLynx <pawelrys2011@gmail.com>
2025-11-20 15:32:08 +0100

    Add the GoldFromCapturingCity unique

--- core/src/com/unciv/logic/city/managers/CityConquestFunctions.kt
+++ core/src/com/unciv/logic/city/managers/CityConquestFunctions.kt
@@ -34,14 +34,21 @@ class CityConquestFunctions(val city: City) {
         val baseGold = 20 + 10 * city.population.population + tileBasedRandom.nextInt(40)
         val turnModifier = max(0, min(50, city.civ.gameInfo.turns - city.turnAcquired)) / 50f
         val cityModifier = if (city.containsBuildingUnique(UniqueType.DoublesGoldFromCapturingCity)) 2f else 1f
-        val conqueringCivModifier = if (conqueringCiv.hasUnique(UniqueType.TripleGoldFromEncampmentsAndCities)) 3f else 1f
+        
+        var cityModifierNew = 1f
         var conqueringCivModifierNew = 1f
+        
+        for (unique in city.getMatchingUniques(UniqueType.GoldFromCapturingCity, city.state)) {
+            cityModifierNew *= unique.params[0].toPercent()
+        }
+        
+        val conqueringCivModifier = if (conqueringCiv.hasUnique(UniqueType.TripleGoldFromEncampmentsAndCities)) 3f else 1f
         
         for (unique in conqueringCiv.getMatchingUniques(UniqueType.GoldFromEncampmentsAndCities, conqueringCiv.state)) {
             conqueringCivModifierNew *= unique.params[0].toPercent()
         }
         
-        val goldPlundered = baseGold * turnModifier * cityModifier * conqueringCivModifier * conqueringCivModifierNew
+        val goldPlundered = baseGold * turnModifier * cityModifier * conqueringCivModifier * conqueringCivModifierNew * cityModifierNew
         return goldPlundered.toInt()
     }
 
--- core/src/com/unciv/models/ruleset/unique/UniqueType.kt
+++ core/src/com/unciv/models/ruleset/unique/UniqueType.kt
@@ -345,6 +345,8 @@ enum class UniqueType(
 
     DestroyedWhenCityCaptured("Destroyed when the city is captured", UniqueTarget.Building),
     NotDestroyedWhenCityCaptured("Never destroyed when the city is captured", UniqueTarget.Building),
+    GoldFromCapturingCity("[relativeAmount]% Gold given to enemy if city is captured", UniqueTarget.Building),
+    @Deprecated("As of 4.18.15", ReplaceWith("[+100]% Gold given to enemy if city is captured"), DeprecationLevel.WARNING)
     DoublesGoldFromCapturingCity("Doubles Gold given to enemy if city is captured", UniqueTarget.Building),
 
 
