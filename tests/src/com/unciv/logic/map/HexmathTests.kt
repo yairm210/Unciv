9a6982aea2c5827e7d8662acb55f40ccb91e06eb
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2025-12-02 00:17:11 +0100

    Better HexCoord serialization and linting

--- tests/src/com/unciv/logic/map/HexmathTests.kt
+++ tests/src/com/unciv/logic/map/HexmathTests.kt
@@ -7,17 +7,18 @@ import com.unciv.models.ruleset.tile.Terrain
 import com.unciv.models.ruleset.tile.TerrainType
 import org.junit.Assert
 import org.junit.Test
+import kotlin.random.Random
 
 
 class HexmathTests {
     // Looks like our current movement is actually unoptimized, since it fails this test :)
     @Test
-    fun zeroIndexed(){
+    fun zeroIndexed() {
         Assert.assertEquals(0, HexMath.getZeroBasedIndex(0,0))
     }
 
     @Test
-    fun testMappingIsOneToOne(){
+    fun testMappingIsOneToOne() {
         val seenCoordsMapping = hashSetOf<Int>()
         for (ring in 1..100) {
             val coords = HexMath.getHexCoordsAtDistance(HexCoord.Zero, ring, 100, false)
@@ -31,9 +32,9 @@ class HexmathTests {
             }
         }
     }
-    
+
     @Test
-    fun testTileNeighborMappingIsNonConflicting(){
+    fun testTileNeighborMappingIsNonConflicting() {
         val ruleset = Ruleset()
         ruleset.terrains["Plains"] = Terrain().apply {
             name = "Plains"
@@ -41,7 +42,7 @@ class HexmathTests {
         }
         val tileMap = TileMap(100, ruleset, false)
         val seenCoordsMapping = hashSetOf<Int>()
-        
+
         for (tile in tileMap.values){
             for (neighbor in tile.neighbors){
                 val index = HexMath.tilesAndNeighborUniqueIndex(tile, neighbor)
@@ -51,9 +52,9 @@ class HexmathTests {
             }
         }
     }
-    
+
     @Test
-    fun testHexCoordConversions(){
+    fun testHexCoordConversions() {
         for (x in -2..2)
             for (y in -2..2){
                 val hexCoord = HexCoord.of(x, y)
@@ -62,21 +63,20 @@ class HexmathTests {
             }
     }
 
-    
     @Test
-    fun testHexCoordVector2SerDeser(){
+    fun testHexCoordVector2SerDeser() {
         val json = json()
-        
+
         for (x in -2..2)
-            for (y in -2..2){
+            for (y in -2..2) {
                 val hexCoord = HexCoord(x, y)
                 val vector2 = Vector2(x.toFloat(), y.toFloat())
-                
+
                 // hexcoord -> json -> vector2
                 val hexCoordSerialized = json.toJson(hexCoord)
                 val vector2Deserialized = json.fromJson(Vector2::class.java, hexCoordSerialized)
                 Assert.assertEquals(vector2, vector2Deserialized)
-                
+
                 // vector2 -> json -> hexcoord
                 val vector2Serialized = json.toJson(vector2)
                 val hexCoordDeserialized = json.fromJson(HexCoord::class.java, vector2Serialized)
@@ -84,4 +84,19 @@ class HexmathTests {
             }
     }
 
+    @Test
+    /** Prove that Vector2 with fractions serialized then deserialized to a HexCoord is the same as direct conversion (truncating vs 0) */
+    fun testFractionalVector2DeserializedAsHexCoord() {
+        val json = json()
+        val rnd = Random(42)
+        fun rndCoord() = (rnd.nextFloat() - 0.5f) * 42f
+
+        repeat(42) {
+            val vector = Vector2(rndCoord(), rndCoord())
+            val serialized = json.toJson(vector)
+            val actual = json.fromJson(HexCoord::class.java, serialized)
+            val expected = vector.toHexCoord()
+            Assert.assertEquals(expected, actual)
+        }
+    }
 }
