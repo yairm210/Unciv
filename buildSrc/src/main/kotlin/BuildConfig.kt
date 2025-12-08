8227422cdad905fc6cb7e3f31ca9745e2b04162f
Yair Morgenstern <yairm210@hotmail.com>
2025-12-08 09:58:19 +0200

    Version rollout (#14324)
    * Bump version and create initial changelog entry
    
    * Update Catalan.properties (#14322)
    
    * Update French.properties (#14308)
    
    * Update Italian.properties (#14298)
    
    * Update Russian translations in properties file (#14290)
    
    * Update Russian translations in properties file
    
    * Fix translation for relativeAmount Strength in Russian
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Joan Josep <juanjo.ng.83@gmail.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: Giuseppe D'Addio <41149920+Smashfanful@users.noreply.github.com>
    Co-authored-by: mrmazay <4014044+mrmazay@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,7 +3,7 @@ package com.unciv.build
 
 object BuildConfig {
     const val appName = "Unciv"
-    const val appCodeNumber = 1183
-    const val appVersion = "4.18.18-patch1"
+    const val appCodeNumber = 1184
+    const val appVersion = "4.18.19"
     const val identifier = "com.unciv.app"
 }
