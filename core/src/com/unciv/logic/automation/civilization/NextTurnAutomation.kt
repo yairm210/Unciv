a4a123df4ecc3b6c79441f105079e4d1a9eb9ca7
EmperorPinguin <99119424+EmperorPinguin@users.noreply.github.com>
2025-12-24 10:02:39 +0100

    AI: build settlers also when at war (#14413)
    * Modify isLateGame()
    
    * Add late game check to trainSettler function
    
    * Add import for CivilianUnitAutomation

--- core/src/com/unciv/logic/automation/civilization/NextTurnAutomation.kt
+++ core/src/com/unciv/logic/automation/civilization/NextTurnAutomation.kt
@@ -3,6 +3,7 @@ package com.unciv.logic.automation.civilization
 import com.unciv.UncivGame
 import com.unciv.logic.automation.Automation
 import com.unciv.logic.automation.ThreatLevel
+import com.unciv.logic.automation.unit.CivilianUnitAutomation
 import com.unciv.logic.automation.unit.EspionageAutomation
 import com.unciv.logic.automation.unit.UnitAutomation
 import com.unciv.logic.battle.*
@@ -538,9 +539,9 @@ object NextTurnAutomation {
         val personality = civInfo.getPersonality()
         if (civInfo.isCityState) return
         if (civInfo.isOneCityChallenger()) return
-        if (civInfo.isAtWar()) return // don't train settlers when you could be training troops.
         if (civInfo.cities.none()) return
         if (civInfo.getHappiness() <= civInfo.cities.size) return
+        if (CivilianUnitAutomation.isLateGame(civInfo)) return // all suitable land should be occupied already; there will only be a risk of the settler getting bombed
 
         // This is a tough one - if we don't ignore conditionals we could have units that can found only on certain tiles that are ignored
         // If we DO ignore conditionals we could get a unit that can only found if there's a certain tech, or something
