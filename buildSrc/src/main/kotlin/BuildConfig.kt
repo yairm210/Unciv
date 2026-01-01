061f20b3851e8f7a2d756724cd5c6e66df8f4a06
Yair Morgenstern <yairm210@hotmail.com>
2026-01-02 00:12:06 +0200

    Version rollout (#14454)
    * Bump version and create initial changelog entry
    
    * Update German.properties (#14453)
    
    Filled in some missing translations and did one change to an existing one.
    
    * FINISHED Maltese.properties (#14451)
    
    * FINISHED Maltese.properties
    
    * some small changes
    
    * forgot this
    
    * Update Maltese.properties
    
    * update vietnamese translation 16 (#14450)
    
    guh im back from the big final test phew
    - add new updated stuffs ( as usual )
    - i passed all of them so yes 
    - im returning to translate this again ( yee )
    
    * Update French.properties (#14445)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: PCPandaStyle <81259139+PCPandaStyle@users.noreply.github.com>
    Co-authored-by: Jioido <j9725206@gmail.com>
    Co-authored-by: theperfectoast1337 <duynamdo000@gmail.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,7 +3,7 @@ package com.unciv.build
 
 object BuildConfig {
     const val appName = "Unciv"
-    const val appCodeNumber = 1191
-    const val appVersion = "4.19.4-patch1"
+    const val appCodeNumber = 1192
+    const val appVersion = "4.19.5"
     const val identifier = "com.unciv.app"
 }
