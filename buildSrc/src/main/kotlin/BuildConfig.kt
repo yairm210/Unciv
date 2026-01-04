f8f278dd223d4d91c3e640cc8ea3229960258ea5
Yair Morgenstern <yairm210@hotmail.com>
2026-01-04 21:54:18 +0200

    Version rollout (#14467)
    * Bump version and create initial changelog entry
    
    * Update French.properties (#14460)
    
    * Update Italian.properties (#14459)
    
    * Update German.properties (#14458)
    
    * Update German.properties
    
    revised commit, compared with an older version of the file.
    
    * add missing "requires translation" in the comments at the top
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: Giuseppe D'Addio <41149920+Smashfanful@users.noreply.github.com>
    Co-authored-by: PCPandaStyle <81259139+PCPandaStyle@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,7 +3,7 @@ package com.unciv.build
 
 object BuildConfig {
     const val appName = "Unciv"
-    const val appCodeNumber = 1192
-    const val appVersion = "4.19.5"
+    const val appCodeNumber = 1193
+    const val appVersion = "4.19.6"
     const val identifier = "com.unciv.app"
 }
