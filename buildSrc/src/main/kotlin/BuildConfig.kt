ee3ea2cd6dd42c43b2823af50f137129f7e68400
Yair Morgenstern <yairm210@hotmail.com>
2026-01-22 10:44:40 +0200

    Version rollout (#14506)
    * Bump version and create initial changelog entry
    
    * Update Brazilian_Portuguese.properties (#14504)
    
    * Update French.properties (#14502)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Vitor Gabriel <Ranbut_Beater@protonmail.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,7 +3,7 @@ package com.unciv.build
 
 object BuildConfig {
     const val appName = "Unciv"
-    const val appCodeNumber = 1196
-    const val appVersion = "4.19.8"
+    const val appCodeNumber = 1197
+    const val appVersion = "4.19.9"
     const val identifier = "com.unciv.app"
 }
