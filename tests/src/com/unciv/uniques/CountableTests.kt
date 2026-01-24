85190ca67a54849e7d58b7c4d99bfc1f43ecee6d
Rob Loach <robloach@gmail.com>
2026-01-24 14:51:38 -0500

    countables: Add [cityFilter] Cities by [civFilter] Civilizations countable

--- tests/src/com/unciv/uniques/CountableTests.kt
+++ tests/src/com/unciv/uniques/CountableTests.kt
@@ -510,6 +510,29 @@ class CountableTests {
     }
 
     @Test
+    @CoversCountable(Countables.FilteredCitiesByCivs)
+    fun testFilteredCitiesByCivs() {
+        setupModdedGame()
+        val civ2 = game.addCiv()
+        val city2 = game.addCity(civ2, game.tileMap[-2, 0])
+        city2.isPuppet = true
+        val context = GameContext(civ)
+
+        runTestParcours("Filtered cities by civs countable", { test: String ->
+            Countables.getCountableAmount(test, context)
+        },
+            "[Capital] Cities by [All] Civilizations", 2,
+            "[Puppeted] Cities by [All] Civilizations", 1,
+            "[All] Cities by [All] Civilizations", 2,
+            "[Capital] Cities by [${civ.civName}] Civilizations", 1,
+            "[Puppeted] Cities by [${civ.civName}] Civilizations", 0,
+            "[Puppeted] Cities by [${civ2.civName}] Civilizations", 1,
+            "[All] Cities by [${civ2.civName}] Civilizations", 1,
+            "[Capital] Cities by [City-State] Civilizations", 0,
+        )
+    }
+
+    @Test
     @CoversCountable(Countables.RemainingCivs, Countables.FilteredUnits, Countables.Carried)
     fun testFilteredUnitsCountable() {
         val ruleset = setupModdedGame()
