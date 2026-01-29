bf811bf562e52bc50fd3abf1acd04238945b2c23
Yair Morgenstern <yairm210@hotmail.com>
2026-01-29 23:56:40 +0200

    Version rollout (#14518)
    * Bump version and create initial changelog entry
    
    * Update Japanese.properties (#14514)
    
    * Update Japanese.properties
    
    * Translate Great People names
    
    * restore deleted line
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: cknight828 <128895471+cknight828@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,7 +3,7 @@ package com.unciv.build
 
 object BuildConfig {
     const val appName = "Unciv"
-    const val appCodeNumber = 1197
-    const val appVersion = "4.19.9"
+    const val appCodeNumber = 1198
+    const val appVersion = "4.19.10"
     const val identifier = "com.unciv.app"
 }
