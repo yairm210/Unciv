52cb69cd2448123d4fc1311dcec670b49ca4c835
AutumnPizazz <autumnpizazz@users.noreply.github.com>
2026-02-13 08:28:18 +0800

    Spectators not affected by hide victory stats option

--- core/src/com/unciv/ui/screens/victoryscreen/VictoryScreen.kt
+++ core/src/com/unciv/ui/screens/victoryscreen/VictoryScreen.kt
@@ -60,18 +60,22 @@ class VictoryScreen(
         Demographics('D', allowAsSecret = true) {
             override fun getContent(parent: VictoryScreen) = VictoryScreenDemographics(parent.worldScreen)
             override fun isHidden(playerCiv: Civilization) =
-                !playerCiv.gameInfo.gameParameters.showVictoryStats || !UncivGame.Current.settings.useDemographics
+                !UncivGame.Current.settings.useDemographics ||
+                (!playerCiv.isSpectator() && !playerCiv.gameInfo.gameParameters.showVictoryStats)
         },
         Rankings('R', allowAsSecret = true) {
             override fun getContent(parent: VictoryScreen) = VictoryScreenCivRankings(parent.worldScreen)
             override fun isHidden(playerCiv: Civilization) =
-                !playerCiv.gameInfo.gameParameters.showVictoryStats || UncivGame.Current.settings.useDemographics
+                UncivGame.Current.settings.useDemographics ||
+                (!playerCiv.isSpectator() && !playerCiv.gameInfo.gameParameters.showVictoryStats)
         },
         Charts('C') {
             override fun getContent(parent: VictoryScreen) = VictoryScreenCharts(parent.worldScreen)
             override fun isHidden(playerCiv: Civilization) =
-                if (playerCiv.isSpectator()) playerCiv.gameInfo.civilizations.all { it.statsHistory.size < 2 }
-                else playerCiv.statsHistory.size < 2
+                if (playerCiv.isSpectator())
+                    playerCiv.gameInfo.civilizations.all { it.statsHistory.size < 2 }
+                else
+                    playerCiv.statsHistory.size < 2 || !playerCiv.gameInfo.gameParameters.showVictoryStats
         },
         Replay('P', allowAsSecret = true) {
             override fun getContent(parent: VictoryScreen) = VictoryScreenReplay(parent.worldScreen)
