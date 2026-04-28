yairm210/Unciv

commit 9691e5bd80b3e30ab80ba6f048dc74f5b528aabf

Confirmation popup before breaking promise not to spy (#14749)
* Confirmation popup before moving spy to city of civilization we promised not to spy on

* Remove unnecessary condition

core/src/com/unciv/ui/screens/overviewscreen/EspionageOverviewScreen.kt
@@ -9,6 +9,7 @@ import com.unciv.Constants
 import com.unciv.UncivGame
 import com.unciv.logic.city.City
 import com.unciv.logic.civilization.Civilization
+import com.unciv.logic.civilization.diplomacy.DiplomacyFlags
 import com.unciv.models.Spy
 import com.unciv.models.SpyAction
 import com.unciv.models.translations.tr
@@ -225,9 +226,25 @@ class EspionageOverviewScreen(val civInfo: Civilization, val worldScreen: WorldS
             arrow.setOrigin(Align.center)
             arrow.color = Color.WHITE
             onClick {
-                selectedSpy!!.moveTo(city)
-                resetSelection()
-                update()
+                fun move() {
+                    selectedSpy!!.moveTo(city)
+                    resetSelection()
+                    update()
+                }
+                if (city != null
+                    && city.civ.civName != civInfo.civName
+                    && city.civ.isMajorCiv()
+                    && city.civ.getDiplomacyManager(civInfo)!!.hasFlag(DiplomacyFlags.AgreedToNotSendSpies)) {
+                    ConfirmPopup(
+                        UncivGame.Current.screen!!,
+                        // The promise isn't broken when you move the spy - only when they catch you stealing a tech
+                        "This may result in breaking your promise to [${city.civ.civName}]",
+                        "Move",
+                        action = ::move
+                    ).open(force = true)
+                } else {
+                    move()
+                }
             }
             spyActionButtons[this] = city
             isVisible = false

android/assets/jsons/translations/template.properties
@@ -197,6 +197,7 @@ We have cancelled our Defensive Pact with [civName]! =
 [civName] and [targetCivName] have signed a Declaration of Friendship! = 
 [civName] has denounced [targetCivName]! = 
 Do you want to break your promise to [leaderName]? = 
+This may result in breaking your promise to [civName] = 
 Break promise = 
 
 [civName] is upset that you demanded tribute from [cityState], whom they have pledged to protect! = 

commit d7a971903c8d0d14c89be7781627496681afaa6c

Fix `null!!` regression from #14749

core/src/com/unciv/ui/screens/overviewscreen/EspionageOverviewScreen.kt
@@ -234,6 +234,7 @@ class EspionageOverviewScreen(val civInfo: Civilization, val worldScreen: WorldS
                 if (city != null
                     && city.civ.civName != civInfo.civName
                     && city.civ.isMajorCiv()
+                    && city.civ.knows(civInfo) // ensures the !! below won't crash
                     && city.civ.getDiplomacyManager(civInfo)!!.hasFlag(DiplomacyFlags.AgreedToNotSendSpies)) {
                     ConfirmPopup(
                         UncivGame.Current.screen!!,