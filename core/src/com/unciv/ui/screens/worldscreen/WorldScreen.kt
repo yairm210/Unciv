908db99eb17c19b4b2267a7e807e3338b799e0e4
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2026-05-19 13:28:37 +0200

    Allow dragging TileInfoTable between right and bottom edge

--- core/src/com/unciv/ui/screens/worldscreen/WorldScreen.kt
+++ core/src/com/unciv/ui/screens/worldscreen/WorldScreen.kt
@@ -434,8 +434,7 @@ class WorldScreen(
             minimapWrapper.update(getGameViewConsideringForOfWar().civView.getCiv())
             bottomTileInfoTable.civView = getGameViewConsideringForOfWar().civView
             bottomTileInfoTable.updateTileTable(mapHolder.selectedTile)
-            bottomTileInfoTable.x = stage.width - bottomTileInfoTable.width
-            bottomTileInfoTable.y = if (game.settings.showMinimap) minimapWrapper.height + 5f else 0f
+            bottomTileInfoTable.setPosition()
 
             battleTable.update()
 
@@ -501,8 +500,12 @@ class WorldScreen(
         updateGameplayButtons()
 
         val coveredNotificationsTop = stage.height - statusButtons.y
-        val coveredNotificationsBottom = (bottomTileInfoTable.height + bottomTileInfoTable.y)
-//                (if (game.settings.showMinimap) minimapWrapper.height else 0f)
+        val coveredNotificationsBottom = when {
+            game.settings.tileInfoPosition == GameSettings.WidgetPosition.Right ->
+                bottomTileInfoTable.height + bottomTileInfoTable.y
+            game.settings.showMinimap -> minimapWrapper.height
+            else -> 0f
+        }
         notificationsScroll.update(viewingCiv.notifications, coveredNotificationsTop, coveredNotificationsBottom)
 
         val posZoomFromRight = if (game.settings.showMinimap) minimapWrapper.width
--- core/src/com/unciv/ui/screens/worldscreen/bottombar/TileInfoTable.kt
+++ core/src/com/unciv/ui/screens/worldscreen/bottombar/TileInfoTable.kt
@@ -1,16 +1,19 @@
 package com.unciv.ui.screens.worldscreen.bottombar
 
 import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.Touchable
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.utils.DragListener
 import com.badlogic.gdx.utils.Align
-import com.unciv.ui.objectdescriptions.TileDescription
+import com.unciv.GUI
+import com.unciv.models.metadata.GameSettings
 import com.unciv.models.translations.tr
 import com.unciv.ui.components.extensions.addBorderAllowOpacity
 import com.unciv.ui.components.extensions.darken
 import com.unciv.ui.components.extensions.toLabel
 import com.unciv.ui.components.input.onClick
+import com.unciv.ui.objectdescriptions.TileDescription
 import com.unciv.ui.popups.Popup
 import com.unciv.ui.screens.basescreen.BaseScreen
 import com.unciv.ui.screens.civilopediascreen.FormattedLine.IconDisplay
@@ -22,12 +25,15 @@ import com.unciv.view.TileView
 
 class TileInfoTable(private val worldScreen: WorldScreen) : Table(BaseScreen.skin) {
     var civView: CivView = worldScreen.selectedGameView.civView
+    var position by worldScreen.game.settings::tileInfoPosition
 
     init {
         background = BaseScreen.skinStrings.getUiBackground(
             "WorldScreen/TileInfoTable",
             tintColor = BaseScreen.skinStrings.skinConfig.baseColor.darken(0.5f)
         )
+        touchable = Touchable.enabled
+        addListener(MoveDragListener())
     }
 
     internal fun updateTileTable(selectedTileView: TileView?) {
@@ -36,21 +42,37 @@ class TileInfoTable(private val worldScreen: WorldScreen) : Table(BaseScreen.ski
 
         // A retained selection may still carry the previous spectator or civilization perspective.
         val tileView = selectedTileView?.let { civView.gameView.getTile(it) }
-        if (tileView != null && (DebugUtils.VISIBLE_MAP || civView.hasExplored(tileView))) {
-            add(getStatsTable(tileView)).left().row()
-            add(MarkupRenderer.render(TileDescription.toMarkup(tileView, civView), padding = 0f, iconDisplay = IconDisplay.None) {
-                worldScreen.openCivilopedia(it)
-            } ).padTop(5f).row()
-            if (DebugUtils.VISIBLE_MAP) add(tileView.position().toPrettyString().toLabel()).colspan(2).pad(5f)
-            if (DebugUtils.SHOW_TILE_IMAGE_LOCATIONS){
-                val imagesString = "Images: " + worldScreen.mapHolder.tileGroups[tileView]!!.layerTerrain.tileBaseImages.joinToString{"\n"+it.name}
-                add(imagesString.toLabel())
-            }
-            
+        if (tileView == null || !(DebugUtils.VISIBLE_MAP || civView.hasExplored(tileView))) {
+            isVisible = false
+            return
+        }
+        add(getStatsTable(tileView)).left().row()
+        add(MarkupRenderer.render(TileDescription.toMarkup(tileView, civView), padding = 0f, iconDisplay = IconDisplay.None) {
+            worldScreen.openCivilopedia(it)
+        } ).padTop(5f).row()
+        if (DebugUtils.VISIBLE_MAP) add(tileView.position().toPrettyString().toLabel()).colspan(2).pad(5f)
+        if (DebugUtils.SHOW_TILE_IMAGE_LOCATIONS) {
+            val imagesString = "Images: " + worldScreen.mapHolder.tileGroups[tileView]!!.layerTerrain.tileBaseImages.joinToString{"\n"+it.name}
+            add(imagesString.toLabel())
         }
 
         pack()
         addBorderAllowOpacity(1f, Color.WHITE)
+        isVisible = true
+    }
+
+    internal fun setPosition() {
+        val x: Float
+        val y: Float
+        val isBottom = position == GameSettings.WidgetPosition.Bottom
+        if (worldScreen.game.settings.showMinimap) {
+            x = stage.width - (if (isBottom) worldScreen.minimapWrapper.width + 5f else 0f)
+            y = if (isBottom) 0f else worldScreen.minimapWrapper.height + 5f
+        } else {
+            x = stage.width
+            y = 0f
+        }
+        setPosition(x, y, Align.bottomRight)
     }
 
     private fun getStatsTable(tileView: TileView): Table {
@@ -72,5 +94,23 @@ class TileInfoTable(private val worldScreen: WorldScreen) : Table(BaseScreen.ski
         return table
     }
 
-    override fun draw(batch: Batch?, parentAlpha: Float) = super.draw(batch, parentAlpha)
+    private inner class MoveDragListener : DragListener() {
+        private var dragged = false
+        override fun touchDragged(event: InputEvent, x: Float, y: Float, pointer: Int) {
+            super.touchDragged(event, x, y, pointer)
+            if (!isDragging) return
+            dragged = true
+            this@TileInfoTable.setPosition(event.stageX, event.stageY, Align.center)
+        }
+        override fun touchUp(event: InputEvent, x: Float, y: Float, pointer: Int, button: Int) {
+            if (dragged) event.stop()
+            super.touchUp(event, x, y, pointer, button)
+            if (dragged) {
+                position = if (event.stageY > stage.width - event.stageX) GameSettings.WidgetPosition.Right
+                    else GameSettings.WidgetPosition.Bottom
+                setPosition()
+                GUI.setUpdateWorldOnNextRender() // full update so the notification scroll can adapt
+            }
+        }
+    }
 }
