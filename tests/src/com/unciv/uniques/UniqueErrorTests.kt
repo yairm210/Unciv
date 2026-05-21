970ba57313ef69002dc7542d92dfc74b1e504cbb
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2026-05-21 23:50:02 +0200

    Unit test for Educated Elite's Unique and its effect

--- tests/src/com/unciv/uniques/UniqueErrorTests.kt
+++ tests/src/com/unciv/uniques/UniqueErrorTests.kt
@@ -1,12 +1,17 @@
 package com.unciv.uniques
 
+import com.unciv.UncivGame
+import com.unciv.logic.civilization.CivFlags
 import com.unciv.models.ruleset.RulesetCache
 import com.unciv.models.ruleset.unique.Unique
 import com.unciv.models.ruleset.unique.UniqueTarget
 import com.unciv.models.ruleset.unique.UniqueType
 import com.unciv.models.ruleset.validation.RulesetErrorSeverity
 import com.unciv.models.ruleset.validation.UniqueValidator
+import com.unciv.models.translations.getPlaceholderParameters
 import com.unciv.testing.GdxTestRunner
+import com.unciv.testing.TestGame
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -59,5 +64,63 @@ class UniqueErrorTests {
         val uniqueWithSourceObject = Unique(uniqueText, sourceObjectType = UniqueTarget.Promotion)
         val errorListCorrectUniqueContainer = UniqueValidator(ruleset).checkUnique(uniqueWithSourceObject, false, null)
         assert(errorListCorrectUniqueContainer.getFinalSeverity() == RulesetErrorSeverity.OK)
+    }
+
+    @Test
+    fun testEducatedEliteGreatPersonGifting() {
+        // set up game
+        var failures = 0
+        fun logFailure(msg: String) {
+            failures++
+            println(msg)
+        }
+        val flagName = CivFlags.CityStateGreatPersonGift.name
+        val game = TestGame()
+        game.makeHexagonalMap(2)
+        game.setSpeed("Quick")
+        // prevent files access from completing tutorial tasks
+        UncivGame.Current.settings.tutorialTasksCompleted.addAll(
+            game.gameInfo.ruleset.events.keys
+                .filter { it.startsWith("Tutorial Task: [") }
+                .flatMap { it.getPlaceholderParameters() }
+        )
+
+        // set up civs
+        val civ = game.addCiv()
+        game.addCity(civ, game.getTile(-2, 0))
+        game.gameInfo.currentPlayerCiv = civ
+        val cityState = game.addCiv(cityStateType = "Mercantile")
+        game.addCity(cityState, game.getTile(2, 0))
+
+        // make the city-state allied
+        civ.diplomacyFunctions.makeCivilizationsMeet(cityState)
+        civ.addGold(10000)
+        cityState.cityStateFunctions.receiveGoldGift(civ, 10000)
+        if (cityState.allyCiv != civ)
+            logFailure("The test civ and city-state should be allied after a 10k gift")
+
+        // adopt the GP-gift-enabling policy
+        civ.removeFlag(flagName)
+        val ee = game.ruleset.policies["Educated Elite"]!!
+        civ.policies.freePolicies++
+        civ.policies.adopt(ee)
+        if (!civ.hasFlag(flagName))
+            logFailure("The test civ should have the $flagName flag after adopting Educated Elite")
+
+        // Ensure automation won't use up the GP - a threat won't do as it will be placed in the capital
+        game.ruleset.units.entries.removeIf { it.value.isGreatPerson }
+        game.createBaseUnit("Civilian", "Great Person - [Gold]", "Unbuildable", "Uncapturable")
+
+        // Force the countdown and pass *one* turn, a GP should appear
+        civ.addFlag(flagName, 1)
+        game.gameInfo.simulateUntilWin = true
+        game.gameInfo.simulateMaxTurns = 1
+        game.gameInfo.nextTurn()
+        val unit = civ.units.getCivUnits().firstOrNull { it.isGreatPerson() }
+        if (unit == null)
+            logFailure("No gifted Great Person unit found after setting $flagName to 1 and passing a turn")
+        else
+            println("GP unit gifted: $unit")
+        Assert.assertEquals(0, failures)
     }
 }
