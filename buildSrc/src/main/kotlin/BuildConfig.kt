3da753e44bf0163a497ea9205109e051cd805baf
Yair Morgenstern <yairm210@hotmail.com>
2026-05-31 14:57:19 +0300

    Bump version and create initial changelog entry (#14958)
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,7 +3,7 @@ package com.unciv.build
 
 object BuildConfig {
     const val appName = "Unciv"
-    const val appCodeNumber = 1223
-    const val appVersion = "4.20.9-patch1"
+    const val appCodeNumber = 1224
+    const val appVersion = "4.20.10"
     const val identifier = "com.unciv.app"
 }
