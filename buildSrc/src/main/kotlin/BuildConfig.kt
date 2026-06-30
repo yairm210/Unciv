e277dabe2523966770f557f27a0781b2273d06c5
yairm210 <yairm210@users.noreply.github.com>
2026-06-30 19:59:12 +0000

    Bump version and create initial changelog entry

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,7 +3,7 @@ package com.unciv.build
 
 object BuildConfig {
     const val appName = "Unciv"
-    const val appCodeNumber = 1230
-    const val appVersion = "4.20.16"
+    const val appCodeNumber = 1231
+    const val appVersion = "4.20.17"
     const val identifier = "com.unciv.app"
 }
