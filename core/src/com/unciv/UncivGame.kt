5ea942cba57d7821de287a2772d814e8b697950a
Yair Morgenstern <yairm210@hotmail.com>
2026-08-10 23:55:25 +0300

    Version rollout (#15369)
    * Bump version and create initial changelog entry
    
    * Translation: Update Hungarian.properties (#15344)
    
    Translate new strings
    
    * Update Indonesian.properties (#15343)
    
    Translation and correction.
    
    * Update Brazilian_Portuguese.properties (#15342)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: summoner <summoner001@users.noreply.github.com>
    Co-authored-by: X2003 <158255876+Xenia2003@users.noreply.github.com>
    Co-authored-by: Vitor Gabriel <Ranbut_Beater@protonmail.com>

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -469,7 +469,7 @@ open class UncivGame(val isConsoleMode: Boolean = false) : Game(), PlatformSpeci
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.21.6", 1242)
+        val VERSION = Version("4.21.7", 1243)
         //endregion
 
         /** Global reference to the one Gdx.Game instance created by the platform launchers - do not use without checking [isCurrentInitialized] first. */
