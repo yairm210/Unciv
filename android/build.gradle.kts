1b8c8b9c2b0ec7fae6a5290b322041a541def3ed
yairm210 <yairm210@hotmail.com>
2026-08-11 08:19:55 +0300

    Apparently that's the wrong fix, it's expressly disallowed in the new gradle version

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,7 +6,6 @@ import java.util.Properties
 
 plugins {
     id("com.android.application")
-    id("kotlin-android")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -155,7 +155,6 @@ private fun getSdkPath(): String? {
 if (getSdkPath() != null) {
     project(":android") {
         apply(plugin = "com.android.application")
-        apply(plugin = "kotlin-android")
 
         val natives by configurations.creating
 
