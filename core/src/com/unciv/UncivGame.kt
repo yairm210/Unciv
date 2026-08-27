4e6e32b17d0e6c2c177e168770881a2302ce6a11
yairm210 <yairm210@users.noreply.github.com>
2026-08-27 07:52:36 +0000

    Bump version and create initial changelog entry

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -476,7 +476,7 @@ private fun logRunningThreads() {
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.21.12", 1253)
+        val VERSION = Version("4.21.13", 1254)
         //endregion
 
         /** Global reference to the one Gdx.Game instance created by the platform launchers - do not use without checking [isCurrentInitialized] first. */
