0040835f43a75c07285f37b276a91c1dc63384f8
yairm210 <yairm210@hotmail.com>
2026-08-30 10:21:03 +0300

    chore: Clearer "on dispose wait for autosave", doesn't solve the problem but does clear up what the problem is

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -429,20 +429,21 @@ open class UncivGame(val isConsoleMode: Boolean = false) : Game(), PlatformSpeci
         // We stop the *in-game* multiplayer update, so that it doesn't keep working and A. we'll have errors and B. we'll have multiple updaters active
         if (::onlineMultiplayer.isInitialized) onlineMultiplayer.multiplayerGameUpdater.cancel()
 
-        val curGameInfo = gameInfo
-        if (curGameInfo != null) {
+        settings.save()
+
+        if (gameInfo != null) {
             val autoSaveJob = files.autosaves.autoSaveJob
             if (autoSaveJob != null && autoSaveJob.isActive) {
-                // auto save is already in progress (e.g. started by onPause() event)
-                // let's allow it to finish and do not try to autosave second time
-                Concurrency.runBlocking {
+                // onPause() call always precedes dispose() call so auto save is already in progress
+                // This is the primary cause of ANRs at the moment - 
+                //   we need some way to have the autosave keep working but remove the other threads,
+                // I'm not sure what the right way is, we can either not clear daemon threads
+                // or we can cancel the job if it takes too long...?
+                Concurrency.runBlocking { 
                     autoSaveJob.join()
                 }
-            } else {
-                files.autosaves.autoSave(curGameInfo)      // NO new thread
             }
         }
-        settings.save()
         Concurrency.stopThreadPools()
 
         // On desktop this should only be this one and "DestroyJavaVM"
