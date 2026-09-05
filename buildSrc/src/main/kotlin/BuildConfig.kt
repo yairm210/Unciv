c8eb2836cea8d00416a9beb416703a53f6d3d0b9
Yair Morgenstern <yairm210@hotmail.com>
2026-09-05 22:45:02 +0300

    Version rollout (#15545)
    * Bump version and create initial changelog entry
    
    * Translate and complete Vietnamese properties file (#15534)
    
    * Update Greek.properties (#15532)
    
    * Update Greek.properties
    
    * Update Greek.properties
    
    * Update French.properties (#15528)
    
    * Update Indonesian.properties (#15527)
    
    Translation and correction.
    
    * Update Brazilian_Portuguese.properties (#15526)
    
    * Translation: Update Hungarian.properties (#15525)
    
    Translate new strings
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: dangdinhbaohoang12 <hoang.s297312@ilamail.edu.vn>
    Co-authored-by: G.Delakos <103367604+Delakos98@users.noreply.github.com>
    Co-authored-by: Ouaz <Ouaz@users.noreply.github.com>
    Co-authored-by: X2003 <158255876+Xenia2003@users.noreply.github.com>
    Co-authored-by: Vitor Gabriel <Ranbut_Beater@protonmail.com>
    Co-authored-by: summoner <summoner001@users.noreply.github.com>

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,7 +3,7 @@ package com.unciv.build
 
 object BuildConfig {
     const val appName = "Unciv"
-    const val appCodeNumber = 1257
-    const val appVersion = "4.21.16"
+    const val appCodeNumber = 1258
+    const val appVersion = "4.21.17"
     const val identifier = "com.unciv.app"
 }
