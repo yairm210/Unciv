f5719024a7517c1c350d984032af8dcab557c867
Yair Morgenstern <yairm210@hotmail.com>
2026-09-07 15:39:08 +0300

    Version rollout (#15559)
    * Bump version and create initial changelog entry
    
    * Update Italian.properties (#15558)
    
    ---------
    
    Co-authored-by: yairm210 <yairm210@users.noreply.github.com>
    Co-authored-by: Giuseppe D'Addio <41149920+Smashfanful@users.noreply.github.com>

--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -493,7 +493,7 @@ private fun logRunningThreads() {
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.21.17", 1258)
+        val VERSION = Version("4.21.18", 1259)
         //endregion
 
         /** Global reference to the one Gdx.Game instance created by the platform launchers - do not use without checking [isCurrentInitialized] first. */
