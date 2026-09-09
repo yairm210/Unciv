32cd4e2c76174404209411c2b8de2a2b7528c2e0
yairm210 <yairm210@users.noreply.github.com>
2026-09-09 12:37:44 +0000

    Bump version and create initial changelog entry

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -3,7 +3,7 @@ package com.unciv.build
 
 object BuildConfig {
     const val appName = "Unciv"
-    const val appCodeNumber = 1259
-    const val appVersion = "4.21.18"
+    const val appCodeNumber = 1260
+    const val appVersion = "4.21.19"
     const val identifier = "com.unciv.app"
 }
--- core/src/com/unciv/UncivGame.kt
+++ core/src/com/unciv/UncivGame.kt
@@ -493,7 +493,7 @@ private fun logRunningThreads() {
 
     companion object {
         //region AUTOMATICALLY GENERATED VERSION DATA - DO NOT CHANGE THIS REGION, INCLUDING THIS COMMENT
-        val VERSION = Version("4.21.18", 1259)
+        val VERSION = Version("4.21.19", 1260)
         //endregion
 
         /** Global reference to the one Gdx.Game instance created by the platform launchers - do not use without checking [isCurrentInitialized] first. */
