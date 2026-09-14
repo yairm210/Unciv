3884609c79e6375a631fc0b0ebac887c28be9bda
Loof <223481293+unciv-loof@users.noreply.github.com>
2026-09-14 13:45:15 +0200

    Automation setting for specialist control in new cities (#15584)

--- core/src/com/unciv/logic/city/managers/CityFounder.kt
+++ core/src/com/unciv/logic/city/managers/CityFounder.kt
@@ -88,6 +88,7 @@ class CityFounder {
         addStartingBuildings(city, civInfo, startingEra)
         
         city.resetDisabledConstructions()
+        city.resetSpecialistsControl()
 
         for (unique in civInfo.getTriggeredUniques(UniqueType.TriggerUponFoundingCity,
             GameContext(civInfo, city, unit)
