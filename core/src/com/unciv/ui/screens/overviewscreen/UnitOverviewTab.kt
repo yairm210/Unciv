361debddfe4e6fa8c2452c753591b4cdcb5c0b02
dangdinhbaohoang12 <hoang.s297312@ilamail.edu.vn>
2026-09-26 08:58:38 +0000

    resolve https://github.com/yairm210/Unciv/issues/15646

--- core/src/com/unciv/ui/screens/overviewscreen/UnitOverviewTab.kt
+++ core/src/com/unciv/ui/screens/overviewscreen/UnitOverviewTab.kt
@@ -67,7 +67,7 @@ class UnitOverviewTab(
         paddingVert = 5f,
         paddingHorz = 8f,
         separateHeader = true,
-        gameInfo = gameInfo
+        viewingPlayer = viewingPlayer
     ) { header, details, totals ->
         // Notes: header.parent is the LinkedScrollPane of TabbedPager. Its linked twin is details.parent.parent.parent however!
         // horizontal "slack" if available width > content width is taken up between SortableGrid and CityOverviewTab for the details,
