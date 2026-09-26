7a25be907421230f5be14611e32f8d6fa2b80ad5
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2026-09-26 18:30:52 +0000

    Add a `--log` option to the Linux Unciv.sh launcher to enable logs from the release build (#15649)

--- desktop/linuxFilesForJar/Unciv.sh
+++ desktop/linuxFilesForJar/Unciv.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 CONFIG_DIR="$HOME/.local/share/Unciv"
+LOG_FILE=""
+ENABLE_LOG=false
 
-USAGE="Unciv [--help | -h | --config-dir PATH]
+USAGE="Unciv [--help | -h | --config-dir PATH | --log [FILE]]
 
 Run the Unciv game.
 
@@ -11,6 +13,8 @@ With '--help' or '-h', show this help info and exit.
 With '--config-dir PATH', use/make configuration files in PATH instead
 of the default of '$CONFIG_DIR'.
 
+With '--log', enable debug logging. Output goes to the terminal by
+default, or to FILE if given.
 "
 
 usage() {
@@ -34,8 +38,15 @@ if [ "$#" -gt "0" ]; then
             CONFIG_DIR="$2"
             shift 2
             ;;
+    --log)
+        ENABLE_LOG=true
+        shift
+        case "$1" in
+            --*|"") ;;
+            *) LOG_FILE="$1"; shift ;;
+        esac
+        ;;
     esac
-    shift
 fi
 if ! [ "$#" -eq "0" ]; then
     fail "Unknown argument(s): $*"
@@ -43,4 +54,13 @@ fi
 
 mkdir -p "$CONFIG_DIR"
 cd "$CONFIG_DIR" || fail "Could not 'cd' to '$CONFIG_DIR'"
-java -jar /usr/share/Unciv/Unciv.jar
+
+if [ "$ENABLE_LOG" = true ]; then
+    if [ -n "$LOG_FILE" ]; then
+        java -ea -jar /usr/share/Unciv/Unciv.jar > "$LOG_FILE" 2>&1
+    else
+        java -ea -jar /usr/share/Unciv/Unciv.jar
+    fi
+else
+    java -jar /usr/share/Unciv/Unciv.jar
+fi
